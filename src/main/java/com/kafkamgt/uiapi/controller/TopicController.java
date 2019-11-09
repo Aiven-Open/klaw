16c99f7ee0a5eb56f0c367c7248245727fec5b36
Muralidhar Basani <murali.basani@gmail.com>
2019-11-09 14:50:17 +0100

    Removed modules, added validation to requests pages, added decline requests

---
+++ src/main/java/com/kafkamgt/uiapi/controller/TopicController.java
@@ -0,0 +1,108 @@
+package com.kafkamgt.uiapi.controller;
+
+
+import com.kafkamgt.uiapi.dao.Topic;
+import com.kafkamgt.uiapi.dao.TopicRequest;
+import com.kafkamgt.uiapi.error.KafkawizeException;
+import com.kafkamgt.uiapi.model.PCStream;
+import com.kafkamgt.uiapi.model.TopicInfo;
+import com.kafkamgt.uiapi.service.TopicControllerService;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.MediaType;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.*;
+
+import java.util.List;
+
+
+@RestController
+@RequestMapping("/")
+public class TopicController {
+
+    @Autowired
+    private TopicControllerService topicControllerService;
+
+    @Autowired
+    public TopicController(TopicControllerService topicControllerService){
+        this.topicControllerService = topicControllerService;
+    }
+
+    @PostMapping(value = "/createTopics")
+    public ResponseEntity<String> createTopics(@RequestBody TopicRequest addTopicRequest) {
+        String result = topicControllerService.createTopics(addTopicRequest);
+        return new ResponseEntity<>(result, HttpStatus.OK);
+    }
+
+    @PostMapping(value = "/updateSyncTopics")
+    public ResponseEntity<String> updateSyncTopics(@RequestParam ("updatedSyncTopics") String updatedSyncTopics,
+                                                   @RequestParam ("envSelected") String envSelected) {
+        String updateSyncTopicsResult = topicControllerService.updateSyncTopics(updatedSyncTopics, envSelected);
+        return new ResponseEntity<>(updateSyncTopicsResult, HttpStatus.OK);
+    }
+
+    @RequestMapping(value = "/getTopicStreams", method = RequestMethod.GET, produces = {MediaType.APPLICATION_JSON_VALUE})
+    public ResponseEntity<List<PCStream>> getTopicStreams(@RequestParam ("env") String envSelected,
+                                                          @RequestParam("pageNo") String pageNo,
+                                                          @RequestParam(value="topicnamesearch",required=false) String topicNameSearch) {
+        return new ResponseEntity<>(topicControllerService.getTopicStreams(envSelected, pageNo, topicNameSearch), HttpStatus.OK);
+    }
+
+    @RequestMapping(value = "/getTopicRequests", method = RequestMethod.GET, produces = {MediaType.APPLICATION_JSON_VALUE})
+    public ResponseEntity<List<TopicRequest>> getTopicRequests() {
+
+        return new ResponseEntity<>(topicControllerService.getTopicRequests(), HttpStatus.OK);
+    }
+
+    @RequestMapping(value = "/getTopicTeam", method = RequestMethod.GET, produces = {MediaType.APPLICATION_JSON_VALUE})
+    public ResponseEntity<Topic> getTopicTeam(@RequestParam("topicName") String topicName,
+                                                     @RequestParam("env") String env) {
+       return new ResponseEntity<>(topicControllerService.getTopicTeam(topicName, env), HttpStatus.OK);
+    }
+
+    @RequestMapping(value = "/getCreatedTopicRequests", method = RequestMethod.GET, produces = {MediaType.APPLICATION_JSON_VALUE})
+    public ResponseEntity<List<TopicRequest>> getCreatedTopicRequests() {
+
+        return new ResponseEntity<>(topicControllerService.getCreatedTopicRequests(), HttpStatus.OK);
+    }
+
+    @RequestMapping(value = "/deleteTopicRequests", method = RequestMethod.GET, produces = {MediaType.APPLICATION_JSON_VALUE})
+    public ResponseEntity<String> deleteTopicRequests(@RequestParam("topicName") String topicName) {
+
+        return new ResponseEntity<>(topicControllerService.deleteTopicRequests(topicName), HttpStatus.OK);
+    }
+
+    @RequestMapping(value = "/execTopicRequests", method = RequestMethod.GET, produces = {MediaType.APPLICATION_JSON_VALUE})
+    public ResponseEntity<String> approveTopicRequests(@RequestParam("topicName") String topicName) throws KafkawizeException {
+
+        return new ResponseEntity<>(topicControllerService.approveTopicRequests(topicName), HttpStatus.OK);
+    }
+
+    @RequestMapping(value = "/execTopicRequestsDecline", method = RequestMethod.GET, produces = {MediaType.APPLICATION_JSON_VALUE})
+    public ResponseEntity<String> declineTopicRequests(@RequestParam("topicName") String topicName) throws KafkawizeException {
+
+        return new ResponseEntity<>(topicControllerService.declineTopicRequests(topicName), HttpStatus.OK);
+    }
+
+    @RequestMapping(value = "/getTopics", method = RequestMethod.GET, produces = {MediaType.APPLICATION_JSON_VALUE})
+    public ResponseEntity<List<TopicInfo>> getTopics(@RequestParam("env") String env,
+                                                     @RequestParam("pageNo") String pageNo,
+                                                     @RequestParam(value="topicnamesearch",required=false) String topicNameSearch) throws Exception {
+
+        return new ResponseEntity<>(topicControllerService.getTopics(env, pageNo, topicNameSearch), HttpStatus.OK);
+    }
+
+    @RequestMapping(value = "/getTopicsOnly", method = RequestMethod.GET, produces = {MediaType.APPLICATION_JSON_VALUE})
+    public ResponseEntity<List<String>> getTopicsOnly(@RequestParam("env") String env) throws Exception {
+
+        return new ResponseEntity<>(topicControllerService.getAllTopics(env), HttpStatus.OK);
+    }
+
+    @RequestMapping(value = "/getSyncTopics", method = RequestMethod.GET, produces = {MediaType.APPLICATION_JSON_VALUE})
+    public ResponseEntity<List<TopicRequest>> getSyncTopics(@RequestParam("env") String env, @RequestParam("pageNo") String pageNo,
+                        @RequestParam(value="topicnamesearch",required=false) String topicNameSearch) throws Exception {
+
+        return new ResponseEntity<>(topicControllerService.getSyncTopics(env, pageNo, topicNameSearch), HttpStatus.OK);
+    }
+
+}
