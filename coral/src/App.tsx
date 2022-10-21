b3a39a1a7ab5b6e7cccf2e8ac28d26f0e7d13b7f
Samuli Suortti <smulis@aiven.io>
2022-10-21 18:31:33 +0300

    feat[coral]: Showcase css modules with example
    CSS Modules work by default.
    
    Refers: #113

--- coral/src/App.tsx
+++ coral/src/App.tsx
@@ -1,4 +1,5 @@
 import { Flexbox, TagLabel, Typography } from "@aivenio/design-system";
+import classes from "./App.module.css";
 
 function App() {
   return (
@@ -6,7 +7,7 @@ function App() {
       <Typography htmlTag={"h1"} variant={"heading-2xl"}>
         Hello Klaw 👋
       </Typography>
-      <Flexbox alignItems={"center"}>
+      <Flexbox alignItems={"center"} className={classes.aClassName}>
         <p>This uses the aiven design system! &nbsp; </p>
         <TagLabel title="yey" />
       </Flexbox>
