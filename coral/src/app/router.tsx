6b6bc0e49905398977fa81114148f6d58e65c627
Mirjam Aulbach <mirjam.aulbach@aiven.io>
2022-10-31 18:03:38 +0100

    Add login page to routing.

--- coral/src/app/router.tsx
+++ coral/src/app/router.tsx
@@ -1,11 +1,16 @@
 import { createBrowserRouter, Navigate, RouteObject } from "react-router-dom";
 import HomePage from "src/app/pages";
 import Hello from "src/app/pages/Hello";
+import Login from "src/app/pages/Login";
 
 const routes: Array<RouteObject> = [
   {
     path: "/",
     element: <HomePage />,
+  },
+  {
+    path: "/login",
+    element: <Login />,
   },
   {
     path: "/hello",
