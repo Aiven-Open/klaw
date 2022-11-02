229e8a2dd6adb0028861159c7e8506320537ae39
Mirjam Aulbach <mirjam.aulbach@aiven.io>
2022-11-02 13:36:57 +0100

    Add react-query devtools. Update react-query.

--- coral/src/app/main.tsx
+++ coral/src/app/main.tsx
@@ -2,16 +2,19 @@ import React from "react";
 import ReactDOM from "react-dom/client";
 import { RouterProvider } from "react-router-dom";
 import router from "src/app/router";
-
 import "@aivenio/design-system/dist/styles.css";
 import { QueryClient, QueryClientProvider } from "@tanstack/react-query";
+import { ReactQueryDevtools } from "@tanstack/react-query-devtools";
 
+// @TODO add vite env
+const DEV_MODE = true;
 const queryClient = new QueryClient();
 
 ReactDOM.createRoot(document.getElementById("root") as HTMLElement).render(
   <React.StrictMode>
     <QueryClientProvider client={queryClient}>
-      <RouterProvider router={router} />
+      {DEV_MODE && <RouterProvider router={router} />}
+      <ReactQueryDevtools />
     </QueryClientProvider>
   </React.StrictMode>
 );
