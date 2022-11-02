8525d903ca59c8391b0fafd9d9e5b667b14a6cf5
Mirjam Aulbach <mirjam.aulbach@aiven.io>
2022-11-02 15:11:52 +0100

    coral: setup react query (#170)
    * feat: add react-query library.
    
    * feat: Add useQuery with a mocked response to home page.
    
    - the example confirms that react-query is working.
    - it enables to write a test to confirm that all
    basic functionality can be tested with the current setup.
    
    * Create utils test function.
    
    * Add react-query devtools. Update react-query.
    
    * fix: setup in main.tsx
    
    - use proper dev env instead of hard coded setting
    - wrap the right component in condition

--- coral/src/app/pages/index.test.tsx
+++ coral/src/app/pages/index.test.tsx
@@ -1,15 +1,43 @@
 import HomePage from "src/app/pages";
-import { render, cleanup, screen } from "@testing-library/react";
+import { cleanup, screen } from "@testing-library/react";
+import { renderWithQueryClient } from "src/services/test-utils";
+
+const loadingText = "data is loading";
+const textFromApi = "hello";
 
 describe("HomePage", () => {
   beforeEach(() => {
-    render(<HomePage />);
+    renderWithQueryClient(<HomePage />);
   });
 
   afterEach(() => {
     cleanup();
   });
-  it("shoud render dummy content", () => {
-    expect(screen.getByText("Index")).toBeVisible();
+
+  it("renders dummy content", () => {
+    const heading = screen.getByRole("heading", { name: "Index" });
+
+    expect(heading).toBeVisible();
+  });
+
+  it("shows a loading information as long as data is not returned from 'API'", () => {
+    const loadingInfo = screen.getByText(loadingText);
+
+    expect(loadingInfo).toBeVisible();
+  });
+
+  it("shows the data returned from the 'API'", async () => {
+    const dataFromApi = await screen.findByText(textFromApi);
+
+    expect(dataFromApi).toBeVisible();
+  });
+
+  it("removes loading information when data is returned from the 'API'", async () => {
+    const loadingInfo = screen.queryByText(loadingText);
+    expect(loadingInfo).toBeVisible();
+
+    await screen.findByText(textFromApi);
+
+    expect(loadingInfo).not.toBeInTheDocument();
   });
 });
---
+++ coral/src/services/test-utils/index.ts
@@ -0,0 +1,3 @@
+import { renderWithQueryClient } from "src/services/test-utils/render-with-query-client";
+
+export { renderWithQueryClient };
