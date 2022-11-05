b8f8ff092177955185f449862d54d1432808fb8d
Sebastian Kaiser <sk4is3r@gmail.com>
2022-11-05 06:34:30 +0100

    Use log.err(String, Throwable) when printing some exceptions

--- cluster-api/src/main/java/io/aiven/klaw/clusterapi/services/KafkaConnectService.java
+++ cluster-api/src/main/java/io/aiven/klaw/clusterapi/services/KafkaConnectService.java
@@ -40,7 +40,7 @@ public class KafkaConnectService {
     try {
       reqDetails.getRight().delete(reqDetails.getLeft(), String.class);
     } catch (RestClientException e) {
-      log.error("Error in deleting connector " + e.toString());
+      log.error("Error in deleting connector ", e);
       result.put("result", ApiResultStatus.ERROR.value);
       result.put("errorText", e.toString().replaceAll("\"", ""));
       return result;
@@ -71,7 +71,7 @@ public class KafkaConnectService {
     try {
       reqDetails.getRight().put(reqDetails.getLeft(), request, String.class);
     } catch (RestClientException e) {
-      log.error("Error in updating connector " + e.toString());
+      log.error("Error in updating connector ", e);
       result.put("result", ApiResultStatus.ERROR.value);
       result.put("errorText", e.toString().replaceAll("\"", ""));
       return result;
