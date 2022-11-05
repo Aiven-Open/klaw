452156c3980cd0cfdaf7b822875716dfde354335
Sebastian Kaiser <sk4is3r@gmail.com>
2022-11-05 06:27:10 +0100

    Avoid adding empty String to non-constant primitives

--- cluster-api/src/main/java/io/aiven/klaw/clusterapi/services/MonitoringService.java
+++ cluster-api/src/main/java/io/aiven/klaw/clusterapi/services/MonitoringService.java
@@ -13,9 +13,8 @@ import org.springframework.stereotype.Service;
 @Service
 public class MonitoringService {
 
-  final ClusterApiUtils clusterApiUtils;
-
   private static final long timeOutSecsForAcls = 5;
+  final ClusterApiUtils clusterApiUtils;
 
   public MonitoringService(ClusterApiUtils clusterApiUtils) {
     this.clusterApiUtils = clusterApiUtils;
@@ -75,10 +74,10 @@ public class MonitoringService {
         long latestOffset = listOffsetsLatestResult.partitionResult(topicPartition).get().offset();
         long lag = latestOffset - earliestOffset;
 
-        offsetDetails.put("topicPartitionId", topicPartition.partition() + "");
-        offsetDetails.put("currentOffset", earliestOffset + "");
-        offsetDetails.put("endOffset", latestOffset + "");
-        offsetDetails.put("lag", lag + "");
+        offsetDetails.put("topicPartitionId", Long.toString(topicPartition.partition()));
+        offsetDetails.put("currentOffset", Long.toString(earliestOffset));
+        offsetDetails.put("endOffset", Long.toString(latestOffset));
+        offsetDetails.put("lag", Long.toString(lag));
         consumerGroupOffsetList.add(offsetDetails);
       }
       return consumerGroupOffsetList;
