ee5999c5fbc2f7b516444c78800ac17acf4bdf3a
Mirjam Aulbach <mirjam.aulbach@aiven.io>
2022-11-10 15:08:17 +0100

    Avoid open handle in jest due render with query.
    see: https://tanstack.com/query/v4/docs/guides/testing#set-cachetime-to-infinity-with-jest

--- coral/src/services/test-utils/render-helpers.tsx
+++ coral/src/services/test-utils/render-helpers.tsx
@@ -8,6 +8,7 @@ function withQueryClient({ children }: { children: ReactElement }) {
     defaultOptions: {
       queries: {
         retry: false,
+        cacheTime: Infinity,
       },
     },
   });
