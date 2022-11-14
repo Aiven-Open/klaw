6a15b3dad0435360c56b4ed19517e586aa25675a
Mirjam Aulbach <mirjam.aulbach@aiven.io>
2022-11-14 16:37:33 +0100

    Use Flexbox to create layout of Topics page.

--- coral/src/app/pages/topics/index.tsx
+++ coral/src/app/pages/topics/index.tsx
@@ -1,16 +1,15 @@
 import Layout from "src/app/layout/Layout";
 import BrowseTopics from "src/app/features/topics/BrowseTopics";
-import { PageHeader } from "@aivenio/design-system";
+import { Flexbox, PageHeader } from "@aivenio/design-system";
 
 const Topics = () => {
   return (
     <Layout>
-      <>
+      <Flexbox direction={"column"} rowGap={"l2"}>
         <PageHeader title={"Browse all topics"} />
-        <div style={{ paddingTop: "30px" }}>
-          <BrowseTopics />
-        </div>
-      </>
+
+        <BrowseTopics />
+      </Flexbox>
     </Layout>
   );
 };
