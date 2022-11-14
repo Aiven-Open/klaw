aff595ce30a965c65b82ad367818d7be7b55919c
Mirjam Aulbach <mirjam.aulbach@aiven.io>
2022-11-14 13:37:07 +0100

    Use PageHeader component.

--- coral/src/app/pages/topics/index.tsx
+++ coral/src/app/pages/topics/index.tsx
@@ -1,14 +1,12 @@
 import Layout from "src/app/layout/Layout";
 import { BrowseTopics } from "src/app/features/topics/BrowseTopics";
-import { Typography } from "@aivenio/design-system";
+import { PageHeader } from "@aivenio/design-system";
 
 const Topics = () => {
   return (
     <Layout>
       <>
-        <Typography.Heading htmlTag={"h1"}>
-          Browse all topics
-        </Typography.Heading>
+        <PageHeader title={"Browse all topics"} />
         <div style={{ paddingTop: "30px" }}>
           <BrowseTopics />
         </div>
