26de3a217a9c6468f40aca76db2de97d90ba83cf
Mirjam Aulbach <mirjam.aulbach@aiven.io>
2022-11-16 16:01:10 +0100

    Add select-env feature in topics.
    - no real values and functionality yet.

---
+++ coral/src/app/features/topics/select-env/components/SelectEnv.tsx
@@ -0,0 +1,7 @@
+import { Select } from "@aivenio/design-system";
+
+function SelectEnv() {
+  return <Select labelText="Kafka Environment" options={["one", "two"]} />;
+}
+
+export default SelectEnv;
