e1c680ebcc21260409ad108a11c037587596ac72
Mirjam Aulbach <mirjam.aulbach@aiven.io>
2022-11-17 15:27:46 +0100

    Add tests for useGetEnvs hook.

---
+++ coral/src/app/features/topics/select-env/hooks/useGetEnvs.test.tsx
@@ -0,0 +1,87 @@
+import { QueryClient, QueryClientProvider } from "@tanstack/react-query";
+import { renderHook, waitFor } from "@testing-library/react";
+import { useGetEnvs } from "src/app/features/topics/select-env/hooks/useGetEnvs";
+import { ReactElement } from "react";
+import { server } from "src/services/api-mocks/server";
+import { mockGetEnvs } from "src/domain/topics/topics-api.msw";
+import { TopicEnv } from "src/domain/topics";
+
+const queryClient = new QueryClient({
+  defaultOptions: {
+    queries: {
+      retry: false,
+      cacheTime: Infinity,
+    },
+  },
+});
+
+const wrapper = ({ children }: { children: ReactElement }) => (
+  <QueryClientProvider client={queryClient}>{children}</QueryClientProvider>
+);
+
+describe("useGetEnvs", () => {
+  const originalConsoleError = console.error;
+
+  beforeAll(() => {
+    server.listen();
+  });
+
+  afterEach(() => {
+    server.resetHandlers();
+  });
+
+  afterAll(() => {
+    console.error = originalConsoleError;
+    server.close();
+  });
+
+  describe("handles loading and error state", () => {
+    it("returns a loading state before starting to fetch data", async () => {
+      mockGetEnvs({
+        mswInstance: server,
+      });
+
+      const { result } = await renderHook(() => useGetEnvs(), {
+        wrapper,
+      });
+      expect(result.current.isLoading).toBe(true);
+
+      await waitFor(() => {
+        expect(result.current.isLoading).toBe(false);
+      });
+    });
+
+    it("returns an error when request fails", async () => {
+      console.error = jest.fn();
+
+      mockGetEnvs({ mswInstance: server, scenario: "error" });
+
+      const { result } = await renderHook(() => useGetEnvs(), {
+        wrapper,
+      });
+
+      await waitFor(() => {
+        expect(result.current.isError).toBe(true);
+      });
+      expect(console.error).toHaveBeenCalledTimes(1);
+    });
+  });
+
+  describe("handles successful response", () => {
+    it("returns a list of environments", async () => {
+      mockGetEnvs({
+        mswInstance: server,
+      });
+
+      const { result } = await renderHook(() => useGetEnvs(), {
+        wrapper,
+      });
+
+      await waitFor(() => {
+        expect(result.current.isSuccess).toBe(true);
+      });
+
+      expect(result.current.data).toEqual([TopicEnv.DEV, TopicEnv.TST]);
+    });
+  });
+});
