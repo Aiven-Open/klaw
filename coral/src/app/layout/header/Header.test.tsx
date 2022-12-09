258c06d2ff99640fb94499f58633503ae3c1085b
Mirjam Aulbach <mirjam.aulbach@aiven.io>
2022-12-09 09:56:23 +0100

    Extend test header for keyboard usability.
    Signed-off-by: Mirjam Aulbach <mirjam.aulbach@aiven.io>

--- coral/src/app/layout/header/Header.test.tsx
+++ coral/src/app/layout/header/Header.test.tsx
@@ -1,5 +1,9 @@
 import Header from "src/app/layout/header/Header";
 import { cleanup, screen, render, within } from "@testing-library/react";
+import {
+  tabThroughBackward,
+  tabThroughForward,
+} from "src/services/test-utils/tabbing";
 
 // mock out svgs to avoid clutter
 jest.mock("@aivenio/design-system", () => {
@@ -23,46 +27,104 @@ const quickLinksNavItems = [
 ];
 
 describe("Header.tsx", () => {
-  beforeAll(() => {
-    render(<Header />);
-  });
+  describe("shows all necessary elements", () => {
+    beforeAll(() => {
+      render(<Header />);
+    });
 
-  afterAll(cleanup);
+    afterAll(cleanup);
 
-  it("renders a header element", () => {
-    const heading = screen.getByRole("banner");
+    it("renders a header element", () => {
+      const heading = screen.getByRole("banner");
 
-    expect(heading).toBeVisible();
-  });
+      expect(heading).toBeVisible();
+    });
 
-  it("renders a link to the Klaw hompeage", () => {
-    const link = screen.getByRole("link", { name: "Klaw homepage" });
+    it("renders a link to the Klaw hompeage", () => {
+      const link = screen.getByRole("link", { name: "Klaw homepage" });
 
-    expect(link).toBeVisible();
-  });
+      expect(link).toBeVisible();
+    });
 
-  it("renders the Klaw logo inside link hidden from assistive technology", () => {
-    const link = screen.getByRole("link", { name: "Klaw homepage" });
-    const logo = link.querySelector("img");
+    it("renders the Klaw logo inside link hidden from assistive technology", () => {
+      const link = screen.getByRole("link", { name: "Klaw homepage" });
+      const logo = link.querySelector("img");
 
-    expect(logo).toHaveAttribute("src", "/klaw_logo.png");
-    expect(logo).toHaveAttribute("aria-hidden", "true");
-  });
+      expect(logo).toHaveAttribute("src", "/klaw_logo.png");
+      expect(logo).toHaveAttribute("aria-hidden", "true");
+    });
 
-  quickLinksNavItems.forEach((item) => {
-    it(`renders a link to ${item}`, () => {
+    quickLinksNavItems.forEach((item) => {
+      it(`renders a link to ${item}`, () => {
+        const nav = screen.getByRole("navigation", { name: "Quick links" });
+        const link = within(nav).getByRole("link", { name: item.name });
+
+        expect(link).toBeEnabled();
+        expect(link).toHaveAttribute("href", item.linkTo);
+      });
+    });
+
+    it("renders all links in the header menu", () => {
       const nav = screen.getByRole("navigation", { name: "Quick links" });
-      const link = within(nav).getByRole("link", { name: item.name });
+      const links = within(nav).getAllByRole("link");
 
-      expect(link).toBeEnabled();
-      expect(link).toHaveAttribute("href", item.linkTo);
+      expect(links).toHaveLength(quickLinksNavItems.length);
     });
   });
 
-  it("renders all links in the header menu", () => {
-    const nav = screen.getByRole("navigation", { name: "Quick links" });
-    const links = within(nav).getAllByRole("link");
+  describe("enables user to navigate with keyboard only", () => {
+    const allHeaderLinks = [
+      "Klaw homepage",
+      ...quickLinksNavItems.map((link) => link.name),
+    ];
 
-    expect(links).toHaveLength(quickLinksNavItems.length);
+    describe("user can navigate through links", () => {
+      beforeEach(() => {
+        render(<Header />);
+        const heading = screen.getByRole("banner");
+        heading.focus();
+      });
+
+      afterEach(cleanup);
+
+      allHeaderLinks.forEach((headerLink, index) => {
+        const numbersOfTabs = index + 1;
+        it(`sets focus on ${headerLink} when user tabs ${numbersOfTabs} times`, async () => {
+          const link = screen.getByRole("link", { name: headerLink });
+          expect(link).not.toHaveFocus();
+
+          await tabThroughForward(numbersOfTabs);
+
+          expect(link).toHaveFocus();
+        });
+      });
+    });
+
+    describe("user can navigate backward through links", () => {
+      beforeEach(() => {
+        render(<Header />);
+        const lastElement = allHeaderLinks[allHeaderLinks.length - 1];
+        const lastNavItem = screen.getByRole("link", {
+          name: lastElement,
+        });
+        lastNavItem.focus();
+      });
+
+      afterEach(cleanup);
+
+      const allHeaderLinksReversed = [...allHeaderLinks].reverse();
+      allHeaderLinksReversed.forEach((headerLink, index) => {
+        const numbersOfTabs = index;
+
+        it(`sets focus on ${headerLink} when user shift+tabs ${numbersOfTabs} times`, async () => {
+          const link = screen.getByRole("link", { name: headerLink });
+          index > 0 && expect(link).not.toHaveFocus();
+
+          await tabThroughBackward(numbersOfTabs);
+
+          expect(link).toHaveFocus();
+        });
+      });
+    });
   });
 });
