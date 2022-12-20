a890ae56145fac9804086c3400d1a9de92eaaa79
Murali Basani <muralidhar.basani@aiven.io>
2022-12-20 09:05:25 +0100

    Remove deprecated WebSecurityConfigurerAdapter (#343)
    * Remove deprecated WebSecurityConfigurerAdapter
    
    Signed-off-by: muralibasani <muralidhar.basani@aiven.io>

--- core/src/main/java/io/aiven/klaw/config/SecurityConfigNoSSO.java
+++ core/src/main/java/io/aiven/klaw/config/SecurityConfigNoSSO.java
@@ -2,66 +2,39 @@ package io.aiven.klaw.config;
 
 import static io.aiven.klaw.model.enums.AuthenticationType.ACTIVE_DIRECTORY;
 import static io.aiven.klaw.model.enums.AuthenticationType.DATABASE;
-import static io.aiven.klaw.model.enums.AuthenticationType.LDAP;
 
 import io.aiven.klaw.auth.KwRequestFilter;
 import io.aiven.klaw.dao.UserInfo;
-import io.aiven.klaw.error.KlawException;
 import java.util.*;
-import javax.naming.directory.Attributes;
 import lombok.extern.slf4j.Slf4j;
 import org.jasypt.util.text.BasicTextEncryptor;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.beans.factory.annotation.Value;
 import org.springframework.boot.autoconfigure.condition.ConditionalOnProperty;
 import org.springframework.context.annotation.Bean;
-import org.springframework.ldap.NamingException;
-import org.springframework.ldap.core.AttributesMapper;
 import org.springframework.ldap.core.LdapTemplate;
 import org.springframework.security.authentication.AuthenticationManager;
 import org.springframework.security.authentication.AuthenticationProvider;
 import org.springframework.security.authentication.ProviderManager;
-import org.springframework.security.config.annotation.authentication.builders.AuthenticationManagerBuilder;
+import org.springframework.security.config.annotation.authentication.configuration.AuthenticationConfiguration;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
-import org.springframework.security.config.annotation.web.configuration.WebSecurityConfigurerAdapter;
-import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
 import org.springframework.security.crypto.factory.PasswordEncoderFactories;
 import org.springframework.security.crypto.password.PasswordEncoder;
 import org.springframework.security.ldap.authentication.ad.ActiveDirectoryLdapAuthenticationProvider;
 import org.springframework.security.provisioning.InMemoryUserDetailsManager;
+import org.springframework.security.web.SecurityFilterChain;
 import org.springframework.security.web.authentication.UsernamePasswordAuthenticationFilter;
 
 @EnableWebSecurity
 @ConditionalOnProperty(name = "klaw.enable.sso", havingValue = "false")
 @Slf4j
-public class SecurityConfigNoSSO extends WebSecurityConfigurerAdapter {
+public class SecurityConfigNoSSO {
 
   @Autowired private ManageDatabase manageTopics;
 
   @Value("${klaw.login.authentication.type}")
   private String authenticationType;
-
-  @Value("${spring.ldap.base:@null}")
-  private String ldapBase;
-
-  @Value("${spring.ldap.url:@null}")
-  private String ldapUrls;
-
-  @Value("${spring.ldap.userDnPatterns:@null}")
-  private String userDnPatterns;
-
-  @Value("${spring.ldap.passwordAttribute:@null}")
-  private String passwordAttribute;
-
-  @Value("${spring.ldap.managerusername:@null}")
-  private String managerUserName;
-
-  @Value("${spring.ldap.managerpassword:@null}")
-  private String managerPwd;
-
-  @Value("${klaw.ldap.password.encryption:@null}")
-  private String encryptionType;
 
   @Value("${spring.ad.domain:}")
   private String adDomain;
@@ -89,8 +62,8 @@ public class SecurityConfigNoSSO extends WebSecurityConfigurerAdapter {
     // ((ConfigurableApplicationContext) contextApp).close();
   }
 
-  @Override
-  protected void configure(HttpSecurity http) throws Exception {
+  @Bean
+  public SecurityFilterChain filterChain(HttpSecurity http) throws Exception {
     http.csrf()
         .disable()
         .authorizeRequests()
@@ -110,27 +83,12 @@ public class SecurityConfigNoSSO extends WebSecurityConfigurerAdapter {
 
     //         Add a filter to validate the username/pwd with every request
     http.addFilterBefore(kwRequestFilterup, UsernamePasswordAuthenticationFilter.class);
+    return http.build();
   }
 
-  @Override
-  public void configure(AuthenticationManagerBuilder auth) throws Exception {
-    if (authenticationType != null && authenticationType.equals(DATABASE.value)) {
-      dbAuthentication(auth);
-    } else if (authenticationType != null && authenticationType.equals(LDAP.value)) {
-      ldapAuthentication(auth);
-    } else if (authenticationType != null && authenticationType.equals(ACTIVE_DIRECTORY.value)) {
-      auth.authenticationProvider(activeDirectoryLdapAuthenticationProvider())
-          .userDetailsService(userDetailsService());
-    } else {
-      log.info("No proper authentication set. Possible values db/ldap/ad");
-      shutdownApp();
-    }
-  }
-
-  @Override
   @Bean
-  public AuthenticationManager authenticationManagerBean() throws Exception {
-    AuthenticationManager result;
+  public AuthenticationManager authenticationManager(
+      AuthenticationConfiguration authenticationConfiguration) throws Exception {
     if (authenticationType != null && authenticationType.equals(ACTIVE_DIRECTORY.value)) {
       log.info("AD authentication configured.");
       log.info(
@@ -139,13 +97,10 @@ public class SecurityConfigNoSSO extends WebSecurityConfigurerAdapter {
           adDomain,
           adRootDn,
           adFilter);
-      result =
-          new ProviderManager(
-              Collections.singletonList(activeDirectoryLdapAuthenticationProvider()));
-    } else {
-      return super.authenticationManagerBean();
+      return new ProviderManager(
+          Collections.singletonList(activeDirectoryLdapAuthenticationProvider()));
     }
-    return result;
+    return authenticationConfiguration.getAuthenticationManager();
   }
 
   public AuthenticationProvider activeDirectoryLdapAuthenticationProvider() {
@@ -160,46 +115,13 @@ public class SecurityConfigNoSSO extends WebSecurityConfigurerAdapter {
     return provider;
   }
 
-  private void ldapAuthentication(AuthenticationManagerBuilder auth) throws Exception {
-    try {
-      log.info("Ldap authentication configured.");
-      if (!checkLdapConnectivity()) {
-        throw new KlawException("Cannot connect to Ldap !!");
-      }
-
-      if (encryptionType != null && encryptionType.equals("bcrypt")) {
-        auth.ldapAuthentication()
-            .userSearchBase(ldapBase)
-            .userDnPatterns(userDnPatterns)
-            .contextSource()
-            .url(ldapUrls)
-            .managerDn(managerUserName)
-            .managerPassword(managerPwd)
-            .and()
-            .passwordCompare()
-            .passwordEncoder(new BCryptPasswordEncoder())
-            .passwordAttribute(passwordAttribute);
-      }
-
-    } catch (Exception e) {
-      log.error("Cannot connect to Ldap !! ", e);
-      shutdownApp();
-      throw new Exception("Cannot connect to Ldap !!");
-    }
-  }
-
-  private void dbAuthentication(AuthenticationManagerBuilder auth) throws Exception {
-    log.info("Db authentication configured.");
-    auth.userDetailsService(inMemoryUserDetailsManager());
-  }
-
   @Bean
   public InMemoryUserDetailsManager inMemoryUserDetailsManager() throws Exception {
     final Properties globalUsers = new Properties();
     if (authenticationType != null && authenticationType.equals(DATABASE.value)) {
-      log.info("Loading all users !!");
+      log.info("Db authentication configured.");
+      log.debug("Loading all users !!");
       List<UserInfo> users;
-
       try {
         users = manageTopics.selectAllUsersInfo();
       } catch (Exception e) {
@@ -214,54 +136,30 @@ public class SecurityConfigNoSSO extends WebSecurityConfigurerAdapter {
       }
 
       Iterator<UserInfo> iter = users.iterator();
-      UserInfo userInfo;
       PasswordEncoder encoder = PasswordEncoderFactories.createDelegatingPasswordEncoder();
-      while (iter.hasNext()) {
-        userInfo = iter.next();
-        try {
-          String secPwd = userInfo.getPwd();
-          if (secPwd != null && secPwd.equals("")) {
-            secPwd = "gfGF%64GFDd766hfgfHFD$%#453";
-          } else {
-            secPwd = decodePwd(secPwd);
-          }
-          globalUsers.put(
-              userInfo.getUsername(),
-              encoder.encode(secPwd) + "," + userInfo.getRole() + ",enabled");
-        } catch (Exception e) {
-          log.error("Error : User not loaded {}. Check password.", userInfo.getUsername(), e);
-        }
-      }
+      loadAllUsers(globalUsers, iter, encoder);
     }
     return new InMemoryUserDetailsManager(globalUsers);
   }
 
-  private boolean checkLdapConnectivity() {
-
-    try {
-      log.info("Checking Ldap connectivity.");
-      ldapTemplate.search(
-          ldapBase,
-          "(objectclass=top)",
-          new AttributesMapper() {
-            public Object mapFromAttributes(Attributes attrs) throws NamingException {
-              try {
-                return attrs.get("cn").get();
-              } catch (javax.naming.NamingException e) {
-                log.error("Ldap connectivity error : ", e);
-                return null;
-              }
-            }
-          });
-    } catch (org.springframework.ldap.NameNotFoundException e) {
-      log.error("Exception:", e);
-      return true;
-    } catch (Exception e) {
-      log.error("Exception:", e);
-      return false;
+  private void loadAllUsers(
+      Properties globalUsers, Iterator<UserInfo> iter, PasswordEncoder encoder) {
+    UserInfo userInfo;
+    while (iter.hasNext()) {
+      userInfo = iter.next();
+      try {
+        String secPwd = userInfo.getPwd();
+        if (secPwd != null && secPwd.equals("")) {
+          secPwd = "gfGF%64GFDd766hfgfHFD$%#453";
+        } else {
+          secPwd = decodePwd(secPwd);
+        }
+        globalUsers.put(
+            userInfo.getUsername(), encoder.encode(secPwd) + "," + userInfo.getRole() + ",enabled");
+      } catch (Exception e) {
+        log.error("Error : User not loaded {}. Check password.", userInfo.getUsername(), e);
+      }
     }
-
-    return true;
   }
 
   private String decodePwd(String pwd) {
