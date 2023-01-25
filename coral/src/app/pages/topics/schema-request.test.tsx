2e0d5ade0c12761fb63aea5b10362605b88b31c8
Mirjam Aulbach <mirjam.aulbach@aiven.io>
2023-01-25 17:57:20 +0100

    Mock out api calls in test for page.
    Signed-off-by: Mirjam Aulbach <mirjam.aulbach@aiven.io>

--- coral/src/app/pages/topics/schema-request.test.tsx
+++ coral/src/app/pages/topics/schema-request.test.tsx
@@ -4,6 +4,8 @@ import { getQueryClientForTests } from "src/services/test-utils/query-client-tes
 import { render } from "@testing-library/react";
 import { QueryClientProvider } from "@tanstack/react-query";
 import { MemoryRouter, Routes, Route } from "react-router-dom";
+import { getSchemaRegistryEnvironments } from "src/domain/environment";
+import { createSchemaRequest } from "src/domain/schema-request";
 
 // mock out svgs to avoid clutter
 jest.mock("@aivenio/aquarium", () => {
@@ -14,11 +16,24 @@ jest.mock("@aivenio/aquarium", () => {
   };
 });
 
+jest.mock("src/domain/schema-request/schema-request-api.ts");
+jest.mock("src/domain/environment/environment-api.ts");
+
+const mockGetSchemaRegistryEnvironments =
+  getSchemaRegistryEnvironments as jest.MockedFunction<
+    typeof getSchemaRegistryEnvironments
+  >;
+const mockCreateSchemaRequest = createSchemaRequest as jest.MockedFunction<
+  typeof createSchemaRequest
+>;
+
 describe("SchemaRequest", () => {
   describe("renders the page to request a new schema for a topic", () => {
     const topicName = "my-awesome-topic";
 
     beforeAll(() => {
+      mockGetSchemaRegistryEnvironments.mockResolvedValue([]);
+      mockCreateSchemaRequest.mockImplementation(jest.fn());
       // @TODO if we decide to go with this kind of dynamic routes,
       // this should be enabled by customRender!
       const queryClient = getQueryClientForTests();
