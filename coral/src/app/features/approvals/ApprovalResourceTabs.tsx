6bd2fe691e0596394a326266b52f0ab4821a010b
Samuli Suortti <smulis@aiven.io>
2023-01-27 11:50:23 +0200

    feat(coral): improve approvals tab screen reader values
    Signed-off-by: Samuli Suortti <smulis@aiven.io>

--- coral/src/app/features/approvals/ApprovalResourceTabs.tsx
+++ coral/src/app/features/approvals/ApprovalResourceTabs.tsx
@@ -12,21 +12,49 @@ type Props = {
 
 function ApprovalResourceTabs({ currentTab }: Props) {
   const navigate = useNavigate();
+
+  const numberOfPendingTopicApprovals = 99;
+  const numberOfPendingAclApprovals = 33;
+  const numberOfPendingSchemaApprovals = undefined;
+  const numberOfPendingConnectorApprovals = 0;
   return (
     <Tabs
       value={currentTab}
       onChange={(resourceTypeId) => navigateToTab(navigate, resourceTypeId)}
     >
-      <Tabs.Tab title="Topics" value={ApprovalsTabEnum.TOPICS} badge={99}>
+      <Tabs.Tab
+        title="Topics"
+        value={ApprovalsTabEnum.TOPICS}
+        badge={getBadgeValue(numberOfPendingTopicApprovals)}
+        aria-label={getTabAriaLabel("Topics", numberOfPendingTopicApprovals)}
+      >
         {currentTab === ApprovalsTabEnum.TOPICS && <Outlet />}
       </Tabs.Tab>
-      <Tabs.Tab title="ACLs" value={ApprovalsTabEnum.ACLS} badge={99}>
+      <Tabs.Tab
+        title="ACLs"
+        value={ApprovalsTabEnum.ACLS}
+        badge={getBadgeValue(numberOfPendingAclApprovals)}
+        aria-label={getTabAriaLabel("ACLs", numberOfPendingAclApprovals)}
+      >
         {currentTab === ApprovalsTabEnum.ACLS && <Outlet />}
       </Tabs.Tab>
-      <Tabs.Tab title="Schemas" value={ApprovalsTabEnum.SCHEMAS} badge={99}>
+      <Tabs.Tab
+        title="Schemas"
+        value={ApprovalsTabEnum.SCHEMAS}
+        badge={getBadgeValue(numberOfPendingSchemaApprovals)}
+        aria-label={getTabAriaLabel("Schemas", numberOfPendingSchemaApprovals)}
+      >
         {currentTab === ApprovalsTabEnum.SCHEMAS && <Outlet />}
       </Tabs.Tab>
-      <Tabs.Tab title="Connectors" value={ApprovalsTabEnum.CONNECTORS}>
+      <Tabs.Tab
+        title="Connectors"
+        value={ApprovalsTabEnum.CONNECTORS}
+        badge={getBadgeValue(numberOfPendingSchemaApprovals)}
+        aria-label={getTabAriaLabel(
+          "Connectors",
+          numberOfPendingConnectorApprovals
+        )}
+      >
         {currentTab === ApprovalsTabEnum.CONNECTORS && <Outlet />}
       </Tabs.Tab>
     </Tabs>
@@ -41,6 +69,25 @@ function ApprovalResourceTabs({ currentTab }: Props) {
         replace: true,
       });
     }
+  }
+
+  function getTabAriaLabel(
+    title: string,
+    pendingApprovals: number | undefined
+  ): string {
+    if (typeof pendingApprovals === "number" && pendingApprovals > 0) {
+      return `${title}, ${pendingApprovals} approvals waiting`;
+    }
+    return `${title}, no pending approvals`;
+  }
+
+  function getBadgeValue(
+    pendingApprovals: number | undefined
+  ): number | undefined {
+    if (typeof pendingApprovals === "number" && pendingApprovals > 0) {
+      return pendingApprovals;
+    }
+    return undefined;
   }
 }
 
