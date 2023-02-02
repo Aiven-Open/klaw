8a4143eca6df56721fe458be4467594fe6f6af6e
Samuli Suortti <smulis@aiven.io>
2023-02-02 16:38:26 +0200

    refactor(coral): add comment to explain why /getAuth is used for notifications
    Signed-off-by: Samuli Suortti <smulis@aiven.io>

--- coral/src/domain/notification/notification-api.ts
+++ coral/src/domain/notification/notification-api.ts
@@ -4,6 +4,10 @@ import { transformGetNotificationCounts } from "src/domain/notification/notifica
 import { Notifications } from "src/domain/notification/notification-types";
 
 function getNotificationCounts(): Promise<Notifications> {
+  // Using the GET /getAuth endpoint is not the most obvious API to
+  // fetch notification counts. However, this endpoint is used for the
+  // current Angular application as well, so lets use it in Coral as well
+  // before we get special purpose API endpoints.
   return api
     .get<KlawApiResponse<"getAuth">>("/getAuth")
     .then(transformGetNotificationCounts);
