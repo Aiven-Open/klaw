91d8e13aca47f71c5d4a067f1121ba4b9be7f1aa
Mirjam Aulbach <mirjam.aulbach@aiven.io>
2023-02-06 20:43:18 +0100

    Add modal component based on DS.
    Signed-off-by: Mirjam Aulbach <mirjam.aulbach@aiven.io>

---
+++ coral/src/app/components/Modal.test.tsx
@@ -0,0 +1,225 @@
+import { cleanup, render, screen } from "@testing-library/react";
+import { Modal } from "src/app/components/Modal";
+import userEvent from "@testing-library/user-event";
+
+describe("Modal.tsx", () => {
+  const testTitle = "Modal title";
+  const mockChildren = <div>This is child content</div>;
+  const mockPrimary = {
+    text: "Primary action 1",
+    onClick: jest.fn(),
+  };
+
+  describe("renders a default modal with all necessary elements", () => {
+    afterEach(jest.clearAllMocks);
+
+    describe("renders all necessary elements", () => {
+      beforeAll(() => {
+        render(
+          <>
+            <div id={"root"}></div>
+            <Modal title={testTitle} primaryAction={mockPrimary}>
+              {mockChildren}
+            </Modal>
+          </>
+        );
+      });
+
+      afterAll(cleanup);
+
+      it("sets the app root to aria hidden true", () => {
+        const root = document.getElementById("root");
+
+        expect(root).toHaveAttribute("aria-hidden", "true");
+      });
+
+      it("sets the tabindex of app root to -1 so users can only access modal", () => {
+        const root = document.getElementById("root");
+
+        expect(root).toHaveAttribute("tabindex", "-1");
+      });
+
+      it("shows a dialog", () => {
+        const dialog = screen.getByRole("dialog");
+
+        expect(dialog).toBeVisible();
+      });
+
+      it("marks the modal as a aria modal to help assistive technology", () => {
+        const dialog = screen.getByRole("dialog");
+
+        expect(dialog).toHaveAttribute("aria-modal", "true");
+      });
+
+      it("shows a given title", () => {
+        const title = screen.getByRole("heading", { name: testTitle });
+
+        expect(title).toBeVisible();
+      });
+
+      it("moves focus to the title in the dialog", () => {
+        const title = screen.getByRole("heading", { name: testTitle });
+
+        expect(title).toHaveFocus();
+      });
+
+      it("shows the given content", () => {
+        const text = screen.getByText("This is child content");
+
+        expect(text).toBeVisible();
+      });
+
+      it("shows a button with a given primary action", () => {
+        const button = screen.getByRole("button", { name: mockPrimary.text });
+
+        expect(button).toBeEnabled();
+      });
+
+      it("shows no other buttons for secondary action or close", () => {
+        const buttons = screen.getAllByRole("button");
+
+        expect(buttons).toHaveLength(1);
+        expect(buttons[0]).toHaveTextContent(mockPrimary.text);
+      });
+    });
+
+    describe("handles user input", () => {
+      beforeEach(() => {
+        render(
+          <>
+            <div id={"root"}></div>
+            <Modal title={testTitle} primaryAction={mockPrimary}>
+              {mockChildren}
+            </Modal>
+          </>
+        );
+      });
+
+      afterEach(cleanup);
+
+      it("enables user to click the secondary action", async () => {
+        const button = screen.getByRole("button", { name: mockPrimary.text });
+
+        await userEvent.click(button);
+        expect(mockPrimary.onClick).toHaveBeenCalled();
+      });
+
+      it("enables user access and trigger the primary action with keyboard", async () => {
+        await userEvent.tab();
+        const button = screen.getByRole("button", { name: mockPrimary.text });
+
+        expect(button).toHaveFocus();
+
+        await userEvent.keyboard("{Enter}");
+        expect(mockPrimary.onClick).toHaveBeenCalled();
+      });
+    });
+  });
+
+  describe("renders additional elements dependent on props", () => {
+    describe("handles a secondary action", () => {
+      const mockSecondary = {
+        text: "this is the secondary action",
+        onClick: jest.fn(),
+      };
+
+      beforeEach(() => {
+        render(
+          <Modal
+            title={testTitle}
+            primaryAction={mockPrimary}
+            secondaryAction={mockSecondary}
+          >
+            {mockChildren}
+          </Modal>
+        );
+      });
+
+      afterEach(cleanup);
+
+      it("shows the secondary action as a button", () => {
+        const button = screen.getByRole("button", { name: mockSecondary.text });
+
+        expect(button).toBeEnabled();
+      });
+
+      it("shows two buttons when secodary and primary action are given", () => {
+        const buttons = screen.getAllByRole("button");
+
+        expect(buttons).toHaveLength(2);
+      });
+
+      it("enables user to click the secondary action", async () => {
+        const button = screen.getByRole("button", { name: mockSecondary.text });
+
+        await userEvent.click(button);
+        expect(mockSecondary.onClick).toHaveBeenCalled();
+      });
+
+      it("enables user access and trigger the secondary action with keyboard", async () => {
+        const buttonPrimary = screen.getByRole("button", {
+          name: mockPrimary.text,
+        });
+        const buttonSecondary = screen.getByRole("button", {
+          name: mockSecondary.text,
+        });
+        await userEvent.tab();
+        expect(buttonSecondary).toHaveFocus();
+        await userEvent.tab();
+        expect(buttonPrimary).toHaveFocus();
+
+        await userEvent.tab({ shift: true });
+        expect(buttonSecondary).toHaveFocus();
+
+        await userEvent.keyboard("{Enter}");
+        expect(mockSecondary.onClick).toHaveBeenCalled();
+      });
+    });
+
+    describe("adds option to close the modal without triggering an action", () => {
+      const mockClose = jest.fn();
+
+      beforeEach(() => {
+        render(
+          <Modal
+            title={testTitle}
+            primaryAction={mockPrimary}
+            close={mockClose}
+          >
+            {mockChildren}
+          </Modal>
+        );
+      });
+
+      afterEach(cleanup);
+
+      it("shows a close button", () => {
+        const button = screen.getByRole("button", { name: "Close modal" });
+
+        expect(button).toBeEnabled();
+      });
+
+      it("enables user to click close the button with the mouse", async () => {
+        const button = screen.getByRole("button", { name: "Close modal" });
+
+        await userEvent.click(button);
+        expect(mockClose).toHaveBeenCalled();
+      });
+
+      it("enables user access and trigger close with the keyboard", async () => {
+        const closeButton = screen.getByRole("button", { name: "Close modal" });
+        await userEvent.tab();
+        expect(closeButton).toHaveFocus();
+
+        await userEvent.click(closeButton);
+        expect(mockClose).toHaveBeenCalled();
+      });
+
+      it("enables user to close the modal with pressing the Escape key", async () => {
+        await userEvent.keyboard("{Escape}");
+
+        expect(mockClose).toHaveBeenCalled();
+      });
+    });
+  });
+});
---
+++ coral/src/app/components/Modal.tsx
@@ -0,0 +1,181 @@
+import { createPortal } from "react-dom";
+import { ReactElement, useEffect } from "react";
+import { Box, Button, IconButton, Typography } from "@aivenio/aquarium";
+import cross from "@aivenio/aquarium/icons/cross";
+import classes from "src/app/components/Modal.module.css";
+
+type ModalAction = {
+  text: string;
+  onClick: () => void;
+};
+type ModalProps = {
+  title: string;
+  subtitle?: string;
+  close?: () => void;
+  primaryAction: ModalAction;
+  secondaryAction?: ModalAction;
+  children: ReactElement;
+};
+
+function Modal(props: ModalProps) {
+  const { close, primaryAction, secondaryAction, children, title, subtitle } =
+    props;
+
+  function setFocus(appRoot: HTMLElement, modal: HTMLElement) {
+    appRoot.setAttribute("aria-hidden", "true");
+    appRoot.setAttribute("tabindex", "-1");
+    appRoot.setAttribute("inert", "true");
+    modal.setAttribute("tabindex", "0");
+    modal.focus();
+  }
+
+  function removeFocus(appRoot: HTMLElement, modal: HTMLElement) {
+    appRoot.removeAttribute("aria-hidden");
+    appRoot.removeAttribute("tabindex");
+    modal.removeAttribute("tabindex");
+  }
+
+  useEffect(() => {
+    const appRoot = document.getElementById("root");
+    const modalFocus = document.getElementById("modal-focus");
+    if (appRoot && modalFocus) {
+      setFocus(appRoot, modalFocus);
+      return () => {
+        removeFocus(appRoot, modalFocus);
+      };
+    }
+  }, []);
+
+  useEffect(() => {
+    if (close) {
+      const handleEscape = (event: KeyboardEvent) => {
+        if (event.key !== "Escape") return;
+        close();
+      };
+
+      window.addEventListener("keydown", handleEscape);
+
+      return () => {
+        window.removeEventListener("keydown", handleEscape);
+      };
+    }
+  }, []);
+
+  useEffect(() => {
+    const isFirefox = window.navigator.userAgent.includes("Firefox");
+
+    if (isFirefox) {
+      const focusableElements =
+        document.querySelectorAll<HTMLElement>("[data-focusable]");
+      const firstElement = focusableElements[0];
+      const lastElement = focusableElements[focusableElements.length - 1];
+
+      const handleTab = (event: KeyboardEvent) => {
+        if (event.key !== "Tab") return;
+
+        if (!event.shiftKey && document.activeElement === lastElement) {
+          event.preventDefault();
+          firstElement.focus();
+        }
+
+        if (event.shiftKey && document.activeElement === firstElement) {
+          event.preventDefault();
+          lastElement.focus();
+        }
+      };
+
+      window.addEventListener("keydown", handleTab);
+
+      return () => {
+        window.removeEventListener("keydown", handleTab);
+      };
+    }
+  }, []);
+
+  return (
+    <>
+      {createPortal(
+        <Box
+          display={"flex"}
+          alignItems={"center"}
+          justifyContent={"center"}
+          className={classes.modalWrapper}
+        >
+          <Box
+            component={"dialog"}
+            aria-modal={"true"}
+            paddingX={"l3"}
+            paddingY={"l2"}
+            display={"flex"}
+            flexDirection={"column"}
+            borderRadius={"4px"}
+            backgroundColor={"white"}
+            width={"6/12"}
+            // value is arbitrary, it should prevent buttons overflowing
+            // the modal in a very small screen
+            //eslint-disable-next-line @typescript-eslint/ban-ts-comment
+            //@ts-ignore*
+            minWidth={"600px"}
+          >
+            <Box
+              display={"flex"}
+              justifyContent={"space-between"}
+              alignItems={"start"}
+              paddingBottom={"l2"}
+            >
+              <div>
+                <Typography.Subheading
+                  htmlTag={"h1"}
+                  id={"modal-focus"}
+                  data-focusable
+                >
+                  {title}
+                </Typography.Subheading>
+                {subtitle && (
+                  <Typography.SmallText htmlTag={"h2"} color={"grey-60"}>
+                    {subtitle}
+                  </Typography.SmallText>
+                )}
+              </div>
+              {close && (
+                <IconButton
+                  aria-label="Close modal"
+                  icon={cross}
+                  onClick={close}
+                  data-focusable
+                />
+              )}
+            </Box>
+            <Box className={classes.modalTextBlock}>{children}</Box>
+            <Box
+              paddingTop={"l2"}
+              display={"flex"}
+              justifyContent={"end"}
+              colGap={"l1"}
+            >
+              {secondaryAction && (
+                <Button
+                  kind={"secondary"}
+                  onClick={secondaryAction.onClick}
+                  data-focusable
+                >
+                  {secondaryAction.text}
+                </Button>
+              )}
+              <Button
+                kind={"primary"}
+                onClick={primaryAction.onClick}
+                data-focusable
+              >
+                {primaryAction.text}
+              </Button>
+            </Box>
+          </Box>
+        </Box>,
+        document.body
+      )}
+    </>
+  );
+}
+
+export { Modal };
