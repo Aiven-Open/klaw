04069c60a5b0b6e037f3b3374e4484d69cd3502c
Mathieu Anderson <mathieu.anderson@aiven.io>
2023-02-14 10:55:56 +0100

    feat(coral): Add tests
    Signed-off-by: Mathieu Anderson <mathieu.anderson@aiven.io>

---
+++ coral/src/app/features/approvals/components/RequestDetailsModal.test.tsx
@@ -0,0 +1,74 @@
+import { cleanup, render, screen } from "@testing-library/react";
+import userEvent from "@testing-library/user-event";
+import RequestDetailsModal from "src/app/features/approvals/components/RequestDetailsModal";
+
+const baseProps = {
+  onClose: jest.fn(),
+  onApprove: jest.fn(),
+  onReject: jest.fn(),
+};
+
+describe.only("RequestDetailsModal.test", () => {
+  afterEach(cleanup);
+
+  it("renders a Modal with correct elements (isLoading false)", () => {
+    render(
+      <RequestDetailsModal {...baseProps} isLoading={false}>
+        <div>content</div>
+      </RequestDetailsModal>
+    );
+
+    expect(
+      screen.getByRole("heading", { name: "Request details" })
+    ).toBeVisible();
+    expect(screen.getByText("content")).toBeVisible();
+    expect(screen.getByRole("button", { name: "Close modal" })).toBeVisible();
+    expect(screen.getByRole("button", { name: "Close modal" })).toBeVisible();
+    expect(screen.getByRole("button", { name: "Approve" })).toBeEnabled();
+    expect(screen.getByRole("button", { name: "Approve" })).toBeEnabled();
+    expect(screen.getByRole("button", { name: "Reject" })).toBeVisible();
+    expect(screen.getByRole("button", { name: "Reject" })).toBeEnabled();
+  });
+
+  it("renders a Modal with correct elements (isLoading true)", () => {
+    render(
+      <RequestDetailsModal {...baseProps} isLoading={true}>
+        <div>content</div>
+      </RequestDetailsModal>
+    );
+
+    expect(
+      screen.getByRole("heading", { name: "Request details" })
+    ).toBeVisible();
+    expect(screen.getByText("content")).toBeVisible();
+    expect(screen.getByRole("button", { name: "Close modal" })).toBeVisible();
+    expect(screen.getByRole("button", { name: "Close modal" })).toBeVisible();
+    expect(screen.getByRole("button", { name: "Approve" })).toBeVisible();
+    expect(screen.getByRole("button", { name: "Approve" })).toBeDisabled();
+    expect(screen.getByRole("button", { name: "Reject" })).toBeVisible();
+    expect(screen.getByRole("button", { name: "Reject" })).toBeDisabled();
+  });
+
+  it("calls correct functions when clicking buttons", async () => {
+    render(
+      <RequestDetailsModal {...baseProps} isLoading={false}>
+        <div>content</div>
+      </RequestDetailsModal>
+    );
+
+    const { onClose, onApprove, onReject } = baseProps;
+
+    const closeButton = screen.getByRole("button", { name: "Close modal" });
+    const approveButton = screen.getByRole("button", { name: "Approve" });
+    const rejectButton = screen.getByRole("button", { name: "Reject" });
+
+    await userEvent.click(closeButton);
+    expect(onClose).toHaveBeenCalledTimes(1);
+
+    await userEvent.click(approveButton);
+    expect(onApprove).toHaveBeenCalledTimes(1);
+
+    await userEvent.click(rejectButton);
+    expect(onReject).toHaveBeenCalledTimes(1);
+  });
+});
---
+++ coral/src/app/features/approvals/components/RequestRejectModal.test.tsx
@@ -0,0 +1,115 @@
+import { cleanup, render, screen } from "@testing-library/react";
+import userEvent from "@testing-library/user-event";
+import RequestRejectModal from "src/app/features/approvals/components/RequestRejectModal";
+
+const baseProps = {
+  onClose: jest.fn(),
+  onSubmit: jest.fn(),
+  onCancel: jest.fn(),
+};
+
+describe.only("RequestRejectModal.test", () => {
+  afterEach(cleanup);
+
+  it("renders a Modal with correct elements on first load (isLoading false)", () => {
+    render(<RequestRejectModal {...baseProps} isLoading={false} />);
+
+    expect(
+      screen.getByRole("heading", { name: "Reject request" })
+    ).toBeVisible();
+    expect(
+      screen.getByRole("textbox", {
+        name: "Submit a reason to decline the request *",
+      })
+    ).toBeVisible();
+    expect(
+      screen.getByRole("textbox", {
+        name: "Submit a reason to decline the request *",
+      })
+    ).toBeEnabled();
+    expect(screen.getByRole("button", { name: "Close modal" })).toBeVisible();
+    expect(screen.getByRole("button", { name: "Close modal" })).toBeEnabled();
+    expect(screen.getByRole("button", { name: "Cancel" })).toBeVisible();
+    expect(screen.getByRole("button", { name: "Cancel" })).toBeEnabled();
+    expect(
+      screen.getByRole("button", { name: "Reject request" })
+    ).toBeVisible();
+    expect(
+      screen.getByRole("button", { name: "Reject request" })
+    ).toBeDisabled();
+  });
+
+  it("renders a Modal with correct elements (isLoading true)", () => {
+    render(<RequestRejectModal {...baseProps} isLoading={true} />);
+
+    expect(
+      screen.getByRole("heading", { name: "Reject request" })
+    ).toBeVisible();
+    expect(
+      screen.getByRole("textbox", {
+        name: "Submit a reason to decline the request *",
+      })
+    ).toBeVisible();
+    expect(
+      screen.getByRole("textbox", {
+        name: "Submit a reason to decline the request *",
+      })
+    ).toBeDisabled();
+    expect(screen.getByRole("button", { name: "Close modal" })).toBeVisible();
+    expect(screen.getByRole("button", { name: "Close modal" })).toBeEnabled();
+    expect(screen.getByRole("button", { name: "Cancel" })).toBeVisible();
+    expect(screen.getByRole("button", { name: "Cancel" })).toBeDisabled();
+    expect(
+      screen.getByRole("button", { name: "Reject request" })
+    ).toBeVisible();
+    expect(
+      screen.getByRole("button", { name: "Reject request" })
+    ).toBeDisabled();
+  });
+
+  it("calls correct functions when clicking close and cancel buttons", async () => {
+    render(<RequestRejectModal {...baseProps} isLoading={false} />);
+
+    const { onClose, onCancel } = baseProps;
+
+    const closeButton = screen.getByRole("button", { name: "Close modal" });
+    const cancelButton = screen.getByRole("button", { name: "Cancel" });
+
+    await userEvent.click(closeButton);
+    expect(onClose).toHaveBeenCalledTimes(1);
+
+    await userEvent.click(cancelButton);
+    expect(onCancel).toHaveBeenCalledTimes(1);
+  });
+
+  it("calls correct function when clicking reject request button", async () => {
+    render(<RequestRejectModal {...baseProps} isLoading={false} />);
+
+    const { onSubmit } = baseProps;
+
+    const rejectButton = screen.getByRole("button", { name: "Reject request" });
+    const textArea = screen.getByRole("textbox", {
+      name: "Submit a reason to decline the request *",
+    });
+
+    await userEvent.type(textArea, "reason");
+    await userEvent.click(rejectButton);
+    expect(onSubmit).toHaveBeenCalledWith("reason");
+  });
+
+  it("shows error when entering a too long message", async () => {
+    render(<RequestRejectModal {...baseProps} isLoading={false} />);
+    const tooLong =
+      "Quisque commodo aliquam tristique. Class aptent taciti sociosqu ad litora torquent per conubia nostra, per inceptos himenaeos. Sed ornare turpis ac cursus vulputate. Morbi auctor sodales porttitor. Mauris placerat ante id facilisis vehicula. Pellentesque ornare quis massa elementum auctor. Suspendisse potenti. Phasellus dignissim sit amet risus vitae aliquet. Vivamus at dolor vehicula, placerat odio sit amet, imperdiet enim. Donec scelerisque pretium metus ut dignissim. Morbi posuere tortor in cursus porttitor. Maecenas a diam ut urna mattis convallis a vel ligula.";
+
+    const rejectButton = screen.getByRole("button", { name: "Reject request" });
+    const textArea = screen.getByRole("textbox", {
+      name: "Submit a reason to decline the request *",
+    });
+
+    await userEvent.type(textArea, tooLong);
+    expect(rejectButton).toBeDisabled();
+    expect(textArea).toBeInvalid();
+    expect(screen.getByText("Rejection message is too long.")).toBeVisible();
+  });
+});
