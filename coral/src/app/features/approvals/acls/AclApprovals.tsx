01a80df27e2eb7c6b3045bdf27955247f7d78eb9
Mathieu Anderson <mathieu.anderson@aiven.io>
2023-02-15 15:23:53 +0100

    feat(coral): Add details content to RequestDetailsModal
    Signed-off-by: Mathieu Anderson <mathieu.anderson@aiven.io>

--- coral/src/app/features/approvals/acls/AclApprovals.tsx
+++ coral/src/app/features/approvals/acls/AclApprovals.tsx
@@ -3,6 +3,8 @@ import {
   DataTableColumn,
   Flexbox,
   GhostButton,
+  Grid,
+  GridItem,
   Icon,
   NativeSelect,
   SearchInput,
@@ -145,7 +147,7 @@ function AclApprovals() {
     {
       type: "status",
       field: "environmentName",
-      headerName: "Cluster",
+      headerName: "Environment",
       status: ({ environmentName }) => ({
         status: "neutral",
         text: environmentName,
@@ -173,7 +175,10 @@ function AclApprovals() {
     {
       type: "text",
       field: "requesttimestring",
-      headerName: "Date requested",
+      headerName: "Requested on",
+      formatter: (value) => {
+        return `${value} UTC`;
+      },
     },
     {
       // Not having a headerName triggers React error:
@@ -308,6 +313,120 @@ function AclApprovals() {
     </div>,
   ];
 
+  const renderRequestDetails = (request?: AclRequest) => {
+    if (request === undefined) {
+      return <div>Request not found.</div>;
+    }
+
+    const {
+      topictype,
+      environmentName = "Environment not found",
+      topicname,
+      acl_ssl,
+      acl_ip,
+      consumergroup = "Consumer group not found",
+      remarks,
+      requesttimestring = "Request time not found",
+      username = "User not found",
+      requestingTeamName = "Team name not found",
+    } = request;
+
+    const ips = acl_ip || [];
+    const principals = acl_ssl || [];
+
+    const Label = ({ children }: { children: React.ReactNode }) => (
+      <label className="inline-block mb-2 typography-small-strong text-grey-60">
+        {children}
+      </label>
+    );
+
+    return (
+      <Grid cols={"2"} rows={"6"} rowGap={"6"}>
+        <GridItem>
+          <Flexbox direction={"column"} width={"min"}>
+            <Label>ACL type</Label>
+            <div>
+              <StatusChip
+                status={topictype === "Producer" ? "info" : "success"}
+                text={topictype}
+              />
+            </div>
+          </Flexbox>
+        </GridItem>
+        <GridItem>
+          <Flexbox direction={"column"}>
+            <Label>Requesting team</Label>
+            {requestingTeamName}
+          </Flexbox>
+        </GridItem>
+        <GridItem>
+          <Flexbox direction={"column"} width={"min"}>
+            <Label>Environment</Label>
+            <div>
+              <StatusChip status={"neutral"} text={environmentName} />
+            </div>
+          </Flexbox>
+        </GridItem>
+        <GridItem>
+          <Flexbox direction={"column"}>
+            <Label>Topic</Label>
+            {topicname}
+          </Flexbox>
+        </GridItem>
+        <GridItem colSpan={"span-2"}>
+          <Flexbox direction={"column"}>
+            <Label>Principals/Usernames</Label>
+            <Flexbox direction={"row"} gap={"2"}>
+              {principals.map((principal) => (
+                <StatusChip
+                  key={principal}
+                  status={"neutral"}
+                  text={`${principal} `}
+                />
+              ))}
+            </Flexbox>
+          </Flexbox>
+        </GridItem>
+        {ips.length > 0 && (
+          <GridItem colSpan={"span-2"}>
+            <Flexbox direction={"column"}>
+              <Label>IP addresses</Label>
+              <Flexbox direction={"row"} gap={"2"}>
+                {ips.map((ip) => (
+                  <StatusChip key={ip} status={"neutral"} text={`${ip} `} />
+                ))}
+              </Flexbox>
+            </Flexbox>
+          </GridItem>
+        )}
+        <GridItem colSpan={"span-2"}>
+          <Flexbox direction={"column"}>
+            <Label>Consumer group</Label>
+            {topictype === "Consumer" ? consumergroup : <i>Not applicable</i>}
+          </Flexbox>
+        </GridItem>
+        <GridItem colSpan={"span-2"}>
+          <Flexbox direction={"column"}>
+            <Label>Message for the approver</Label>
+            {remarks || <i>No message</i>}
+          </Flexbox>
+        </GridItem>
+        <GridItem>
+          <Flexbox direction={"column"}>
+            <Label> Request by</Label>
+            {username}
+          </Flexbox>
+        </GridItem>
+        <GridItem>
+          <Flexbox direction={"column"}>
+            <Label> Requested on</Label>
+            {requesttimestring} UTC
+          </Flexbox>
+        </GridItem>
+      </Grid>
+    );
+  };
+
   return (
     <>
       {detailsModal.isOpen && (
@@ -322,13 +441,11 @@ function AclApprovals() {
           }}
           isLoading={approveIsLoading}
         >
-          <div>
-            {JSON.stringify(
-              data.entries.find(
-                (request) => request.req_no === Number(detailsModal.reqNo)
-              )
-            )}
-          </div>
+          {renderRequestDetails(
+            data.entries.find(
+              (request) => request.req_no === Number(detailsModal.reqNo)
+            )
+          )}
         </RequestDetailsModal>
       )}
       {rejectModal.isOpen && (
