895ba244c8688b78e629d475193e2fb29ec6fdbb
Mathieu Anderson <mathieu.anderson@aiven.io>
2023-02-22 16:53:16 +0100

    fix(coral): Add table-filters folder
    Signed-off-by: Mathieu Anderson <mathieu.anderson@aiven.io>

--- coral/src/app/features/components/table-filters/SelectTeam.test.tsx
+++ coral/src/app/features/components/table-filters/SelectTeam.test.tsx
@@ -1,7 +1,7 @@
 import { cleanup, screen, waitFor } from "@testing-library/react";
 import { waitForElementToBeRemoved } from "@testing-library/react/pure";
 import userEvent from "@testing-library/user-event";
-import SelectTeam from "src/app/features/components/SelectTeam";
+import SelectTeam from "src/app/features/components/table-filters/SelectTeam";
 import { getTeams } from "src/domain/team/team-api";
 import { customRender } from "src/services/test-utils/render-with-wrappers";
 
