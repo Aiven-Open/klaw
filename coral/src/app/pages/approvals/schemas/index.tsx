e0837a8b59c25028341e42eeb63b72f2b4858394
Mathieu Anderson <mathieu.anderson@aiven.io>
2023-02-27 18:04:00 +0100

    feat(coral): Add preview banners for Approvals (#715)
    Signed-off-by: Mathieu Anderson <mathieu.anderson@aiven.io>

--- coral/src/app/pages/approvals/schemas/index.tsx
+++ coral/src/app/pages/approvals/schemas/index.tsx
@@ -1,7 +1,13 @@
+import PreviewBanner from "src/app/components/PreviewBanner";
 import SchemaApprovals from "src/app/features/approvals/schemas/SchemaApprovals";
 
 const SchemaApprovalsPage = () => {
-  return <SchemaApprovals />;
+  return (
+    <>
+      <PreviewBanner linkTarget={"/execSchemas"} />
+      <SchemaApprovals />
+    </>
+  );
 };
 
 export default SchemaApprovalsPage;
