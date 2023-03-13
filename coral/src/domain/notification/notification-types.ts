5d4650ec0eb20ff3849779a2514bf79ecd19387d
Mathieu Anderson <mathieu.anderson@aiven.io>
2023-03-13 09:41:39 +0100

    refactor(coral): Delete notification domain (obsolete, unused)
    Signed-off-by: Mathieu Anderson <mathieu.anderson@aiven.io>

--- coral/src/domain/notification/notification-types.ts
+++
@@ -1,9 +0,0 @@
-type Notifications = {
-  topicNotificationCount: number;
-  connectorNotificationCount: number;
-  schemaNotificationCount: number;
-  userNotificationCount: number;
-  aclNotificationCount: number;
-};
-
-export type { Notifications };
