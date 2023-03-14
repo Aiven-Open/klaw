3a88fff419658c5f54970e4fc5e07e67ddf8cfcf
Mirjam Aulbach <mirjam.aulbach@aiven.io>
2023-03-14 16:26:29 +0100

    Mock out api call for team name as temp solution.
    Signed-off-by: Mirjam Aulbach <mirjam.aulbach@aiven.io>

--- coral/test-setup/setup-files-after-env.ts
+++ coral/test-setup/setup-files-after-env.ts
@@ -1,7 +1,13 @@
 import "@testing-library/jest-dom";
 import "whatwg-fetch";
 import * as process from "process";
+
 //eslint-disable-next-line no-relative-import-paths/no-relative-import-paths
 
 process.env.API_BASE_URL = "http://localhost:8080";
 process.env.FEATURE_FLAG_TOPIC_REQUEST = "true";
+
+jest.mock("src/domain/auth-user", () => ({
+  ...jest.requireActual("src/domain/auth-user"),
+  getUserTeamName: () => "TeamName",
+}));
