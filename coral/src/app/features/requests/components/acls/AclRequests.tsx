37139bd64e2ceafc0b4430be731f5a922af158d9
Mathieu Anderson <mathieu.anderson@aiven.io>
2023-03-15 09:16:12 +0100

    feat(coral): Add pagination to AclRequests
    Signed-off-by: Mathieu Anderson <mathieu.anderson@aiven.io>

--- coral/src/app/features/requests/components/acls/AclRequests.tsx
+++ coral/src/app/features/requests/components/acls/AclRequests.tsx
@@ -1,16 +1,39 @@
 import { useQuery } from "@tanstack/react-query";
+import { useSearchParams } from "react-router-dom";
+import { Pagination } from "src/app/components/Pagination";
 import { TableLayout } from "src/app/features/components/layouts/TableLayout";
-import { getAclRequests } from "src/domain/acl/acl-api";
 import { AclRequestsTable } from "src/app/features/requests/components/acls/components/AclRequestsTable";
+import { getAclRequests } from "src/domain/acl/acl-api";
 
 function AclRequests() {
+  const [searchParams, setSearchParams] = useSearchParams();
+
+  const currentPage = searchParams.get("page")
+    ? Number(searchParams.get("page"))
+    : 1;
+
   const { data, isLoading, isError, error } = useQuery({
-    queryKey: ["aclRequests"],
+    queryKey: ["aclRequests", currentPage],
     queryFn: () =>
       getAclRequests({
-        pageNo: "1",
+        pageNo: String(currentPage),
       }),
+    keepPreviousData: true,
   });
+
+  const handleChangePage = (page: number) => {
+    searchParams.set("page", page.toString());
+    setSearchParams(searchParams);
+  };
+
+  const pagination =
+    data?.totalPages && data.totalPages > 1 ? (
+      <Pagination
+        activePage={data.currentPage}
+        totalPages={data.totalPages}
+        setActivePage={handleChangePage}
+      />
+    ) : undefined;
 
   return (
     <TableLayout
@@ -22,6 +45,7 @@ function AclRequests() {
           onDelete={() => null}
         />
       }
+      pagination={pagination}
       isLoading={isLoading}
       isErrorLoading={isError}
       errorMessage={error}
