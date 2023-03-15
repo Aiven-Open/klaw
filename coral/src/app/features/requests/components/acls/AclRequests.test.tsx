13434bc11f10b34eb97cf1d37174a55d3c86cdb0
Mathieu Anderson <mathieu.anderson@aiven.io>
2023-03-15 11:59:53 +0100

    feat(coral): Acl Requests: Add pagination to AclRequests (#829)
    Signed-off-by: Mathieu Anderson <mathieu.anderson@aiven.io>

--- coral/src/app/features/requests/components/acls/AclRequests.test.tsx
+++ coral/src/app/features/requests/components/acls/AclRequests.test.tsx
@@ -1,4 +1,9 @@
-import { cleanup } from "@testing-library/react";
+import {
+  cleanup,
+  screen,
+  waitForElementToBeRemoved,
+} from "@testing-library/react";
+import userEvent from "@testing-library/user-event";
 import { AclRequests } from "src/app/features/requests/components/acls/AclRequests";
 import { getAclRequests } from "src/domain/acl/acl-api";
 import transformAclRequestApiResponse from "src/domain/acl/acl-transformer";
@@ -98,7 +103,153 @@ describe("AclRequests", () => {
   it("makes a request to the api to get the team's ACL requests", () => {
     customRender(<AclRequests />, {
       queryClient: true,
+      memoryRouter: true,
     });
     expect(getAclRequests).toBeCalledTimes(1);
+  });
+
+  describe("renders pagination dependent on response", () => {
+    beforeEach(() => {
+      mockGetAclRequests.mockResolvedValue({
+        totalPages: 1,
+        currentPage: 1,
+        entries: [],
+      });
+    });
+    afterEach(() => {
+      cleanup();
+      jest.clearAllMocks();
+    });
+
+    it("fetches the right page number if a page is set in search params", async () => {
+      const routePath = "/?page=100";
+      customRender(<AclRequests />, {
+        queryClient: true,
+        memoryRouter: true,
+        customRoutePath: routePath,
+      });
+
+      await waitForElementToBeRemoved(screen.getByTestId("skeleton-table"));
+
+      expect(mockGetAclRequests).toHaveBeenCalledWith({
+        pageNo: "100",
+      });
+    });
+
+    it("fetches the first page if no search param is defined", async () => {
+      customRender(<AclRequests />, {
+        queryClient: true,
+        memoryRouter: true,
+      });
+
+      await waitForElementToBeRemoved(screen.getByTestId("skeleton-table"));
+
+      expect(mockGetAclRequests).toHaveBeenCalledWith({
+        pageNo: "1",
+      });
+    });
+
+    it("shows no pagination for a response with only one page", async () => {
+      mockGetAclRequests.mockResolvedValue({
+        ...mockGetAclRequestsResponse,
+        totalPages: 1,
+      });
+
+      customRender(<AclRequests />, {
+        queryClient: true,
+        memoryRouter: true,
+      });
+
+      await waitForElementToBeRemoved(screen.getByTestId("skeleton-table"));
+
+      const pagination = screen.queryByRole("navigation", {
+        name: /Pagination/,
+      });
+      expect(pagination).not.toBeInTheDocument();
+    });
+
+    it("shows a pagination when response has more then one page", async () => {
+      mockGetAclRequests.mockResolvedValue({
+        totalPages: 2,
+        currentPage: 1,
+        entries: [],
+      });
+
+      customRender(<AclRequests />, {
+        queryClient: true,
+        memoryRouter: true,
+      });
+
+      await waitForElementToBeRemoved(screen.getByTestId("skeleton-table"));
+
+      const pagination = screen.getByRole("navigation", {
+        name: "Pagination navigation, you're on page 1 of 2",
+      });
+      expect(pagination).toBeVisible();
+    });
+
+    it("shows the currently active page based on api response", async () => {
+      mockGetAclRequests.mockResolvedValue({
+        totalPages: 4,
+        currentPage: 2,
+        entries: [],
+      });
+
+      customRender(<AclRequests />, {
+        queryClient: true,
+        memoryRouter: true,
+      });
+
+      await waitForElementToBeRemoved(screen.getByTestId("skeleton-table"));
+
+      const pagination = screen.getByRole("navigation", {
+        name: "Pagination navigation, you're on page 2 of 4",
+      });
+      expect(pagination).toBeVisible();
+    });
+  });
+
+  describe("handles user stepping through pagination", () => {
+    beforeEach(async () => {
+      mockGetAclRequests.mockResolvedValue({
+        totalPages: 3,
+        currentPage: 1,
+        entries: [],
+      });
+
+      customRender(<AclRequests />, {
+        queryClient: true,
+        memoryRouter: true,
+      });
+
+      await waitForElementToBeRemoved(screen.getByTestId("skeleton-table"));
+    });
+
+    afterEach(() => {
+      jest.clearAllMocks();
+      cleanup();
+    });
+
+    it("shows page 1 as currently active page and the total page number", () => {
+      const pagination = screen.getByRole("navigation", {
+        name: /Pagination/,
+      });
+
+      expect(pagination).toHaveAccessibleName(
+        "Pagination navigation, you're on page 1 of 3"
+      );
+    });
+
+    it("fetches new data when user clicks on next page", async () => {
+      const pageTwoButton = screen.getByRole("button", {
+        name: "Go to next page, page 2",
+      });
+
+      await userEvent.click(pageTwoButton);
+
+      expect(mockGetAclRequests).toHaveBeenNthCalledWith(2, {
+        pageNo: "2",
+      });
+    });
   });
 });
