e9118580d27b5ff80123747239be5bedfc2d47fc
Mirjam Aulbach <mirjam.aulbach@aiven.io>
2023-03-17 07:46:05 +0100

    coral(feat): My Schema Requests - add search functionality (#845)
    Add search filter for topic name
    
    Signed-off-by: Mirjam Aulbach <mirjam.aulbach@aiven.io>

--- coral/src/app/features/requests/components/schemas/SchemaRequests.tsx
+++ coral/src/app/features/requests/components/schemas/SchemaRequests.tsx
@@ -4,12 +4,15 @@ import { SchemaRequestTable } from "src/app/features/requests/components/schemas
 import { TableLayout } from "src/app/features/components/layouts/TableLayout";
 import { useSearchParams } from "react-router-dom";
 import { Pagination } from "src/app/components/Pagination";
+import TopicFilter from "src/app/features/components/table-filters/TopicFilter";
 
 function SchemaRequests() {
   const [searchParams, setSearchParams] = useSearchParams();
+
   const currentPage = searchParams.get("page")
     ? Number(searchParams.get("page"))
     : 1;
+  const currentTopic = searchParams.get("topic") ?? undefined;
 
   const {
     data: schemaRequests,
@@ -17,8 +20,9 @@ function SchemaRequests() {
     isError,
     error,
   } = useQuery({
-    queryKey: ["schemaRequests", currentPage],
-    queryFn: () => getSchemaRequests({ pageNo: String(currentPage) }),
+    queryKey: ["schemaRequests", currentPage, currentTopic],
+    queryFn: () =>
+      getSchemaRequests({ pageNo: String(currentPage), topic: currentTopic }),
     keepPreviousData: true,
   });
 
@@ -38,7 +42,7 @@ function SchemaRequests() {
 
   return (
     <TableLayout
-      filters={[]}
+      filters={[<TopicFilter key={"topic"} />]}
       table={<SchemaRequestTable requests={schemaRequests?.entries || []} />}
       pagination={pagination}
       isLoading={isLoading}
