9f8c8a12901e4e7453dc8f14503449459c127c08
Mirjam Aulbach <mirjam.aulbach@aiven.io>
2023-03-22 10:12:08 +0100

    Add preview banner in coral my-schema-requests
    Signed-off-by: Mirjam Aulbach <mirjam.aulbach@aiven.io>

--- coral/src/app/pages/requests/schemas/index.test.tsx
+++ coral/src/app/pages/requests/schemas/index.test.tsx
@@ -1,7 +1,7 @@
 import { getSchemaRequests } from "src/domain/schema-request";
 import { customRender } from "src/services/test-utils/render-with-wrappers";
 import SchemaRequestsPage from "src/app/pages/requests/schemas/index";
-import { cleanup, screen } from "@testing-library/react";
+import { cleanup, screen, within } from "@testing-library/react";
 import { waitForElementToBeRemoved } from "@testing-library/react/pure";
 import { getSchemaRegistryEnvironments } from "src/domain/environment";
 
@@ -17,7 +17,7 @@ const mockGetSchemaRequests = getSchemaRequests as jest.MockedFunction<
 >;
 
 describe("SchemaRequestPage", () => {
-  beforeAll(() => {
+  beforeAll(async () => {
     mockGetSchemaRegistryEnvironments.mockResolvedValue([]);
     mockGetSchemaRequests.mockResolvedValue({
       entries: [],
@@ -29,12 +29,31 @@ describe("SchemaRequestPage", () => {
       queryClient: true,
       memoryRouter: true,
     });
+    await waitForElementToBeRemoved(screen.getByTestId("skeleton-table"));
   });
 
   afterAll(cleanup);
 
-  it("renders the schema request view", async () => {
-    await waitForElementToBeRemoved(screen.getByTestId("skeleton-table"));
+  it("shows a preview banner to inform users about the early version of the view", () => {
+    const previewBanner = screen.getByLabelText("Preview disclaimer");
+
+    expect(previewBanner).toBeVisible();
+    expect(previewBanner).toHaveTextContent(
+      "You are viewing a preview of the redesigned user interface. You are one of our early reviewers, and your feedback will help us improve the product. You can always go back to the old interface."
+    );
+  });
+
+  it("shows link back back to the original Klaw app for this view in the preview banner", () => {
+    const previewBanner = screen.getByLabelText("Preview disclaimer");
+    const link = within(previewBanner).getByRole("link", {
+      name: "old interface",
+    });
+
+    expect(link).toBeVisible();
+    expect(link).toHaveAttribute("href", "/mySchemaRequests");
+  });
+
+  it("renders the schema request view", () => {
     const emptyRequests = screen.getByText(
       "No Schema request matched your criteria."
     );
