64394fd8000b9249af31d77064ec55b0da4ce0a9
Roope Karhapää <roope-kar@aiven.io>
2023-04-05 10:54:07 +0300

    feat(coral): My Connector Requests: Add Pagination to the ConnectorsTable #965
    Add Pagination controls to ConnectorRequests and sync
    it with the ConnectorRequestsTable
    
    Signed-off-by: Roope Karhapää <roope-kar@aiven.io>

--- coral/src/app/features/requests/connectors/ConnectorRequests.tsx
+++ coral/src/app/features/requests/connectors/ConnectorRequests.tsx
@@ -1,16 +1,38 @@
 import { useQuery } from "@tanstack/react-query";
+import { useSearchParams } from "react-router-dom";
+import { Pagination } from "src/app/components/Pagination";
 import { TableLayout } from "src/app/features/components/layouts/TableLayout";
 import { ConnectorRequestsTable } from "src/app/features/requests/connectors/components/ConnectorRequestsTable";
 import { getConnectorRequests } from "src/domain/connector";
 
 function ConnectorRequests() {
+  const [searchParams, setSearchParams] = useSearchParams();
+  const currentPage = searchParams.get("page")
+    ? Number(searchParams.get("page"))
+    : 1;
+
   const { data, isLoading, isError, error } = useQuery({
-    queryKey: ["connectorRequests"],
+    queryKey: ["connectorRequests", currentPage],
     queryFn: () =>
       getConnectorRequests({
-        pageNo: "1",
+        pageNo: String(currentPage),
       }),
+    keepPreviousData: true,
   });
+
+  const setCurrentPage = (page: number) => {
+    searchParams.set("page", page.toString());
+    setSearchParams(searchParams);
+  };
+
+  const pagination =
+    data?.totalPages && data.totalPages > 1 ? (
+      <Pagination
+        activePage={data.currentPage}
+        totalPages={data?.totalPages}
+        setActivePage={setCurrentPage}
+      />
+    ) : undefined;
 
   return (
     <TableLayout
@@ -23,6 +45,7 @@ function ConnectorRequests() {
           onDelete={() => null}
         />
       }
+      pagination={pagination}
       isLoading={isLoading}
       isErrorLoading={isError}
       errorMessage={error}
