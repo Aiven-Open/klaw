43ead841efd13f698b6a01d7ff5deb516979f73f
Mirjam Aulbach <mirjam.aulbach@aiven.io>
2023-04-06 16:12:43 +0200

    Remove unnecessary api endpoint
    Signed-off-by: Mirjam Aulbach <mirjam.aulbach@aiven.io>

--- coral/src/domain/team/team-api.ts
+++ coral/src/domain/team/team-api.ts
@@ -1,15 +1,8 @@
 import api from "src/services/api";
-import { transformTeamNamesGetResponse } from "src/domain/team/team-transformer";
 import { KlawApiResponse } from "types/utils";
-
-const getTeamNames = () => {
-  return api
-    .get<KlawApiResponse<"getAllTeamsSUOnly">>("/getAllTeamsSUOnly")
-    .then(transformTeamNamesGetResponse);
-};
 
 const getTeams = () => {
   return api.get<KlawApiResponse<"getAllTeamsSU">>("/getAllTeamsSU");
 };
 
-export { getTeamNames, getTeams };
+export { getTeams };
