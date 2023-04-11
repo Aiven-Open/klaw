b7f0647540c75c2c89f88db28922f942b8355ab6
Mirjam Aulbach <mirjam.aulbach@aiven.io>
2023-04-11 10:27:49 +0200

    Add comment to convertQuery function
    Signed-off-by: Mirjam Aulbach <mirjam.aulbach@aiven.io>

--- coral/src/services/api-helper.ts
+++ coral/src/services/api-helper.ts
@@ -8,6 +8,10 @@ type QueryToTransform = {
 
 function convertQuery(query: QueryToTransform): Record<string, string> {
   return cloneDeepWith(query, (value) => {
+    // returning undefined if the value for the customizer function
+    // is an object makes sure we're iterating over everything
+    // this is not strictly needed for our current use-case
+    // but added to be more versatile usable in the future
     return isPlainObject(value) ? undefined : toString(value);
   });
 }
