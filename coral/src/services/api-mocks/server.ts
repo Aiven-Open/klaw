3f7de2c12c7c2ccf67b69354a8ad90351fb140b4
Mirjam Aulbach <mirjam.aulbach@aiven.io>
2023-04-20 16:08:08 +0200

    Add log to help debugging
    Signed-off-by: Mirjam Aulbach <mirjam.aulbach@aiven.io>

--- coral/src/services/api-mocks/server.ts
+++ coral/src/services/api-mocks/server.ts
@@ -4,6 +4,7 @@ import { rest } from "msw";
 export const server = setupServer(
   rest.all("*", (req, res, ctx) => {
     console.error("Please mock all api calls in your tests.");
+    console.error("Request params: ", req.params);
 
     return res(ctx.status(404), ctx.json({}));
   })
