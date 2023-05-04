feea830c4c1194985511948dc37ed105ac2b5f6a
Mirjam Aulbach <mirjam.aulbach@aiven.io>
2023-05-04 08:06:30 +0200

    Add Header so first load is not empty page
    Signed-off-by: Mirjam Aulbach <mirjam.aulbach@aiven.io>

--- coral/src/app/context-provider/AuthProvider.tsx
+++ coral/src/app/context-provider/AuthProvider.tsx
@@ -1,6 +1,8 @@
 import { createContext, ReactNode, useContext } from "react";
 import { useQuery } from "@tanstack/react-query";
 import { AuthUser, getAuth } from "src/domain/auth-user";
+import Header from "src/app/layout/header/Header";
+import { Box, GridItem } from "@aivenio/aquarium";
 
 // for now, we don't do Authentication on Corals side,
 // so we only have a AuthUser in the context
@@ -20,7 +22,11 @@ const AuthProvider = ({ children }: { children: ReactNode }) => {
     );
   }
 
-  return <></>;
+  return (
+    <Box height={"l5"} backgroundColor={"primary-80"} paddingX={"l2"}>
+      <Header />
+    </Box>
+  );
 };
 
 export { useAuthContext, AuthProvider };
