881da95d7011ab879a5f972a451070f341bda1b8
Mathieu Anderson <mathieu.anderson@aiven.io>
2023-05-05 09:13:11 +0200

    feat(coral): Fix link and copy for Schema request page
    Signed-off-by: Mathieu Anderson <mathieu.anderson@aiven.io>

--- coral/src/app/pages/topics/schema-request.tsx
+++ coral/src/app/pages/topics/schema-request.tsx
@@ -12,15 +12,21 @@ const SchemaRequest = () => {
   return (
     <AuthenticationRequiredBoundary>
       <Layout>
-        {topicName && (
-          <>
-            <PreviewBanner
-              linkTarget={`/requestSchema?topicname=${topicName}`}
-            />
-            <PageHeader title={`Request new schema for topic "${topicName}"`} />
-            <TopicSchemaRequest topicName={topicName} />
-          </>
-        )}
+        <PreviewBanner
+          linkTarget={
+            topicName === undefined
+              ? `/requestSchema`
+              : `/requestSchema?topicname=${topicName}`
+          }
+        />
+        <PageHeader
+          title={
+            topicName === undefined
+              ? `Request new schema`
+              : `Request new schema for topic "${topicName}"`
+          }
+        />
+        <TopicSchemaRequest topicName={topicName} />
       </Layout>
     </AuthenticationRequiredBoundary>
   );
