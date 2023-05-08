ccbe8840890b827194253c1fbfc2a073dee341a0
Mirjam Aulbach <mirjam.aulbach@aiven.io>
2023-05-08 14:05:44 +0200

    fix(coral): Remove Layout wrapper from child page components. (#1128)
    Signed-off-by: Mirjam Aulbach <mirjam.aulbach@aiven.io>

--- coral/src/app/pages/requests/topics/index.tsx
+++ coral/src/app/pages/requests/topics/index.tsx
@@ -1,13 +1,12 @@
 import { TopicRequests } from "src/app/features/requests/topics/TopicRequests";
 import PreviewBanner from "src/app/components/PreviewBanner";
-import Layout from "src/app/layout/Layout";
 
 const TopicRequestsPage = () => {
   return (
-    <Layout>
+    <>
       <PreviewBanner linkTarget={"/myTopicRequests"} />
       <TopicRequests />
-    </Layout>
+    </>
   );
 };
 
