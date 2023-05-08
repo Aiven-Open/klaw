db18777bd20890c0ac11c6aa5c483206cdba5d61
Aindriu Lavelle <aindriu.lavelle@aiven.io>
2023-05-08 14:20:28 +0100

    Pass the request status down to the db level to do the filtering.
    Signed-off-by: Aindriu Lavelle <aindriu.lavelle@aiven.io>

--- core/src/test/java/io/aiven/klaw/service/KafkaConnectControllerServiceTest.java
+++ core/src/test/java/io/aiven/klaw/service/KafkaConnectControllerServiceTest.java
@@ -224,7 +224,13 @@ public class KafkaConnectControllerServiceTest {
     when(commonUtilsService.isNotAuthorizedUser(any(), any())).thenReturn(false);
 
     when(handleDbRequests.getAllConnectorRequests(
-            anyString(), eq(null), eq(null), eq(null), eq(101), eq(false)))
+            anyString(),
+            eq(null),
+            eq(RequestStatus.CREATED),
+            eq(null),
+            eq(null),
+            eq(101),
+            eq(false)))
         .thenReturn(generateKafkaConnectorRequests(50));
     when(commonUtilsService.getEnvsFromUserId(anyString()))
         .thenReturn(new HashSet<>(Collections.singletonList("1")));
@@ -234,7 +240,7 @@ public class KafkaConnectControllerServiceTest {
         kafkaConnectControllerService.getConnectorRequests(
             "1",
             "1",
-            "all",
+            RequestStatus.CREATED,
             null,
             null,
             io.aiven.klaw.model.enums.Order.DESC_REQUESTED_TIME,
@@ -263,7 +269,13 @@ public class KafkaConnectControllerServiceTest {
     when(commonUtilsService.isNotAuthorizedUser(any(), any())).thenReturn(false);
 
     when(handleDbRequests.getAllConnectorRequests(
-            anyString(), eq(null), eq(null), eq(null), eq(101), eq(false)))
+            anyString(),
+            eq(null),
+            eq(RequestStatus.CREATED),
+            eq(null),
+            eq(null),
+            eq(101),
+            eq(false)))
         .thenReturn(generateKafkaConnectorRequests(50));
     when(commonUtilsService.getEnvsFromUserId(anyString()))
         .thenReturn(new HashSet<>(Collections.singletonList("1")));
@@ -273,7 +285,7 @@ public class KafkaConnectControllerServiceTest {
         kafkaConnectControllerService.getConnectorRequests(
             "1",
             "1",
-            "all",
+            RequestStatus.CREATED,
             null,
             null,
             io.aiven.klaw.model.enums.Order.ASC_REQUESTED_TIME,
@@ -302,7 +314,13 @@ public class KafkaConnectControllerServiceTest {
     when(commonUtilsService.isNotAuthorizedUser(any(), any())).thenReturn(false);
 
     when(handleDbRequests.getAllConnectorRequests(
-            anyString(), eq(null), eq(null), eq(null), eq(101), eq(true)))
+            anyString(),
+            eq(null),
+            eq(RequestStatus.CREATED),
+            eq(null),
+            eq(null),
+            eq(101),
+            eq(true)))
         .thenReturn(generateKafkaConnectorRequests(50));
     when(commonUtilsService.getEnvsFromUserId(anyString()))
         .thenReturn(new HashSet<>(Collections.singletonList("1")));
@@ -312,7 +330,7 @@ public class KafkaConnectControllerServiceTest {
         kafkaConnectControllerService.getConnectorRequests(
             "1",
             "1",
-            "all",
+            RequestStatus.CREATED,
             null,
             null,
             io.aiven.klaw.model.enums.Order.ASC_REQUESTED_TIME,
@@ -322,7 +340,14 @@ public class KafkaConnectControllerServiceTest {
     assertThat(ordered_response).hasSize(10);
 
     verify(handleDbRequests, times(1))
-        .getAllConnectorRequests(anyString(), eq(null), eq(null), eq(null), eq(101), eq(true));
+        .getAllConnectorRequests(
+            anyString(),
+            eq(null),
+            eq(RequestStatus.CREATED),
+            eq(null),
+            eq(null),
+            eq(101),
+            eq(true));
   }
 
   private static List<KafkaConnectorRequest> generateKafkaConnectorRequests(int number) {
