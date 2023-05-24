fe4bdb703797329a66bc5e20914c970d56e9bab4
Mathieu Anderson <mathieu.anderson@aiven.io>
2023-05-24 11:22:36 +0200

    fix(coral): Add crypto.randomUUID to test setup
    Signed-off-by: Mathieu Anderson <mathieu.anderson@aiven.io>

--- coral/test-setup/setup-files-after-env.ts
+++ coral/test-setup/setup-files-after-env.ts
@@ -1,6 +1,11 @@
 import "@testing-library/jest-dom";
 import "whatwg-fetch";
 import * as process from "process";
+// This is needed to be able to use crypto.randomUUID in testing components which use it
+// ie: components using useToast
+import { Crypto } from "@peculiar/webcrypto";
+
+window.crypto.randomUUID = new Crypto().randomUUID;
 
 //eslint-disable-next-line no-relative-import-paths/no-relative-import-paths
 
