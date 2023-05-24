ab35e47451387d545408c18d30ae81d728073ec1
Aindriu Lavelle <aindriu.lavelle@aiven.io>
2023-05-24 09:45:52 +0100

    remove obsolete method
    Signed-off-by: Aindriu Lavelle <aindriu.lavelle@aiven.io>

--- core/src/main/java/io/aiven/klaw/config/ManageDatabase.java
+++ core/src/main/java/io/aiven/klaw/config/ManageDatabase.java
@@ -422,10 +422,6 @@ public class ManageDatabase implements ApplicationContextAware, InitializingBean
     }
   }
 
-  public void addUserToCache(UserInfo newUser) {
-    allUsersAllTenants.add(newUser);
-  }
-
   public List<UserInfo> selectAllCachedUserInfo() {
     return allUsersAllTenants;
   }
