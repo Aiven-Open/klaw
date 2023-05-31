d2c356d5fac6f9ec6f1d5a8468d58181f68890a4
Mathieu Anderson <mathieu.anderson@aiven.io>
2023-05-31 16:38:47 +0200

    feat(coral): Add overview content
    Signed-off-by: Mathieu Anderson <mathieu.anderson@aiven.io>

--- coral/src/app/features/topics/details/overview/index.tsx
+++ coral/src/app/features/topics/details/overview/index.tsx
@@ -1,9 +1,86 @@
+import {
+  Box,
+  Button,
+  Card,
+  Grid,
+  GridItem,
+  Typography,
+} from "@aivenio/aquarium";
+import add from "@aivenio/aquarium/dist/src/icons/add";
+import { useMemo } from "react";
+import { Link } from "react-router-dom";
 import { useTopicDetails } from "src/app/features/topics/details/TopicDetails";
+import StatsDisplay from "src/app/features/topics/details/components/StatsDisplay";
+import { getTopicStats } from "src/app/features/topics/details/utils";
 
 function TopicOverview() {
-  const { topicName } = useTopicDetails();
+  const { topicName, environmentId, topicOverview, topicSchemas } =
+    useTopicDetails();
 
-  return <div>Placeholder for Overview panel, topic name: {topicName}</div>;
+  const stats = useMemo(() => getTopicStats(topicOverview), [topicOverview]);
+
+  return (
+    <Grid cols={"2"} rows={"2"} gap={"l2"} style={{ gridTemplateRows: "auto" }}>
+      <GridItem colSpan={"span-2"}>
+        <Card title="Topic details" fullWidth>
+          <Box.Flex display="flex" gap={"l7"}>
+            <StatsDisplay amount={stats.replicas} entity={"Replicas"} />
+            <StatsDisplay amount={stats.partitions} entity={"Partitions"} />
+          </Box.Flex>
+        </Card>
+      </GridItem>
+
+      <Card title={"Subscriptions"} fullWidth>
+        <Box.Flex gap={"l7"}>
+          <StatsDisplay amount={stats.producers} entity={"Producers"} />
+          <StatsDisplay amount={stats.consumers} entity={"Consumers"} />
+        </Box.Flex>
+        <Box.Flex flexDirection={"row"} gap={"l3"} paddingTop={"l2"}>
+          <Link to={`/topic/${topicName}/subscribe?env=${environmentId}`}>
+            <Button.Ghost icon={add}>
+              <Typography.SmallStrong color={"primary-80"}>
+                Request new subscription
+              </Typography.SmallStrong>
+            </Button.Ghost>
+          </Link>
+          <Link to={`/topic/${topicName}/subscriptions`}>
+            <Button.Ghost>
+              <Typography.SmallStrong color={"primary-80"}>
+                See subscriptions
+              </Typography.SmallStrong>
+            </Button.Ghost>
+          </Link>
+        </Box.Flex>
+      </Card>
+
+      <Card title={"Schemas"} fullWidth>
+        <StatsDisplay
+          amount={
+            topicSchemas.allSchemaVersions !== undefined
+              ? Object.keys(topicSchemas.allSchemaVersions).length
+              : 0
+          }
+          entity={"Schemas"}
+        />
+        <Box.Flex flexDirection={"row"} gap={"l3"} paddingTop={"l2"}>
+          <Link to={`request/schema`}>
+            <Button.Ghost icon={add}>
+              <Typography.SmallStrong color={"primary-80"}>
+                Request new schema
+              </Typography.SmallStrong>
+            </Button.Ghost>
+          </Link>
+          <Link to={`/topic/${topicName}/schema`}>
+            <Button.Ghost>
+              <Typography.SmallStrong color={"primary-80"}>
+                See schema
+              </Typography.SmallStrong>
+            </Button.Ghost>
+          </Link>
+        </Box.Flex>
+      </Card>
+    </Grid>
+  );
 }
 
 export { TopicOverview };
