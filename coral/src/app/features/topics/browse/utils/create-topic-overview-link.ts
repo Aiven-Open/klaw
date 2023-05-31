d0c4b3a64e9195771045085f2ddbad0ac942b470
Mathieu Anderson <mathieu.anderson@aiven.io>
2023-05-31 10:41:19 +0200

    feat(coral): Change route of Browse topic links -> `topic/:topicName/overview` (#1295)
    feat(coral): Make topic links in Browse topic coral routes
    
    Signed-off-by: Mathieu Anderson <mathieu.anderson@aiven.io>

--- coral/src/app/features/topics/browse/utils/create-topic-overview-link.ts
+++
@@ -1,8 +0,0 @@
-const urlPartKlawAngularApp = "topicOverview?topicname=";
-
-function createTopicOverviewLink(topicName: string): string {
-  const originLocationKlawAngular = window.location.origin;
-  return `${originLocationKlawAngular}/${urlPartKlawAngularApp}${topicName}`;
-}
-
-export { createTopicOverviewLink };
