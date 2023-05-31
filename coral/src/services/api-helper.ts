fdb24959365e31804ee947b13f81a56353f87411
Mathieu Anderson <mathieu.anderson@aiven.io>
2023-05-31 16:15:10 +0200

    feat(coral): Add `getSchemaOfTopic` to `topic-api` domain (#1302)
    feat(coral): Add getSchemaOfTopic to topic-api domain
    
    Signed-off-by: Mathieu Anderson <mathieu.anderson@aiven.io>

--- coral/src/services/api-helper.ts
+++ coral/src/services/api-helper.ts
@@ -3,13 +3,16 @@ import isPlainObject from "lodash/isPlainObject";
 import toString from "lodash/toString";
 
 type QueryToTransform = {
-  [key: string]: string | boolean | number | QueryToTransform;
+  [key: string]: string | boolean | number | string[] | QueryToTransform;
 };
 
 function convertQueryValuesToString(
   query: QueryToTransform
 ): Record<string, string> {
   return cloneDeepWith(query, (value) => {
+    if (Array.isArray(value)) {
+      return value.join(",");
+    }
     // returning undefined if the value for the customizer function
     // is an object makes sure we're iterating over everything
     // this is not strictly needed for our current use-case
