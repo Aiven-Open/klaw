c5fe17b3d03210a5184b520dce2142a82688a699
Mirjam Aulbach <mirjam.aulbach@aiven.io>
2023-06-01 14:44:55 +0200

    Enable passing env through query for schema requests
    Signed-off-by: Mirjam Aulbach <mirjam.aulbach@aiven.io>

--- coral/src/app/features/topics/schema-request/TopicSchemaRequest.tsx
+++ coral/src/app/features/topics/schema-request/TopicSchemaRequest.tsx
@@ -1,7 +1,7 @@
 import { Alert, Box, Button } from "@aivenio/aquarium";
 import { useMutation, useQuery } from "@tanstack/react-query";
 import { useState } from "react";
-import { useNavigate } from "react-router-dom";
+import { useNavigate, useSearchParams } from "react-router-dom";
 import { Dialog } from "src/app/components/Dialog";
 import {
   Form,
@@ -38,6 +38,11 @@ type TopicSchemaRequestProps = {
 // be able to add this tests. I created na issue in github related to MonacoEditor testing already.
 function TopicSchemaRequest(props: TopicSchemaRequestProps) {
   const { topicName } = props;
+  const [searchParams] = useSearchParams();
+
+  const [presetEnvironment, setPresetEnvironment] = useState<
+    string | undefined
+  >();
 
   const [cancelDialogVisible, setCancelDialogVisible] = useState(false);
   const [successModalOpen, setSuccessModalOpen] = useState(false);
@@ -48,6 +53,7 @@ function TopicSchemaRequest(props: TopicSchemaRequestProps) {
     defaultValues: {
       topicname: topicName,
       schemafull: props.schemafullValueForTest || undefined,
+      environment: presetEnvironment,
     },
   });
 
@@ -67,7 +73,7 @@ function TopicSchemaRequest(props: TopicSchemaRequestProps) {
 
       const topicExists = data?.includes(topicName);
       if (!topicExists) {
-        navigate("/topics");
+        navigate(-1);
       }
     },
   });
@@ -78,6 +84,20 @@ function TopicSchemaRequest(props: TopicSchemaRequestProps) {
   >({
     queryKey: ["schemaRegistryEnvironments"],
     queryFn: () => getEnvironmentsForSchemaRequest(),
+    onSuccess: (environments) => {
+      const currentEnv = searchParams.get("env");
+      if (currentEnv) {
+        const isValidEnv =
+          environments.find((env) => currentEnv === env.id) !== undefined;
+        if (!isValidEnv) {
+          navigate(-1);
+        } else {
+          console.log("currentEnv", currentEnv);
+          setPresetEnvironment(currentEnv);
+          form.setValue("environment", currentEnv);
+        }
+      }
+    },
   });
 
   const schemaRequestMutation = useMutation(createSchemaRequest, {
@@ -169,7 +189,10 @@ function TopicSchemaRequest(props: TopicSchemaRequestProps) {
             <NativeSelect<TopicRequestFormSchema>
               name={"environment"}
               labelText={"Environment"}
-              placeholder={"-- Please select --"}
+              placeholder={
+                presetEnvironment ? undefined : "-- Please select --"
+              }
+              readOnly={presetEnvironment !== undefined}
               required={true}
             >
               {environments.map((env) => {
