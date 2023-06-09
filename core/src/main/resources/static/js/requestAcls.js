dc24adc4c9dea1f0b845c86be3a8babf60a1ad8e
Aindriu Lavelle <aindriu.lavelle@aiven.io>
2023-06-09 13:57:00 +0100

    remove the console log line
    Signed-off-by: Aindriu Lavelle <aindriu.lavelle@aiven.io>

--- core/src/main/resources/static/js/requestAcls.js
+++ core/src/main/resources/static/js/requestAcls.js
@@ -604,10 +604,9 @@ app.controller("requestAclsCtrl", function($scope, $http, $location, $window) {
                         $scope.getAllTopics("ALL");
                         $scope.getTopicTeam($scope.addAcl.topicname);
                     } else if (sParameterName[0] == "envId")
-                  {
-                      $scope.addAcl.envId  = sParameterName[1];
-                    console.log('EnvId from Url: ' + $scope.addAcl.envId  );
-                  }
+                    {
+                        $scope.addAcl.envId  = sParameterName[1];
+                    }
                 }
             }
 
