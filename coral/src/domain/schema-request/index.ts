25f20e45e8d3ea2c1a97682a1e5343be840da2bd
Mathieu Anderson <mathieu.anderson@aiven.io>
2023-06-16 13:58:44 +0200

    feat(coral): Topic details - Schema promotion flow (#1353)
    - Add promoteSchema domain handler and types
    - Add SchemaPromotionModal
    - Add promotion flow
    - Only show forceRegister option after one failure
    - Do not show Promotion banner when cannot be promoted
    
    ---------
    
    Signed-off-by: Mathieu Anderson <mathieu.anderson@aiven.io>
    Co-authored-by: Mirjam Aulbach <mirjam.aulbach@aiven.io>

--- coral/src/domain/schema-request/index.ts
+++ coral/src/domain/schema-request/index.ts
@@ -5,18 +5,27 @@ import {
   getSchemaRequests,
   getSchemaRequestsForApprover,
   deleteSchemaRequest,
+  promoteSchemaRequest,
 } from "src/domain/schema-request/schema-request-api";
 import {
   CreatedSchemaRequests,
+  PromoteSchemaPayload,
   SchemaRequest,
+  SchemaRequestApiResponse,
 } from "src/domain/schema-request/schema-request-types";
 
-export type { CreatedSchemaRequests, SchemaRequest };
+export type {
+  CreatedSchemaRequests,
+  SchemaRequest,
+  SchemaRequestApiResponse,
+  PromoteSchemaPayload,
+};
 export {
   createSchemaRequest,
   getSchemaRequestsForApprover,
-  getSchemaRequests,
   approveSchemaRequest,
   declineSchemaRequest,
+  getSchemaRequests,
   deleteSchemaRequest,
+  promoteSchemaRequest,
 };
