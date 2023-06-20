591cf38b946a8ca41d853f03463a9acd7a2da4ec
Mirjam Aulbach <mirjam.aulbach@aiven.io>
2023-06-20 16:34:22 +0200

    Add view for no documentation.
    Signed-off-by: Mirjam Aulbach <mirjam.aulbach@aiven.io>

--- coral/src/app/features/topics/details/documentation/TopicDocumentation.tsx
+++ coral/src/app/features/topics/details/documentation/TopicDocumentation.tsx
@@ -1,11 +1,20 @@
 import { PageHeader } from "@aivenio/aquarium";
-import { useTopicDetails } from "src/app/features/topics/details/TopicDetails";
+import { NoDocumentationBanner } from "src/app/features/topics/details/documentation/components/NoDocumentationBanner";
 
 function TopicDocumentation() {
-  const { topicOverview } = useTopicDetails();
+  // mocked data as topicOverview does not return doc right now
+  const topicOverview = {
+    topicDocumentation: "", //"<h1>hello</h1><div>This is doc</div>",
+  };
+
+  const noDocumentation =
+    !topicOverview?.topicDocumentation ||
+    topicOverview.topicDocumentation.length === 0;
+
   return (
     <>
       <PageHeader title={"Documentation"} />
+      {noDocumentation && <NoDocumentationBanner />}
       <div>{topicOverview.topicDocumentation}</div>
     </>
   );
