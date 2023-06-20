20fcd39e33c5aa05140380f24d2deef334943c60
Mirjam Aulbach <mirjam.aulbach@aiven.io>
2023-06-20 10:43:16 +0200

    Remove msw service worker
    Signed-off-by: Mirjam Aulbach <mirjam.aulbach@aiven.io>

--- coral/src/services/api-mocks/types.ts
+++ coral/src/services/api-mocks/types.ts
@@ -1,6 +1,5 @@
-import { SetupWorkerApi } from "msw";
 import { SetupServerApi } from "msw/node";
 
-type MswInstance = SetupWorkerApi | SetupServerApi;
+type MswInstance = SetupServerApi;
 
 export type { MswInstance };
