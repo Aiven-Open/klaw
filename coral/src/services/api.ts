3a63a1792968b7279ae84051d3c82de3f2b46042
muralibasani <muralidahr.basani@aiven.io>
2023-06-20 10:51:14 +0200

    Update format ts
    Signed-off-by: muralibasani <muralidahr.basani@aiven.io>

--- coral/src/services/api.ts
+++ coral/src/services/api.ts
@@ -109,8 +109,7 @@ const API_PATHS: { [key in keyof ApiOperations]: keyof ApiPaths } = {
   testClusterApiConnection: "/testClusterApiConnection",
   shutdownApp: "/shutdownContext",
   showUsers: "/showUserList",
-  resetMemoryCache:
-    "/resetMemoryCache/{tenantId}/{entityType}/{operationType}",
+  resetMemoryCache: "/resetMemoryCache/{tenantId}/{entityType}/{operationType}",
   resetCache: "/resetCache",
   getRequestStatistics: "/requests/statistics",
   getRegistrationInfoFromId: "/getUserInfoFromRegistrationId",
