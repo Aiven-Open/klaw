157a00c31977460d69b1ae4ba7d34cac3da3e716
Mathieu Anderson <mathieu.anderson@aiven.io>
2023-06-23 11:51:19 +0200

    feat(coral): Add UI for TopicSubscriptionsDetailsModal
    Signed-off-by: Mathieu Anderson <mathieu.anderson@aiven.io>

--- coral/src/app/features/topics/details/subscriptions/components/TopicSubscriptionsDetailsModal.test.tsx
+++ coral/src/app/features/topics/details/subscriptions/components/TopicSubscriptionsDetailsModal.test.tsx
@@ -1,14 +1,21 @@
-import { cleanup } from "@testing-library/react";
+import {
+  cleanup,
+  screen,
+  waitForElementToBeRemoved,
+} from "@testing-library/react";
 import TopicSubscriptionsDetailsModal from "src/app/features/topics/details/subscriptions/components/TopicSubscriptionsDetailsModal";
-import { AclOverviewInfo } from "src/domain/topic/topic-types";
-import { customRender } from "src/services/test-utils/render-with-wrappers";
-
 import {
   getAivenServiceAccountDetails,
   getConsumerOffsets,
 } from "src/domain/acl/acl-api";
+import { AuthUser } from "src/domain/auth-user";
+import { AclOverviewInfo } from "src/domain/topic/topic-types";
+import { customRender } from "src/services/test-utils/render-with-wrappers";
 
 jest.mock("src/domain/acl/acl-api");
+jest.mock("src/app/context-provider/AuthProvider", () => ({
+  useAuthContext: () => mockAuthUser(),
+}));
 
 const mockGetConsumerOffsets = getConsumerOffsets as jest.MockedFunction<
   typeof getConsumerOffsets
@@ -17,8 +24,15 @@ const mockGetAivenServiceAccountDetails =
   getAivenServiceAccountDetails as jest.MockedFunction<
     typeof getAivenServiceAccountDetails
   >;
-
+const mockAuthUser = jest.fn();
 const mockCloseDetailsModal = jest.fn();
+
+const authUser: AuthUser = {
+  canSwitchTeams: "false",
+  teamId: "2",
+  teamname: "awesome-bunch-of-people",
+  username: "i-am-test-user",
+};
 
 const testServiceAccountData = {
   username: "nkira",
@@ -33,7 +47,7 @@ const testOffsetsData = {
   lag: "0",
 };
 
-const testSelectedSub: AclOverviewInfo = {
+const testSelectedSubAiven: AclOverviewInfo = {
   req_no: "1006",
   acl_ssl: "aivtopic3user",
   topicname: "aivtopic3",
@@ -47,29 +61,152 @@ const testSelectedSub: AclOverviewInfo = {
   showDeleteAcl: true,
   kafkaFlavorType: "AIVEN_FOR_APACHE_KAFKA",
 };
+const testSelectedSubNonAiven: AclOverviewInfo = {
+  req_no: "1006",
+  acl_ip: "0.0.0.0",
+  topicname: "aivtopic3",
+  topictype: "Consumer",
+  consumergroup: "-na-",
+  environment: "1",
+  environmentName: "DEV",
+  teamname: "Ospo",
+  teamid: 1003,
+  aclPatternType: "IP",
+  showDeleteAcl: true,
+  kafkaFlavorType: "APACHE_KAFKA",
+};
 
-const defaultProps = {
+const defaultPropsAiven = {
   closeDetailsModal: mockCloseDetailsModal,
   isAivenCluster: true,
-  selectedSub: testSelectedSub,
-  offsetsData: testOffsetsData,
+  selectedSub: testSelectedSubAiven,
   serviceAccountData: testServiceAccountData,
+};
+
+const defaultPropsNonAiven = {
+  closeDetailsModal: mockCloseDetailsModal,
+  isAivenCluster: false,
+  selectedSub: testSelectedSubNonAiven,
+  offsetsData: testOffsetsData,
+};
+
+const findDefinition = (term?: string) => {
+  return screen
+    .getAllByRole("definition")
+    .find((value) => value.textContent === term);
+};
+
+const findTerm = (term: string) => {
+  return screen
+    .getAllByRole("term")
+    .find((value) => value.textContent === term);
 };
 
 describe("TopicSubscriptionsDetailsModal.tsx", () => {
   beforeAll(() => {
-    mockGetConsumerOffsets.mockResolvedValue([testOffsetsData]);
-    mockGetAivenServiceAccountDetails.mockResolvedValue(testServiceAccountData);
+    mockAuthUser.mockReturnValue(authUser);
   });
 
-  afterAll(() => {
+  afterEach(() => {
     cleanup();
   });
 
-  // @TODO finish testing in PR handling proper display of the data
-  it("should render correct data in details modal", async () => {
-    customRender(<TopicSubscriptionsDetailsModal {...defaultProps} />, {
+  it("should render correct data in details modal (Aiven)", async () => {
+    mockGetAivenServiceAccountDetails.mockResolvedValue(testServiceAccountData);
+
+    customRender(<TopicSubscriptionsDetailsModal {...defaultPropsAiven} />, {
       queryClient: true,
     });
+
+    await waitForElementToBeRemoved(screen.getByTestId("pw-skeleton"));
+
+    expect(findTerm("Environment")).toBeVisible();
+    expect(
+      findDefinition(defaultPropsAiven.selectedSub.environmentName)
+    ).toBeVisible();
+
+    expect(findTerm("Subscription type")).toBeVisible();
+    expect(
+      findDefinition(defaultPropsAiven.selectedSub.topictype.toUpperCase())
+    ).toBeVisible();
+
+    expect(findTerm("Pattern type")).toBeVisible();
+    expect(
+      findDefinition(defaultPropsAiven.selectedSub.aclPatternType)
+    ).toBeVisible();
+
+    expect(findTerm("Topic name")).toBeVisible();
+    expect(
+      findDefinition(defaultPropsAiven.selectedSub.topicname)
+    ).toBeVisible();
+
+    expect(findTerm("Consumer group")).toBeVisible();
+    expect(
+      findDefinition(defaultPropsAiven.selectedSub.consumergroup)
+    ).toBeVisible();
+
+    expect(findTerm("IP or Service account based")).toBeVisible();
+    expect(findDefinition("Service account")).toBeVisible();
+
+    expect(findTerm("Service account")).toBeVisible();
+    expect(findDefinition(defaultPropsAiven.selectedSub.acl_ssl)).toBeVisible();
+
+    expect(findTerm("Service account password")).toBeVisible();
+    expect(
+      findDefinition(defaultPropsAiven.serviceAccountData.password)
+    ).toBeVisible();
+
+    expect(screen.queryByText("Consumer offset")).not.toBeInTheDocument();
+  });
+
+  it("should render correct data in details modal (non Aiven consumer)", async () => {
+    mockGetConsumerOffsets.mockResolvedValue([testOffsetsData]);
+
+    customRender(<TopicSubscriptionsDetailsModal {...defaultPropsNonAiven} />, {
+      queryClient: true,
+    });
+
+    await waitForElementToBeRemoved(screen.getByTestId("offsets-skeleton"));
+
+    expect(findTerm("Environment")).toBeVisible();
+    expect(
+      findDefinition(defaultPropsNonAiven.selectedSub.environmentName)
+    ).toBeVisible();
+
+    expect(findTerm("Subscription type")).toBeVisible();
+    expect(
+      findDefinition(defaultPropsNonAiven.selectedSub.topictype.toUpperCase())
+    ).toBeVisible();
+
+    expect(findTerm("Pattern type")).toBeVisible();
+    expect(
+      findDefinition(defaultPropsNonAiven.selectedSub.aclPatternType)
+    ).toBeVisible();
+
+    expect(findTerm("Topic name")).toBeVisible();
+    expect(
+      findDefinition(defaultPropsNonAiven.selectedSub.topicname)
+    ).toBeVisible();
+
+    expect(findTerm("Consumer group")).toBeVisible();
+    expect(
+      findDefinition(defaultPropsNonAiven.selectedSub.consumergroup)
+    ).toBeVisible();
+
+    expect(findTerm("IP or Principal based")).toBeVisible();
+    expect(findDefinition("IP")).toBeVisible();
+
+    expect(findTerm("IP")).toBeVisible();
+    expect(
+      findDefinition(defaultPropsNonAiven.selectedSub.acl_ip)
+    ).toBeVisible();
+
+    expect(
+      screen.getByText("Partition 0 | Current offset 0 | End offset 0 | Lag 0")
+    ).toBeVisible();
+
+    expect(
+      screen.queryByText("Service account password")
+    ).not.toBeInTheDocument();
   });
 });
--- coral/src/app/features/topics/details/subscriptions/components/TopicSubscriptionsDetailsModal.tsx
+++ coral/src/app/features/topics/details/subscriptions/components/TopicSubscriptionsDetailsModal.tsx
@@ -1,4 +1,15 @@
+import {
+  Alert,
+  Box,
+  Divider,
+  Grid,
+  GridItem,
+  Skeleton,
+  StatusChip,
+  Typography,
+} from "@aivenio/aquarium";
 import { useQuery } from "@tanstack/react-query";
+import { useEffect, useState } from "react";
 import { Modal } from "src/app/components/Modal";
 import { useAuthContext } from "src/app/context-provider/AuthProvider";
 import {
@@ -10,6 +21,8 @@ import {
   ServiceAccountDetails,
 } from "src/domain/acl/acl-types";
 import { AclOverviewInfo } from "src/domain/topic/topic-types";
+import { HTTPError } from "src/services/api";
+import { parseErrorMsg } from "src/services/mutation-utils";
 
 interface TopicSubscriptionsDetailsModalProps {
   closeDetailsModal: () => void;
@@ -25,32 +38,44 @@ const TopicSubscriptionsDetailsModal = ({
   selectedSub,
 }: TopicSubscriptionsDetailsModalProps) => {
   const user = useAuthContext();
-  const { environment, consumergroup = "", topicname, req_no } = selectedSub;
+  const {
+    environment,
+    consumergroup,
+    topicname,
+    req_no,
+    topictype,
+    environmentName,
+    acl_ssl,
+    acl_ip,
+    aclPatternType,
+  } = selectedSub;
+
+  const [errors, setErrors] = useState<string[]>([]);
 
   const {
     data: offsetsData,
-    // @TODO: handle loading / error state gracefully
-    // isError: offsetsIsError,
-    // error: offsetsError,
-    // isFetched: offsetDataFetched,
-  } = useQuery(["consumer-offsets", environment, consumergroup], {
-    queryFn: () => {
-      return getConsumerOffsets({
-        topicName: topicname,
-        env: environment,
-        consumerGroupId: consumergroup,
-      });
-    },
-    enabled: environment !== undefined && consumergroup !== undefined,
-  });
+    error: offsetsError,
+    isFetched: offsetsDataFetched,
+  } = useQuery<ConsumerOffsets[], HTTPError>(
+    ["consumer-offsets", topicname, environment, consumergroup],
+    {
+      queryFn: () => {
+        return getConsumerOffsets({
+          topicName: topicname,
+          env: environment,
+          consumerGroupId: consumergroup || "",
+        });
+      },
+      // Offsets data is only available for Consumer subscriptions in non-Aiven clusters
+      enabled: topictype === "Consumer" && !isAivenCluster,
+    }
+  );
 
   const {
     data: serviceAccountData,
-    // @TODO: handle loading / error state gracefully
-    // isError: serviceAccountIsError,
-    // error: serviceAccountError,
-    // isFetched: serviceAccountDataFetched,
-  } = useQuery(
+    error: serviceAccountError,
+    isFetched: serviceAccountDataFetched,
+  } = useQuery<ServiceAccountDetails, HTTPError>(
     [
       "getAivenServiceAccountDetails",
       environment,
@@ -67,9 +92,28 @@ const TopicSubscriptionsDetailsModal = ({
           aclReqNo: req_no,
         });
       },
-      enabled: environment !== undefined && req_no !== undefined,
+      // Service accout data is only available for Aiven clusters
+      enabled: isAivenCluster,
     }
   );
+
+  useEffect(() => {
+    if (offsetsError !== null) {
+      setErrors((prev) => [...prev, parseErrorMsg(offsetsError)]);
+    }
+    if (serviceAccountError !== null) {
+      setErrors((prev) => [...prev, parseErrorMsg(serviceAccountError)]);
+    }
+  }, [offsetsError, serviceAccountError]);
+
+  const serviceAccountDataLoaded =
+    serviceAccountDataFetched && serviceAccountData !== undefined;
+
+  const offsetsDataLoaded = offsetsDataFetched && offsetsData !== undefined;
+
+  const serviceAccountOrPrincipalText = isAivenCluster
+    ? "Service account"
+    : "Principal";
 
   return (
     <Modal
@@ -80,46 +124,125 @@ const TopicSubscriptionsDetailsModal = ({
       }}
       close={closeDetailsModal}
     >
-      <>
-        <p>isAivenClusetr: {String(isAivenCluster)}</p>
-        <p>Env: {selectedSub.environmentName}</p>
-        <p>ACL type: {selectedSub.topictype}</p>
-        <p>ACL pattern: {selectedSub.aclPatternType}</p>
-        <p>Topic name: {selectedSub.topicname}</p>
-        <p>Consumer group: {selectedSub.consumergroup}</p>
-        <p>
-          {isAivenCluster ? "Service accounts" : "Principals"}:{" "}
-          {selectedSub.acl_ssl}
-        </p>
-        <p>
-          {selectedSub.acl_ip === undefined
-            ? null
-            : `IP: ${selectedSub.acl_ip}`}
-        </p>
-        <p>
-          {isAivenCluster
-            ? `Service account password: ${
-                serviceAccountData?.password || "Loading"
-              }`
-            : null}
-        </p>
-        <p>
-          {!isAivenCluster &&
-          selectedSub.topictype === "Consumer" &&
-          offsetsData !== undefined &&
-          offsetsData[0] !== undefined
-            ? `Consumer offset: Partition ${
-                offsetsData[0]?.topicPartitionId || "Loading"
-              } |
-              Current offset ${
-                offsetsData[0]?.currentOffset || "Loading"
-              } | End offset
-              ${offsetsData[0]?.endOffset || "Loading"} | Lag ${
-                offsetsData[0]?.lag || "Loading"
-              }`
-            : null}
-        </p>
-      </>
+      <Grid htmlTag={"dl"} cols={"2"} rowGap={"6"}>
+        {errors.length > 0 && (
+          <GridItem colSpan={"span-2"}>
+            <Alert type="error">
+              <Box.Flex flexDirection={"column"}>
+                {errors.map((error) => (
+                  <div key={error}>{error}</div>
+                ))}
+              </Box.Flex>
+            </Alert>
+          </GridItem>
+        )}
+
+        <GridItem colSpan={"span-2"}>
+          <Box.Flex flexDirection={"column"} width={"min"}>
+            <Typography.SmallStrong htmlTag={"dt"} color={"grey-60"}>
+              Environment
+            </Typography.SmallStrong>
+            <Typography.Default htmlTag="dd">
+              {environmentName}
+            </Typography.Default>
+          </Box.Flex>
+        </GridItem>
+        <GridItem colSpan={"span-2"}>
+          <Divider />
+        </GridItem>
+        <Box.Flex flexDirection={"column"}>
+          <Typography.SmallStrong htmlTag={"dt"} color={"grey-60"}>
+            Subscription type
+          </Typography.SmallStrong>
+          <Typography.Default htmlTag="dd">
+            <StatusChip
+              status={topictype === "Consumer" ? "success" : "info"}
+              text={topictype.toUpperCase()}
+            />
+          </Typography.Default>
+        </Box.Flex>
+        <Box.Flex flexDirection={"column"}>
+          <Typography.SmallStrong htmlTag={"dt"} color={"grey-60"}>
+            Pattern type
+          </Typography.SmallStrong>
+          <Typography.Default htmlTag="dd">{aclPatternType}</Typography.Default>
+        </Box.Flex>
+        <GridItem colSpan={"span-2"}>
+          <Divider />
+        </GridItem>
+        <Box.Flex flexDirection={"column"} width={"min"}>
+          <Typography.SmallStrong htmlTag={"dt"} color={"grey-60"}>
+            Topic name
+          </Typography.SmallStrong>
+          <Typography.Default htmlTag="dd">{topicname}</Typography.Default>
+        </Box.Flex>
+        <Box.Flex flexDirection={"column"}>
+          <Typography.SmallStrong htmlTag={"dt"} color={"grey-60"}>
+            Consumer group
+          </Typography.SmallStrong>
+          <Typography.Default htmlTag="dd">{consumergroup}</Typography.Default>
+        </Box.Flex>
+
+        <Box.Flex flexDirection={"column"}>
+          <Typography.SmallStrong
+            htmlTag={"dt"}
+            color={"grey-60"}
+          >{`IP or ${serviceAccountOrPrincipalText} based`}</Typography.SmallStrong>
+          <Typography.Default htmlTag="dd">
+            {acl_ip === undefined ? serviceAccountOrPrincipalText : "IP"}
+          </Typography.Default>
+        </Box.Flex>
+        <Box.Flex flexDirection={"column"}>
+          <Typography.SmallStrong htmlTag={"dt"} color={"grey-60"}>
+            {acl_ip === undefined ? serviceAccountOrPrincipalText : "IP"}
+          </Typography.SmallStrong>
+          <Typography.Default htmlTag="dd">
+            <StatusChip
+              status={"neutral"}
+              text={acl_ip || acl_ssl || "Not found"}
+            />
+          </Typography.Default>
+        </Box.Flex>
+        {isAivenCluster ? (
+          <GridItem colSpan={"span-2"}>
+            <Box.Flex flexDirection={"column"}>
+              <Typography.SmallStrong htmlTag={"dt"} color={"grey-60"}>
+                Service account password
+              </Typography.SmallStrong>
+              {serviceAccountDataLoaded ? (
+                <Typography.Default htmlTag="dd">
+                  {serviceAccountData.password}
+                </Typography.Default>
+              ) : (
+                <div data-testid={"pw-skeleton"}>
+                  <Skeleton height={25} width={250} />
+                </div>
+              )}
+            </Box.Flex>
+          </GridItem>
+        ) : null}
+        {!isAivenCluster && selectedSub.topictype === "Consumer" ? (
+          <GridItem colSpan={"span-2"}>
+            <Box.Flex flexDirection={"column"}>
+              <Typography.SmallStrong htmlTag={"dt"} color={"grey-60"}>
+                Consumer offset
+              </Typography.SmallStrong>
+              {offsetsDataLoaded ? (
+                <Typography.Default htmlTag="dd">
+                  {`Partition ${offsetsData[0].topicPartitionId} |
+                  Current offset ${offsetsData[0].currentOffset} |
+                  End offset ${offsetsData[0].endOffset} |
+                  Lag ${offsetsData[0].lag}`}
+                </Typography.Default>
+              ) : (
+                <div data-testid={"offsets-skeleton"}>
+                  <Skeleton height={25} width={350} />
+                </div>
+              )}
+            </Box.Flex>
+          </GridItem>
+        ) : null}
+      </Grid>
     </Modal>
   );
 };
