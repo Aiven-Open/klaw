94e74088b9c9a49e6b411dd98e07b9cd0ca377d6
Aindriu Lavelle <aindriu.lavelle@aiven.io>
2023-07-11 12:25:48 +0100

    Add a check to make sure only to return teams with valid team names
    Signed-off-by: Aindriu Lavelle <aindriu.lavelle@aiven.io>

--- core/src/main/java/io/aiven/klaw/service/UsersTeamsControllerService.java
+++ core/src/main/java/io/aiven/klaw/service/UsersTeamsControllerService.java
@@ -63,6 +63,7 @@ import java.util.regex.Matcher;
 import java.util.regex.Pattern;
 import java.util.stream.Collectors;
 import lombok.extern.slf4j.Slf4j;
+import org.apache.commons.lang3.StringUtils;
 import org.apache.commons.lang3.tuple.Pair;
 import org.jasypt.util.text.BasicTextEncryptor;
 import org.springframework.beans.factory.annotation.Autowired;
@@ -1154,7 +1155,9 @@ public class UsersTeamsControllerService {
           teamModel.setTeamId(switchAllowedTeamId);
           teamModel.setTeamname(
               manageDatabase.getTeamNameFromTeamId(tenantId, switchAllowedTeamId));
-          teamModelList.add(teamModel);
+          if (!StringUtils.isEmpty(teamModel.getTeamname())) {
+            teamModelList.add(teamModel);
+          }
         }
       }
     }
