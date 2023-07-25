5257c45d4b9404f9199940a2bd74f00439ccb8b1
Mirjam Aulbach <mirjam.aulbach@aiven.io>
2023-07-25 14:26:33 +0200

    Add promotion banner component.
    Signed-off-by: Mirjam Aulbach <mirjam.aulbach@aiven.io>

---
+++ coral/src/app/features/topics/details/components/PromotionBanner.test.tsx
@@ -0,0 +1,301 @@
+import { KlawApiModel } from "types/utils";
+import { cleanup, render, screen } from "@testing-library/react";
+import { PromotionBanner } from "src/app/features/topics/details/components/PromotionBanner";
+
+const promotionDetails: KlawApiModel<"PromotionStatus"> = {
+  status: "SUCCESS",
+  targetEnv: "TST",
+  sourceEnv: "DEV",
+  targetEnvId: "2",
+  topicName: "my-test-topic",
+};
+describe("PromotionBanner", () => {
+  describe("does not show a promotion banner at all dependent on promotion details", () => {
+    afterEach(cleanup);
+
+    it("returns null if status is NOT_AUTHORIZED", () => {
+      const { container } = render(
+        <PromotionBanner
+          promotionDetails={{ ...promotionDetails, status: "NOT_AUTHORIZED" }}
+          type={"schema"}
+          promoteElement={<></>}
+          hasOpenRequest={false}
+        />
+      );
+
+      expect(container).toBeEmptyDOMElement();
+    });
+
+    it("returns null if status is NO_PROMOTION", () => {
+      const { container } = render(
+        <PromotionBanner
+          promotionDetails={{ ...promotionDetails, status: "NO_PROMOTION" }}
+          type={"schema"}
+          promoteElement={<></>}
+          hasOpenRequest={false}
+        />
+      );
+
+      expect(container).toBeEmptyDOMElement();
+    });
+
+    it("returns null if status is FAILURE", () => {
+      const { container } = render(
+        <PromotionBanner
+          promotionDetails={{ ...promotionDetails, status: "FAILURE" }}
+          type={"schema"}
+          promoteElement={<></>}
+          hasOpenRequest={false}
+        />
+      );
+
+      expect(container).toBeEmptyDOMElement();
+    });
+
+    it("returns null if targetEnv is undefined", () => {
+      const { container } = render(
+        <PromotionBanner
+          promotionDetails={{ ...promotionDetails, targetEnv: undefined }}
+          type={"schema"}
+          promoteElement={<></>}
+          hasOpenRequest={false}
+        />
+      );
+
+      expect(container).toBeEmptyDOMElement();
+    });
+
+    it("returns null if sourceEnv is undefined", () => {
+      const { container } = render(
+        <PromotionBanner
+          promotionDetails={{ ...promotionDetails, sourceEnv: undefined }}
+          type={"schema"}
+          promoteElement={<></>}
+          hasOpenRequest={false}
+        />
+      );
+
+      expect(container).toBeEmptyDOMElement();
+    });
+
+    it("returns null if targetEnvId is undefined", () => {
+      const { container } = render(
+        <PromotionBanner
+          promotionDetails={{ ...promotionDetails, targetEnvId: undefined }}
+          type={"schema"}
+          promoteElement={<></>}
+          hasOpenRequest={false}
+        />
+      );
+
+      expect(container).toBeEmptyDOMElement();
+    });
+
+    it("returns null if topicName is undefined", () => {
+      const { container } = render(
+        <PromotionBanner
+          promotionDetails={{ ...promotionDetails, topicName: undefined }}
+          type={"schema"}
+          promoteElement={<></>}
+          hasOpenRequest={false}
+        />
+      );
+
+      expect(container).toBeEmptyDOMElement();
+    });
+  });
+
+  describe("handles banner for entity with an open request (type schema)", () => {
+    beforeAll(() => {
+      render(
+        <PromotionBanner
+          promotionDetails={promotionDetails}
+          type={"schema"}
+          promoteElement={<></>}
+          hasOpenRequest={true}
+        />
+      );
+    });
+
+    afterAll(cleanup);
+
+    it("shows information about the open request", () => {
+      const information = screen.getByText(
+        `There is an open request for ${promotionDetails.topicName}.`
+      );
+
+      expect(information).toBeVisible();
+    });
+
+    it("shows a link to open requests", () => {
+      const link = screen.getByText("See the request");
+
+      expect(link).toBeVisible();
+      expect(link).toHaveAttribute(
+        "href",
+        "/requests/schemas?search=my-test-topic&status=CREATED&page=1"
+      );
+    });
+  });
+
+  describe("handles banner for entity with an open request (type topic)", () => {
+    beforeAll(() => {
+      render(
+        <PromotionBanner
+          promotionDetails={promotionDetails}
+          type={"topic"}
+          promoteElement={<></>}
+          hasOpenRequest={true}
+        />
+      );
+    });
+
+    afterAll(cleanup);
+
+    it("shows information about the open request", () => {
+      const information = screen.getByText(
+        `There is an open request for ${promotionDetails.topicName}.`
+      );
+
+      expect(information).toBeVisible();
+    });
+
+    it("shows a link to open requests", () => {
+      const link = screen.getByText("See the request");
+
+      expect(link).toBeVisible();
+      expect(link).toHaveAttribute(
+        "href",
+        "/requests/topics?search=my-test-topic&status=CREATED&page=1"
+      );
+    });
+  });
+
+  describe("handles banner for entity with an open promotion request (type schema)", () => {
+    beforeAll(() => {
+      render(
+        <PromotionBanner
+          promotionDetails={{ ...promotionDetails, status: "REQUEST_OPEN" }}
+          type={"schema"}
+          promoteElement={<></>}
+          hasOpenRequest={false}
+        />
+      );
+    });
+
+    afterAll(cleanup);
+
+    it("shows information about the open request", () => {
+      const information = screen.getByText(
+        `There is already an open promotion request for ${promotionDetails.topicName}.`
+      );
+
+      expect(information).toBeVisible();
+    });
+
+    it("shows a link to open requests", () => {
+      const link = screen.getByText("See the request");
+
+      expect(link).toBeVisible();
+      expect(link).toHaveAttribute(
+        "href",
+        "/requests/schemas?search=my-test-topic&requestType=PROMOTE&status=CREATED&page=1"
+      );
+    });
+  });
+
+  describe("handles banner for entity with an open promotion request (type topic)", () => {
+    beforeAll(() => {
+      render(
+        <PromotionBanner
+          promotionDetails={{ ...promotionDetails, status: "REQUEST_OPEN" }}
+          type={"topic"}
+          promoteElement={<></>}
+          hasOpenRequest={false}
+        />
+      );
+    });
+
+    afterAll(cleanup);
+
+    it("shows information about the open request", () => {
+      const information = screen.getByText(
+        `There is already an open promotion request for ${promotionDetails.topicName}.`
+      );
+
+      expect(information).toBeVisible();
+    });
+
+    it("shows a link to open requests", () => {
+      const link = screen.getByText("See the request");
+
+      expect(link).toBeVisible();
+      expect(link).toHaveAttribute(
+        "href",
+        "/requests/topics?search=my-test-topic&requestType=PROMOTE&status=CREATED&page=1"
+      );
+    });
+  });
+
+  describe("handles banner for entity that can be promoted (type schema)", () => {
+    const promoteElement = <div data-testid={"another-test-promote-element"} />;
+    beforeAll(() => {
+      render(
+        <PromotionBanner
+          promotionDetails={promotionDetails}
+          type={"schema"}
+          promoteElement={promoteElement}
+          hasOpenRequest={false}
+        />
+      );
+    });
+
+    afterAll(cleanup);
+
+    it("shows information about possible promotion", () => {
+      const information = screen.getByText(
+        `This schema has not yet been promoted to the ${promotionDetails.targetEnv} environment.`
+      );
+
+      expect(information).toBeVisible();
+    });
+
+    it("renders a given component as element handling the promotion", () => {
+      const promotionElement = screen.getByTestId(
+        "another-test-promote-element"
+      );
+
+      expect(promotionElement).toBeVisible();
+    });
+  });
+
+  describe("handles banner for entity that can be promoted (type topic)", () => {
+    const promoteElement = <div data-testid={"test-promote-element"} />;
+    beforeAll(() => {
+      render(
+        <PromotionBanner
+          promotionDetails={promotionDetails}
+          type={"topic"}
+          promoteElement={promoteElement}
+          hasOpenRequest={false}
+        />
+      );
+    });
+
+    afterAll(cleanup);
+
+    it("shows information about possible promotion", () => {
+      const information = screen.getByText(
+        `This topic has not yet been promoted to the ${promotionDetails.targetEnv} environment.`
+      );
+
+      expect(information).toBeVisible();
+    });
+
+    it("renders a given component as element handling the promotion", () => {
+      const promotionElement = screen.getByTestId("test-promote-element");
+
+      expect(promotionElement).toBeVisible();
+    });
+  });
+});
---
+++ coral/src/app/features/topics/details/components/PromotionBanner.tsx
@@ -0,0 +1,83 @@
+import { Banner, Box, Button, GridItem } from "@aivenio/aquarium";
+import illustration from "src/app/images/topic-details-schema-Illustration.svg";
+import { ReactElement } from "react";
+import { KlawApiModel } from "types/utils";
+
+interface PromotionBannerProps {
+  promotionDetails: KlawApiModel<"PromotionStatus">;
+  type: "schema" | "topic";
+  promoteElement: ReactElement;
+  hasOpenRequest: boolean;
+}
+
+const PromotionBanner = ({
+  promotionDetails,
+  hasOpenRequest,
+  type,
+  promoteElement,
+}: PromotionBannerProps) => {
+  const { status, sourceEnv, targetEnv, topicName, targetEnvId } =
+    promotionDetails;
+
+  // if any of these is not true,
+  // the entity cannot be promoted
+  const isPromotable =
+    status !== "NOT_AUTHORIZED" &&
+    status !== "NO_PROMOTION" &&
+    status !== "FAILURE" &&
+    targetEnv !== undefined &&
+    sourceEnv !== undefined &&
+    targetEnvId !== undefined &&
+    topicName !== undefined;
+
+  const hasOpenPromotionRequest = status === "REQUEST_OPEN";
+
+  if (!isPromotable) return null;
+
+  if (hasOpenRequest) {
+    return (
+      <GridItem colSpan={"span-2"}>
+        <Banner image={illustration} layout="vertical" title={""}>
+          <Box component={"p"} marginBottom={"l1"}>
+            There is an open request for {topicName}.
+          </Box>
+          <Button.ExternalLink
+            href={`/requests/${type}s?search=${topicName}&status=CREATED&page=1`}
+          >
+            See the request
+          </Button.ExternalLink>
+        </Banner>
+      </GridItem>
+    );
+  }
+
+  if (hasOpenPromotionRequest) {
+    return (
+      <GridItem colSpan={"span-2"}>
+        <Banner image={illustration} layout="vertical" title={""}>
+          <Box component={"p"} marginBottom={"l1"}>
+            There is already an open promotion request for {topicName}.
+          </Box>
+          <Button.ExternalLink
+            href={`/requests/${type}s?search=${topicName}&requestType=PROMOTE&status=CREATED&page=1`}
+          >
+            See the request
+          </Button.ExternalLink>
+        </Banner>
+      </GridItem>
+    );
+  }
+
+  return (
+    <GridItem colSpan={"span-2"}>
+      <Banner image={illustration} layout="vertical" title={""}>
+        <Box component={"p"} marginBottom={"l1"}>
+          This {type} has not yet been promoted to the {targetEnv} environment.
+        </Box>
+        {promoteElement}
+      </Banner>
+    </GridItem>
+  );
+};
+
+export { PromotionBanner };
