892d3c9d4d7a99c0ecdc58c2dd7ae63414fb15a9
Mirjam Aulbach <mirjam.aulbach@aiven.io>
2023-07-27 14:33:32 +0200

    Add PromotionStatus as separate domain
    Signed-off-by: Mirjam Aulbach <mirjam.aulbach@aiven.io>

--- coral/src/app/features/topics/details/components/PromotionBanner.tsx
+++ coral/src/app/features/topics/details/components/PromotionBanner.tsx
@@ -1,8 +1,8 @@
-import { Banner, Box, Button, GridItem } from "@aivenio/aquarium";
+import { Banner, Box, Button } from "@aivenio/aquarium";
 import illustration from "src/app/images/topic-details-schema-Illustration.svg";
 import { ReactElement } from "react";
-import { KlawApiModel } from "types/utils";
 import { useNavigate } from "react-router-dom";
+import { PromotionStatus } from "src/domain/promotion";
 
 interface PromotionBannerProps {
   // `entityName` is only optional on
@@ -10,7 +10,7 @@ interface PromotionBannerProps {
   // so we can't rely on it to be part of
   // the promotionDetails
   entityName: string;
-  promotionDetails: KlawApiModel<"PromotionStatus">;
+  promotionDetails: PromotionStatus;
   type: "schema" | "topic";
   promoteElement: ReactElement;
   hasOpenRequest: boolean;
@@ -43,65 +43,59 @@ const PromotionBanner = ({
 
   if (hasOpenRequest) {
     return (
-      <GridItem colSpan={"span-2"}>
-        <Banner image={illustration} layout="vertical" title={""}>
-          <Box component={"p"} marginBottom={"l1"}>
-            There is an open {type} request for {entityName}.
-          </Box>
-          {/*@ TODO DS external link does not support */}
-          {/*  internal links and we don't have a component*/}
-          {/*  from DS that supports that yet. We've to use a */}
-          {/*  button that calls navigate() onClick to support*/}
-          {/*  routing in Coral*/}
-          <Button.Primary
-            onClick={() =>
-              navigate(
-                `/requests/${type}s?search=${entityName}&status=CREATED&page=1`
-              )
-            }
-          >
-            See the request
-          </Button.Primary>
-        </Banner>
-      </GridItem>
+      <Banner image={illustration} layout="vertical" title={""}>
+        <Box component={"p"} marginBottom={"l1"}>
+          There is an open {type} request for {entityName}.
+        </Box>
+        {/*@ TODO DS external link does not support */}
+        {/*  internal links and we don't have a component*/}
+        {/*  from DS that supports that yet. We've to use a */}
+        {/*  button that calls navigate() onClick to support*/}
+        {/*  routing in Coral*/}
+        <Button.Primary
+          onClick={() =>
+            navigate(
+              `/requests/${type}s?search=${entityName}&status=CREATED&page=1`
+            )
+          }
+        >
+          See the request
+        </Button.Primary>
+      </Banner>
     );
   }
 
   if (hasOpenPromotionRequest) {
     return (
-      <GridItem colSpan={"span-2"}>
-        <Banner image={illustration} layout="vertical" title={""}>
-          <Box component={"p"} marginBottom={"l1"}>
-            There is already an open promotion request for {entityName}.
-          </Box>
-          {/*@ TODO DS external link does not support */}
-          {/*  internal links and we don't have a component*/}
-          {/*  from DS that supports that yet. We've to use a */}
-          {/*  button that calls navigate() onClick to support*/}
-          {/*  routing in Coral*/}
-          <Button.Primary
-            onClick={() =>
-              navigate(
-                `/requests/${type}s?search=${entityName}&requestType=PROMOTE&status=CREATED&page=1`
-              )
-            }
-          >
-            See the request
-          </Button.Primary>
-        </Banner>
-      </GridItem>
+      <Banner image={illustration} layout="vertical" title={""}>
+        <Box component={"p"} marginBottom={"l1"}>
+          There is already an open promotion request for {entityName}.
+        </Box>
+        {/*@ TODO DS external link does not support */}
+        {/*  internal links and we don't have a component*/}
+        {/*  from DS that supports that yet. We've to use a */}
+        {/*  button that calls navigate() onClick to support*/}
+        {/*  routing in Coral*/}
+        <Button.Primary
+          onClick={() =>
+            navigate(
+              `/requests/${type}s?search=${entityName}&requestType=PROMOTE&status=CREATED&page=1`
+            )
+          }
+        >
+          See the request
+        </Button.Primary>
+      </Banner>
     );
   }
 
   return (
-    <GridItem colSpan={"span-2"}>
-      <Banner image={illustration} layout="vertical" title={""}>
-        <Box component={"p"} marginBottom={"l1"}>
-          This {type} has not yet been promoted to the {targetEnv} environment.
-        </Box>
-        {promoteElement}
-      </Banner>
-    </GridItem>
+    <Banner image={illustration} layout="vertical" title={""}>
+      <Box component={"p"} marginBottom={"l1"}>
+        This {type} has not yet been promoted to the {targetEnv} environment.
+      </Box>
+      {promoteElement}
+    </Banner>
   );
 };
 
