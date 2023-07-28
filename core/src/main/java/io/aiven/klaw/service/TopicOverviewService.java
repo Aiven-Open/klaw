14c30dcf4dea8e4f7efbf91adb87d7947d2c0964
Aindriu Lavelle <aindriu.lavelle@aiven.io>
2023-07-28 15:32:15 +0100

    Minor update so that if a request is open we let the UI know without divulging the type of request.
    Signed-off-by: Aindriu Lavelle <aindriu.lavelle@aiven.io>

--- core/src/main/java/io/aiven/klaw/service/TopicOverviewService.java
+++ core/src/main/java/io/aiven/klaw/service/TopicOverviewService.java
@@ -220,6 +220,15 @@ public class TopicOverviewService extends BaseOverviewService {
             || topicInfo.isHasOpenClaimRequest());
   }
 
+  private void setHasOpenRequestOnly(
+      TopicOverviewInfo topicInfo, String topicName, String envId, int tenantId) {
+    topicInfo.setHasOpenRequest(
+        isACLRequestOpen(topicName, envId, tenantId)
+            || isSchemaRequestOpen(topicName, envId, tenantId)
+            || isTopicRequestOpen(topicName, envId, tenantId)
+            || isClaimTopicRequestOpen(topicName, tenantId));
+  }
+
   private void setHasSchema(
       TopicOverviewInfo topicInfo, String topicName, String envId, int tenantId) {
     topicInfo.setHasSchema(commonUtilsService.existsSchemaForTopic(topicName, envId, tenantId));
@@ -337,6 +346,14 @@ public class TopicOverviewService extends BaseOverviewService {
           topicInfoList
               .get(0)
               .setHasOpenClaimRequest(isClaimTopicRequestOpen(topicNameSearch, tenantId));
+
+          if (topicInfoList.get(0).isHasOpenClaimRequest()) {
+            log.info("SET THIS THING TO TRUE");
+            topicInfoList.get(0).setHasOpenRequest(true);
+          } else {
+            // only make call to db if it is required.
+            setHasOpenRequestOnly(topicInfoList.get(0), topicNameSearch, environmentId, tenantId);
+          }
         }
       }
     } catch (Exception e) {
