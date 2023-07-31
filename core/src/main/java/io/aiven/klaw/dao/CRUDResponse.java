a32bde8bbfd09849de06672982cf0520314b5df9
Aindriu Lavelle <aindriu.lavelle@aiven.io>
2023-07-31 15:25:21 +0100

    spotless apply
    Signed-off-by: Aindriu Lavelle <aindriu.lavelle@aiven.io>

--- core/src/main/java/io/aiven/klaw/dao/CRUDResponse.java
+++ core/src/main/java/io/aiven/klaw/dao/CRUDResponse.java
@@ -5,8 +5,9 @@ import lombok.Builder;
 import lombok.Data;
 
 /**
- * This Class allows the return of the entities that are saved and stored in the database to be returned when required to be actioned on further
- * e.g. addition or removal from caches.
+ * This Class allows the return of the entities that are saved and stored in the database to be
+ * returned when required to be actioned on further e.g. addition or removal from caches.
+ *
  * @param <T> The Object type being operated on by the database.
  */
 @Data
