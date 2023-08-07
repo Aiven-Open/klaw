3cc64cab369efb68afe166bd79df735bdc0a846f
Mirjam Aulbach <mirjam.aulbach@aiven.io>
2023-08-07 16:52:00 +0200

    refactor(coral): Update topic claim banner to use the graphic banner (#1583)
    * Rename svg file to be more generic.
    * reduce size of svg slightly to have banner without button not too big
    * Use illustration for claim topic banner
    
    ---------
    
    Signed-off-by: Mirjam Aulbach <mirjam.aulbach@aiven.io>

--- coral/src/app/features/topics/details/components/PromotionBanner.tsx
+++ coral/src/app/features/topics/details/components/PromotionBanner.tsx
@@ -1,7 +1,7 @@
 import { Alert, Banner, Box, Spacing } from "@aivenio/aquarium";
 import { ReactElement } from "react";
 import { InternalLinkButton } from "src/app/components/InternalLinkButton";
-import illustration from "src/app/images/topic-details-schema-Illustration.svg";
+import illustration from "src/app/images/topic-details-banner-Illustration.svg";
 import { PromotionStatus } from "src/domain/promotion";
 import { RequestOperationType } from "src/domain/requests/requests-types";
 
