d489482a33a1869c4728f568f8fbb31a04cb5649
Mirjam Aulbach <mirjam.aulbach@aiven.io>
2023-08-08 11:04:31 +0200

    Manually throw unauthorized error in coral if user is superadmin
    Signed-off-by: Mirjam Aulbach <mirjam.aulbach@aiven.io>

--- coral/src/domain/auth-user/auth-user-api.ts
+++ coral/src/domain/auth-user/auth-user-api.ts
@@ -2,7 +2,7 @@ import {
   AuthUser,
   AuthUserLoginData,
 } from "src/domain/auth-user/auth-user-types";
-import api, { API_PATHS } from "src/services/api";
+import api, { API_PATHS, HTTPError } from "src/services/api";
 import { paths as ApiPaths } from "types/api";
 import { KlawApiResponse } from "types/utils";
 
@@ -15,7 +15,31 @@ const getAuthUserMockForLogin = (
   return api.post("/login" as keyof ApiPaths, userLogin);
 };
 
+// user roles are added dynamically by admins, we can't make them enums
+// but "SUPERADMIN" is the default role set by us. It's highly unlikely
+// that users would change that string, so we can use it to identify
+// the superadmin role, which has different view-access
+const SUPERADMIN_USERROLE = "SUPERADMIN";
+function isSuperAdmin(user: AuthUser) {
+  return user.userrole === SUPERADMIN_USERROLE;
+}
+
 function transformAuthResponse(response: KlawApiResponse<"getAuth">): AuthUser {
+  const headers = {} as Headers;
+
+  if (isSuperAdmin(response)) {
+    // We're marking the SUPERADMIN as not authorized
+    // by manually throwing the correct error that will
+    // be caught by isUnauthorizedError
+    const error: HTTPError = {
+      data: " ",
+      headers,
+      status: 401,
+      statusText: "Not authorized to access Coral.",
+    };
+    throw error;
+  }
+
   return {
     username: response.username,
     teamname: response.teamname,
@@ -25,7 +49,7 @@ function transformAuthResponse(response: KlawApiResponse<"getAuth">): AuthUser {
   };
 }
 
-function getAuth(): Promise<AuthUser> {
+async function getAuth(): Promise<AuthUser> {
   return api
     .get<KlawApiResponse<"getAuth">>(API_PATHS.getAuth)
     .then((response) => transformAuthResponse(response));
