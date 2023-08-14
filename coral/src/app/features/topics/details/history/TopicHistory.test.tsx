c89c33312a4422ef503205f58eca474bfbe0a66a
aindriu-aiven <121855584+aindriu-aiven@users.noreply.github.com>
2023-08-14 10:26:26 +0100

    Add boolean to the TopicOverview Object   (#1598)
    * Add boolean at TopicOverview level to indicate if a schema can be created directly on an environment or only promoted into that environment.
    
    Signed-off-by: Aindriu Lavelle <aindriu.lavelle@aiven.io>
    
    * Add missed test
    
    Signed-off-by: Aindriu Lavelle <aindriu.lavelle@aiven.io>
    
    * fix possible null pointer if not set up correctly.
    
    Signed-off-by: Aindriu Lavelle <aindriu.lavelle@aiven.io>
    
    ---------
    
    Signed-off-by: Aindriu Lavelle <aindriu.lavelle@aiven.io>

--- coral/src/app/features/topics/details/history/TopicHistory.test.tsx
+++ coral/src/app/features/topics/details/history/TopicHistory.test.tsx
@@ -30,6 +30,7 @@ const testTopicOverview: TopicOverview = {
   schemaExists: false,
   prefixAclsExists: false,
   txnAclsExists: false,
+  createSchemaAllowed: false,
   topicInfo: {
     noOfPartitions: 1,
     noOfReplicas: "1",
@@ -72,6 +73,7 @@ const testTopicSchemas: TopicSchemaOverview = {
   txnAclsExists: false,
   allSchemaVersions: [1],
   latestVersion: 1,
+  createSchemaAllowed: false,
   schemaPromotionDetails: {
     status: "SUCCESS",
     sourceEnv: "3",
@@ -90,7 +92,6 @@ const testTopicSchemas: TopicSchemaOverview = {
     showNext: false,
     showPrev: false,
     latest: true,
-    promoteOnly: false,
   },
 };
 
