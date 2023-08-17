0d7a5e63c8652ea8045ce017e5ae5420d2e9be4a
Mirjam Aulbach <mirjam.aulbach@aiven.io>
2023-08-17 11:33:33 +0200

    Create ClaimBanner to be reused.
    Signed-off-by: Mirjam Aulbach <mirjam.aulbach@aiven.io>

---
+++ coral/src/app/features/components/ClaimBanner.tsx
@@ -0,0 +1,68 @@
+import { Alert, Banner, Box, Button, Spacing } from "@aivenio/aquarium";
+import { InternalLinkButton } from "src/app/components/InternalLinkButton";
+import illustration from "src/app/images/topic-details-banner-Illustration.svg";
+
+interface ClaimBannerProps {
+  entityType: "topic" | "connector";
+  entityName: string;
+  hasOpenRequest: boolean;
+  hasOpenClaimRequest: boolean;
+  claimEntity: () => void;
+  isError: boolean;
+  errorMessage?: string;
+  entityOwner: string;
+}
+
+const ClaimBanner = ({
+  entityType,
+  entityName,
+  hasOpenClaimRequest,
+  hasOpenRequest,
+  claimEntity,
+  isError,
+  errorMessage,
+  entityOwner,
+}: ClaimBannerProps) => {
+  if (hasOpenRequest) {
+    // We do not render an InternalLinkButton to the Requests page for this state...
+    // .. because a user cannot see the requests opened by members of other teams
+    return (
+      <Banner image={illustration} layout="vertical" title={""}>
+        <Box.Flex minHeight={"full"}>
+          <Box.Flex component={"p"} alignSelf={"center"}>
+            {`${entityName} has pending requests. Your team cannot claim ownership at this time.`}
+          </Box.Flex>
+        </Box.Flex>
+      </Banner>
+    );
+  }
+
+  if (hasOpenClaimRequest) {
+    return (
+      <Banner image={illustration} layout="vertical" title={""}>
+        <Box component={"p"} marginBottom={"l1"}>
+          A claim request for {entityName} is already in progress.
+        </Box>
+        <InternalLinkButton
+          to={`/requests/${entityType}s?search=${entityName}&requestType=CLAIM&status=CREATED&page=1`}
+        >
+          View request
+        </InternalLinkButton>
+      </Banner>
+    );
+  }
+
+  return (
+    <Banner image={illustration} layout="vertical" title={""}>
+      <Spacing gap={"l1"}>
+        {isError && <Alert type="error">{errorMessage}</Alert>}
+        <Box component={"p"} marginBottom={"l1"}>
+          {`This ${entityType} is currently owned by ${entityOwner}. Select "Claim ${entityType}" to request ownership.`}
+        </Box>
+      </Spacing>
+      <Button.Primary onClick={claimEntity}>Claim {entityType}</Button.Primary>
+    </Banner>
+  );
+};
+
+export { ClaimBanner };
