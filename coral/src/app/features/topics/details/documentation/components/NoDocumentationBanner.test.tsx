1136921d3e6bd7023c9c6423a9da7296f511c19a
Mirjam Aulbach <mirjam.aulbach@aiven.io>
2023-08-18 13:13:17 +0200

    Add isTopicOwner to NoDocumentationBanner.tsx
    Signed-off-by: Mirjam Aulbach <mirjam.aulbach@aiven.io>

--- coral/src/app/features/topics/details/documentation/components/NoDocumentationBanner.test.tsx
+++ coral/src/app/features/topics/details/documentation/components/NoDocumentationBanner.test.tsx
@@ -7,48 +7,94 @@ const testAddDocumentation = jest.fn();
 describe("NoDocumentationBanner", () => {
   const user = userEvent.setup();
 
-  describe("shows all necessary elements", () => {
-    beforeAll(() => {
-      render(<NoDocumentationBanner addDocumentation={testAddDocumentation} />);
-    });
-
-    afterAll(cleanup);
-
-    it("shows headline No Documentation", () => {
-      const headline = screen.getByRole("heading", {
-        name: "No readme available",
+  describe("if user is topic owner", () => {
+    describe("shows all necessary elements", () => {
+      beforeAll(() => {
+        render(
+          <NoDocumentationBanner
+            addDocumentation={testAddDocumentation}
+            isUserTopicOwner={true}
+          />
+        );
       });
-      expect(headline).toBeVisible();
+
+      afterAll(cleanup);
+
+      it("shows headline No Documentation", () => {
+        const headline = screen.getByRole("heading", {
+          name: "No readme available",
+        });
+        expect(headline).toBeVisible();
+      });
+
+      it("shows information that no readme is available for this topic", () => {
+        const infoText = screen.getByText(
+          "Add a readme to give your team essential information, guidelines, and context about the topic."
+        );
+        expect(infoText).toBeVisible();
+      });
+
+      it("shows button to add a readme", () => {
+        const button = screen.getByRole("button", { name: "Add readme" });
+        expect(button).toBeVisible();
+      });
     });
 
-    it("shows information that no readme is available for this topic", () => {
-      const infoText = screen.getByText(
-        "Add a readme to give your team essential information, guidelines, and context about the topic."
-      );
-      expect(infoText).toBeVisible();
-    });
+    describe("triggers a addDocumentation event", () => {
+      beforeEach(() => {
+        render(
+          <NoDocumentationBanner
+            addDocumentation={testAddDocumentation}
+            isUserTopicOwner={true}
+          />
+        );
+      });
 
-    it("shows button to add a readme", () => {
-      const button = screen.getByRole("button", { name: "Add readme" });
-      expect(button).toBeVisible();
+      afterEach(() => {
+        cleanup();
+        jest.clearAllMocks();
+      });
+
+      it("adds readme when user clicks button", async () => {
+        const button = screen.getByRole("button", { name: "Add readme" });
+        await user.click(button);
+
+        expect(testAddDocumentation).toHaveBeenCalled();
+      });
     });
   });
 
-  describe("triggers a addDocumentation event", () => {
-    beforeEach(() => {
-      render(<NoDocumentationBanner addDocumentation={testAddDocumentation} />);
-    });
+  describe("if user is not topic owner", () => {
+    describe("shows all necessary elements", () => {
+      beforeAll(() => {
+        render(
+          <NoDocumentationBanner
+            addDocumentation={testAddDocumentation}
+            isUserTopicOwner={false}
+          />
+        );
+      });
 
-    afterEach(() => {
-      cleanup();
-      jest.clearAllMocks();
-    });
+      afterAll(cleanup);
 
-    it("adds readme when user clicks button", async () => {
-      const button = screen.getByRole("button", { name: "Add readme" });
-      await user.click(button);
+      it("shows headline No Documentation", () => {
+        const headline = screen.getByRole("heading", {
+          name: "No readme available",
+        });
+        expect(headline).toBeVisible();
+      });
 
-      expect(testAddDocumentation).toHaveBeenCalled();
+      it("shows no information that no readme is available for this topic", () => {
+        const infoText = screen.queryByText(
+          "Add a readme to give your team essential information, guidelines, and context about the topic."
+        );
+        expect(infoText).not.toBeInTheDocument();
+      });
+
+      it("shows  no button to add a readme", () => {
+        const button = screen.queryByRole("button");
+        expect(button).not.toBeInTheDocument();
+      });
     });
   });
 });
--- coral/src/app/features/topics/details/documentation/components/NoDocumentationBanner.tsx
+++ coral/src/app/features/topics/details/documentation/components/NoDocumentationBanner.tsx
@@ -3,10 +3,22 @@ import illustration from "src/app/images/documentation-illustration.svg";
 
 type NoDocumentationBannerProps = {
   addDocumentation: () => void;
+  isUserTopicOwner: boolean;
 };
 function NoDocumentationBanner({
   addDocumentation,
+  isUserTopicOwner,
 }: NoDocumentationBannerProps) {
+  if (!isUserTopicOwner) {
+    return (
+      <EmptyState
+        title={"No readme available"}
+        image={illustration}
+        layout={EmptyStateLayout.CenterHorizontal}
+      />
+    );
+  }
+
   return (
     <EmptyState
       title={"No readme available"}
