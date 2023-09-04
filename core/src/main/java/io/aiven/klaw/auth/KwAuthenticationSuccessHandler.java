867774d792e009f36d631daed69389eb1a5509d1
muralibasani <muralidahr.basani@aiven.io>
2023-09-04 09:37:26 +0200

    Fix superadmin access
    Signed-off-by: muralibasani <muralidahr.basani@aiven.io>

--- core/src/main/java/io/aiven/klaw/auth/KwAuthenticationSuccessHandler.java
+++ core/src/main/java/io/aiven/klaw/auth/KwAuthenticationSuccessHandler.java
@@ -1,9 +1,13 @@
 package io.aiven.klaw.auth;
 
+import io.aiven.klaw.helpers.KwConstants;
+import io.aiven.klaw.helpers.UtilMethods;
+import io.aiven.klaw.helpers.db.rdbms.HandleDbRequestsJdbc;
 import jakarta.servlet.http.HttpServletRequest;
 import jakarta.servlet.http.HttpServletResponse;
 import java.io.IOException;
 import lombok.extern.slf4j.Slf4j;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.beans.factory.annotation.Value;
 import org.springframework.security.core.Authentication;
 import org.springframework.security.web.authentication.SavedRequestAwareAuthenticationSuccessHandler;
@@ -20,16 +24,21 @@ public class KwAuthenticationSuccessHandler extends SavedRequestAwareAuthenticat
   @Value("${klaw.quickstart.enabled:false}")
   private boolean quickStartEnabled;
 
+  @Value("${klaw.ad.username.attribute:preferred_username}")
+  private String preferredUsernameAttribute;
+
+  @Autowired HandleDbRequestsJdbc handleDbRequests;
+
   @Override
   public void onAuthenticationSuccess(
       HttpServletRequest request, HttpServletResponse response, Authentication authentication)
       throws IOException {
     log.debug("User logged in : {}", authentication.getPrincipal());
     super.clearAuthenticationAttributes(request);
-    response.sendRedirect(contextPath.concat(getRedirectPage(request)));
+    response.sendRedirect(contextPath.concat(getRedirectPage(request, authentication)));
   }
 
-  public String getRedirectPage(HttpServletRequest request) {
+  public String getRedirectPage(HttpServletRequest request, Authentication authentication) {
     DefaultSavedRequest defaultSavedRequest =
         (DefaultSavedRequest) request.getSession().getAttribute("SPRING_SECURITY_SAVED_REQUEST");
     String indexPage = "index";
@@ -37,7 +46,12 @@ public class KwAuthenticationSuccessHandler extends SavedRequestAwareAuthenticat
     String providerRoute = "{{ provider }}";
     String coralTopicsUri = "/coral/topics";
 
-    if (quickStartEnabled) {
+    if (quickStartEnabled
+        && handleDbRequests
+            .getUsersInfo(
+                UtilMethods.getUserName(authentication.getPrincipal(), preferredUsernameAttribute))
+            .getRole()
+            .equals(KwConstants.USER_ROLE)) {
       return coralTopicsUri;
     }
 
