65ecb834774346a88158176f6a03ab42041085a5
Mirjam Aulbach <mirjam.aulbach@aiven.io>
2023-09-06 12:42:58 +0200

    Add basic playwright setup
    Signed-off-by: Mirjam Aulbach <mirjam.aulbach@aiven.io>

---
+++ e2e/.eslintrc.cjs
@@ -0,0 +1,16 @@
+/* eslint-env node */
+module.exports = {
+  extends: [
+    "eslint:recommended",
+    "plugin:@typescript-eslint/recommended",
+    "plugin:@typescript-eslint/stylistic",
+    "prettier",
+  ],
+  parser: "@typescript-eslint/parser",
+  plugins: ["@typescript-eslint"],
+  root: true,
+  parserOptions: {
+    project: true,
+    tsconfigRootDir: __dirname,
+  },
+};
