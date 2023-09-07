52aa5fde7b8302934cce9c92096fc1a41f0a7aa9
Mirjam Aulbach <mirjam.aulbach@aiven.io>
2023-09-07 14:53:40 +0200

    Add test test :D for localhost.
    Signed-off-by: Mirjam Aulbach <mirjam.aulbach@aiven.io>

--- e2e/tests/example.spec.ts
+++ e2e/tests/example.spec.ts
@@ -1,20 +1,17 @@
 import { test, expect } from "@playwright/test";
 
 test("has title", async ({ page }) => {
-  await page.goto("https://playwright.dev/");
+  await page.goto("/");
 
-  // Expect a title "to contain" a substring.
-  await expect(page).toHaveTitle(/Playwright/);
+  await expect(page).toHaveTitle(
+    "Klaw | Kafka Self-service Topic Management Portal",
+  );
 });
 
-test("get started link", async ({ page }) => {
-  await page.goto("https://playwright.dev/");
+test("has login form", async ({ page }) => {
+  await page.goto("/");
 
-  // Click the get started link.
-  await page.getByRole("link", { name: "Get started" }).click();
-
-  // Expects page to have a heading with the name of Installation.
-  await expect(
-    page.getByRole("heading", { name: "Installation" }),
-  ).toBeVisible();
+  const userNameInput = page.getByPlaceholder("Username");
+  await expect(userNameInput).toBeVisible();
+  await expect(page.getByPlaceholder("Password")).toBeVisible();
 });
