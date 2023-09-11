51e0ca60814a4296627bc1a6a3a355ac030b1263
Mathieu Anderson <mathieu.anderson@aiven.io>
2023-09-11 13:57:17 +0200

    fix(coral): Add mocks to index
    Signed-off-by: Mathieu Anderson <mathieu.anderson@aiven.io>

--- coral/src/app/pages/configuration/environments/index.test.tsx
+++ coral/src/app/pages/configuration/environments/index.test.tsx
@@ -1,5 +1,11 @@
 import { cleanup, screen } from "@testing-library/react/pure";
 import EnvironmentsPage from "src/app/pages/configuration/environments";
+import {
+  getPaginatedEnvironmentsForConnector,
+  getPaginatedEnvironmentsForSchema,
+  getPaginatedEnvironmentsForTopicAndAcl,
+} from "src/domain/environment";
+import { EnvironmentPaginatedApiResponse } from "src/domain/environment/environment-types";
 import { customRender } from "src/services/test-utils/render-with-wrappers";
 
 const mockMatches = jest.fn();
@@ -8,7 +14,36 @@ jest.mock("react-router-dom", () => ({
   useMatches: () => mockMatches(),
 }));
 
+const mockedResponse: EnvironmentPaginatedApiResponse = {
+  totalPages: 0,
+  currentPage: 0,
+  totalEnvs: 0,
+  entries: [],
+};
+
+jest.mock("src/domain/environment/environment-api.ts");
+
+const mockGetPaginatedEnvironmentsForTopicAndAcl =
+  getPaginatedEnvironmentsForTopicAndAcl as jest.MockedFunction<
+    typeof getPaginatedEnvironmentsForTopicAndAcl
+  >;
+const mockGetPaginatedEnvironmentsForSchema =
+  getPaginatedEnvironmentsForSchema as jest.MockedFunction<
+    typeof getPaginatedEnvironmentsForSchema
+  >;
+const mockGetPaginatedEnvironmentsForConnector =
+  getPaginatedEnvironmentsForConnector as jest.MockedFunction<
+    typeof getPaginatedEnvironmentsForConnector
+  >;
+
 describe("Environments page", () => {
+  beforeEach(() => {
+    mockGetPaginatedEnvironmentsForTopicAndAcl.mockResolvedValue(
+      mockedResponse
+    );
+    mockGetPaginatedEnvironmentsForSchema.mockResolvedValue(mockedResponse);
+    mockGetPaginatedEnvironmentsForConnector.mockResolvedValue(mockedResponse);
+  });
   describe("renders Environments page with correct text", () => {
     beforeAll(() => {
       mockMatches.mockImplementation(() => [
