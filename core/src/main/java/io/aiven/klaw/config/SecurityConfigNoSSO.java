77d863c651bcdf5663ae6a1aa4ab75f882b7cec7
Aindriu Lavelle <aindriu.lavelle@aiven.io>
2023-09-11 19:44:01 +0100

    Addition of secure https communication
    Signed-off-by: Aindriu Lavelle <aindriu.lavelle@aiven.io>

--- core/src/main/java/io/aiven/klaw/config/SecurityConfigNoSSO.java
+++ core/src/main/java/io/aiven/klaw/config/SecurityConfigNoSSO.java
@@ -24,7 +24,6 @@ import org.springframework.security.authentication.AuthenticationManager;
 import org.springframework.security.authentication.AuthenticationProvider;
 import org.springframework.security.authentication.ProviderManager;
 import org.springframework.security.config.annotation.authentication.configuration.AuthenticationConfiguration;
-import org.springframework.security.config.annotation.method.configuration.EnableGlobalMethodSecurity;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
 import org.springframework.security.crypto.factory.PasswordEncoderFactories;
--- core/src/main/java/io/aiven/klaw/controller/CacheController.java
+++ core/src/main/java/io/aiven/klaw/controller/CacheController.java
@@ -2,18 +2,13 @@ package io.aiven.klaw.controller;
 
 import io.aiven.klaw.config.ManageDatabase;
 import io.aiven.klaw.dao.Env;
-import io.aiven.klaw.error.KlawNotAuthorizedException;
 import io.aiven.klaw.model.ApiResponse;
-import io.aiven.klaw.service.MailUtils;
 import jakarta.validation.Valid;
 import lombok.extern.slf4j.Slf4j;
 import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.beans.factory.annotation.Value;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.MediaType;
 import org.springframework.http.ResponseEntity;
-import org.springframework.security.access.prepost.PreAuthorize;
-import org.springframework.security.core.context.SecurityContextHolder;
 import org.springframework.web.bind.annotation.*;
 
 @RestController
@@ -23,7 +18,6 @@ public class CacheController {
 
   @Autowired private ManageDatabase manageDatabase;
 
-  @PreAuthorize("hasRole('CACHE_ADMIN') or hasRole('SUPERADMIN')")
   @PostMapping(
       value = "/environment/tenant/{tenantId}/id/{id}",
       produces = {MediaType.APPLICATION_JSON_VALUE},
@@ -31,22 +25,18 @@ public class CacheController {
   public ResponseEntity<ApiResponse> addEnvToCache(
       @PathVariable("tenantId") Integer tenantId,
       @PathVariable("id") Integer id,
-      @Valid @RequestBody Env env)
-  {
+      @Valid @RequestBody Env env) {
     manageDatabase.addEnvToCache(tenantId, env, true);
     return new ResponseEntity<>(HttpStatus.OK);
   }
 
-  @PreAuthorize("hasRole('ROLE_ADMIN') or hasRole('ROLE_SUPERADMIN')")
   @DeleteMapping(
       value = "/environment/tenant/{tenantId}/id/{id}",
       produces = {MediaType.APPLICATION_JSON_VALUE},
       consumes = {MediaType.APPLICATION_JSON_VALUE})
   public ResponseEntity<ApiResponse> removeEnvFromCache(
-      @PathVariable("tenantId") Integer tenantId, @PathVariable("id") Integer id)
-       {
+      @PathVariable("tenantId") Integer tenantId, @PathVariable("id") Integer id) {
     manageDatabase.removeEnvFromCache(tenantId, id, true);
     return new ResponseEntity<>(HttpStatus.OK);
   }
-
 }
