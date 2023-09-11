ed59998bc27af00773dc7e7d86297b04c1f9d012
Mirjam Aulbach <mirjam.aulbach@aiven.io>
2023-09-11 14:31:29 +0200

    Add server for all test suites.
    Signed-off-by: Mirjam Aulbach <mirjam.aulbach@aiven.io>

--- coral/test-setup/setup-files-after-env.ts
+++ coral/test-setup/setup-files-after-env.ts
@@ -6,6 +6,9 @@ import "src/services/test-utils/mock-documenation-helper";
 // This is needed to be able to use crypto.randomUUID in testing components which use it
 // ie: components using useToast
 import { Crypto } from "@peculiar/webcrypto";
+// eslint-disable-next-line @typescript-eslint/ban-ts-comment
+// @ts-ignore
+import { server } from "src/services/test-utils/api-mocks/server";
 
 window.crypto.randomUUID = new Crypto().randomUUID;
 
@@ -13,3 +16,11 @@ window.crypto.randomUUID = new Crypto().randomUUID;
 
 process.env.API_BASE_URL = "http://localhost:8080";
 process.env.FEATURE_FLAG_TOPIC_REQUEST = "true";
+
+beforeAll(() => {
+  server.listen();
+});
+
+afterAll(() => {
+  server.close();
+});
