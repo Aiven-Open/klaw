5ba826a87e1144f9432ad448d8046baff14deb71
Mirjam Aulbach <mirjam.aulbach@aiven.io>
2023-09-13 13:04:06 +0200

    Update headerlinks.
    Signed-off-by: Mirjam Aulbach <mirjam.aulbach@aiven.io>

--- coral/src/app/layout/header/HeaderMenuLink.test.tsx
+++ coral/src/app/layout/header/HeaderMenuLink.test.tsx
@@ -3,6 +3,7 @@ import { cleanup, render, screen } from "@testing-library/react";
 import userEvent from "@testing-library/user-event";
 import HeaderMenuLink from "src/app/layout/header/HeaderMenuLink";
 import { tabNavigateTo } from "src/services/test-utils/tabbing";
+import { customRender } from "src/services/test-utils/render-with-wrappers";
 
 const linkText = "Go to your profile page";
 
@@ -10,25 +11,28 @@ describe("HeaderMenuLink.tsx", () => {
   // (icon is not needed for the test, Icon component mocked out)
   const mockIcon = "" as unknown as typeof data;
 
-  describe("renders a default link with required props", () => {
+  describe("renders by default an internal link with required props", () => {
     beforeEach(() => {
-      render(
+      customRender(
         <HeaderMenuLink
           icon={mockIcon}
           href={"/myProfile"}
           linkText={linkText}
-        />
+        />,
+        { memoryRouter: true }
       );
     });
 
     afterEach(cleanup);
 
     it(`renders a link with text dependent on a given property`, () => {
+      const controlLink = screen.getByTestId("internal-link");
       const navLink = screen.getByRole("link", {
         name: linkText,
       });
 
       expect(navLink).toBeVisible();
+      expect(navLink).toEqual(controlLink);
     });
 
     it(`renders a href for that link dependent on a given property`, () => {
@@ -91,7 +95,91 @@ describe("HeaderMenuLink.tsx", () => {
     });
   });
 
-  describe("renders additional attributes dependent on props", () => {
+  describe("renders an external link with required props", () => {
+    beforeEach(() => {
+      render(
+        <HeaderMenuLink
+          icon={mockIcon}
+          href={"/myProfile"}
+          linkText={linkText}
+          externalLink={true}
+        />
+      );
+    });
+
+    afterEach(cleanup);
+
+    it(`renders a link with text dependent on a given property`, () => {
+      const controlLink = screen.getByTestId("external-link");
+      const navLink = screen.getByRole("link", {
+        name: linkText,
+      });
+
+      expect(navLink).toBeVisible();
+      expect(navLink).toEqual(controlLink);
+    });
+
+    it(`renders a href for that link dependent on a given property`, () => {
+      const navLink = screen.getByRole("link", {
+        name: linkText,
+      });
+      expect(navLink).toHaveAttribute("href", "/myProfile");
+    });
+
+    it(`renders an Icon`, async () => {
+      const icon = screen.getByTestId("ds-icon");
+
+      expect(icon).toBeVisible();
+    });
+
+    it(`triggers the rendering of a Tooltip on mouse hover`, async () => {
+      const navLink = screen.getByRole("link", {
+        name: linkText,
+      });
+      const tooltipBeforeHover = screen.queryByRole("tooltip");
+
+      expect(tooltipBeforeHover).toBeNull();
+
+      await userEvent.hover(navLink);
+
+      const tooltipAfterHover = screen.getByRole("tooltip");
+
+      expect(tooltipAfterHover).toBeVisible();
+      expect(tooltipAfterHover).toHaveTextContent(linkText);
+
+      // Assert the tooltip disappears after hover event stops
+      await userEvent.unhover(navLink);
+      expect(tooltipBeforeHover).toBeNull();
+    });
+
+    it(`triggers the rendering of a Tooltip on tab navigation`, async () => {
+      const navLink = screen.getByRole("link", {
+        name: linkText,
+      });
+      const tooltipBeforeFocus = screen.queryByRole("tooltip");
+
+      expect(tooltipBeforeFocus).toBeNull();
+
+      await tabNavigateTo({ targetElement: navLink });
+
+      const tooltipAfterFocus = screen.getByRole("tooltip");
+
+      expect(tooltipAfterFocus).toBeVisible();
+      expect(tooltipAfterFocus).toHaveTextContent(linkText);
+
+      // Assert the tooltip disappears after losing focus
+      await userEvent.tab();
+      expect(tooltipBeforeFocus).toBeNull();
+    });
+
+    it(`renders a hidden child with the tooltip text for assistive technology`, async () => {
+      const hiddenText = screen.getByText(linkText);
+
+      expect(hiddenText).toHaveClass("visually-hidden");
+    });
+  });
+
+  describe("renders an external link with additional attributes dependent on props", () => {
     afterEach(cleanup);
 
     it(`does not add a rel attribute by default`, () => {
@@ -100,6 +188,7 @@ describe("HeaderMenuLink.tsx", () => {
           icon={mockIcon}
           href={"/myProfile"}
           linkText={linkText}
+          externalLink={true}
         />
       );
 
@@ -117,6 +206,7 @@ describe("HeaderMenuLink.tsx", () => {
           href={"/myProfile"}
           linkText={linkText}
           rel={"noreferrer"}
+          externalLink={true}
         />
       );
 
--- coral/src/app/layout/header/HeaderMenuLink.tsx
+++ coral/src/app/layout/header/HeaderMenuLink.tsx
@@ -1,33 +1,23 @@
 import { Icon, Tooltip } from "@aivenio/aquarium";
 import data from "@aivenio/aquarium/dist/src/icons/console";
 import { useState } from "react";
+import { Link } from "react-router-dom";
 
 type HeaderMenuLinkProps = {
   icon: typeof data;
   linkText: string;
   href: string;
   rel?: string;
+  externalLink?: boolean;
 };
 function HeaderMenuLink(props: HeaderMenuLinkProps) {
-  const { icon, linkText, href, rel } = props;
+  const { icon, linkText, href, rel, externalLink } = props;
 
   const [isOpen, setIsOpen] = useState(false);
   const toggleOpen = () => setIsOpen(!isOpen);
 
-  return (
-    <a
-      href={href}
-      rel={rel}
-      // These mouse events are necessary because the Tooltip component does not show the popover on hover with React version >= 18
-      // @TODO: remove when aquarium is compatible with React version >= 18
-      onMouseEnter={toggleOpen}
-      onMouseLeave={toggleOpen}
-      // Allow displaying the tooltip when navigating with keyboard
-      // Because the Tooltip is rendered outside the main DOM hierarchy, it is ignored by screen readers
-      // So we can display it with keyboard navigation for users who use keyboard navigation, but not a screen reader
-      onFocus={toggleOpen}
-      onBlur={toggleOpen}
-    >
+  const children = (
+    <>
       <span className={"visually-hidden"}>{linkText}</span>
       {/*Aquarium does not fully support React 18 now, where children */}
       {/*is not a default prop for FC*/}
@@ -43,7 +33,49 @@ function HeaderMenuLink(props: HeaderMenuLinkProps) {
         {/* aria-hidden="true" is added natively to the Icon component */}
         <Icon icon={icon} fontSize={"20px"} color={"grey-0"} />
       </Tooltip>
-    </a>
+    </>
+  );
+
+  if (externalLink) {
+    return (
+      <a
+        // in tests there's no way to confirm
+        // the right link is rendered as Link
+        // is rendered as an A tag
+        data-testid={"external-link"}
+        href={href}
+        rel={rel}
+        // These mouse events are necessary because the Tooltip component does not show the popover on hover with React version >= 18
+        // @TODO: remove when aquarium is compatible with React version >= 18
+        onMouseEnter={toggleOpen}
+        onMouseLeave={toggleOpen}
+        // Allow displaying the tooltip when navigating with keyboard
+        // Because the Tooltip is rendered outside the main DOM hierarchy, it is ignored by screen readers
+        // So we can display it with keyboard navigation for users who use keyboard navigation, but not a screen reader
+        onFocus={toggleOpen}
+        onBlur={toggleOpen}
+      >
+        {children}
+      </a>
+    );
+  }
+
+  return (
+    <Link
+      data-testid={"internal-link"}
+      to={href}
+      // These mouse events are necessary because the Tooltip component does not show the popover on hover with React version >= 18
+      // @TODO: remove when aquarium is compatible with React version >= 18
+      onMouseEnter={toggleOpen}
+      onMouseLeave={toggleOpen}
+      // Allow displaying the tooltip when navigating with keyboard
+      // Because the Tooltip is rendered outside the main DOM hierarchy, it is ignored by screen readers
+      // So we can display it with keyboard navigation for users who use keyboard navigation, but not a screen reader
+      onFocus={toggleOpen}
+      onBlur={toggleOpen}
+    >
+      {children}
+    </Link>
   );
 }
 
