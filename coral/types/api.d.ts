e3e980d0ef4eb138aa69bd4acf9bc5c30efce847
Aindriu Lavelle <aindriu.lavelle@aiven.io>
2023-09-22 11:53:25 +0100

    Add the APIs to coral
    Signed-off-by: Aindriu Lavelle <aindriu.lavelle@aiven.io>

--- coral/types/api.d.ts
+++ coral/types/api.d.ts
@@ -216,6 +216,10 @@ export type paths = {
   "/chPwd": {
     post: operations["changePwd"];
   };
+  "/cache/tenant/{tenantId}/entityType/environment/id/{id}": {
+    post: operations["addEnvToCache"];
+    delete: operations["removeEnvFromCache"];
+  };
   "/addTenantId": {
     post: operations["addTenantId"];
   };
@@ -884,6 +888,40 @@ export type components = {
       envId: string;
       includeOnlyFailedTasks: boolean;
     };
+    Env: {
+      id?: string;
+      /** Format: int32 */
+      tenantId?: number;
+      name?: string;
+      stretchCode?: string;
+      /** Format: int32 */
+      clusterId?: number;
+      type?: string;
+      otherParams?: string;
+      envExists?: string;
+      /** @enum {string} */
+      envStatus?: "OFFLINE" | "ONLINE" | "NOT_KNOWN";
+      /** Format: date-time */
+      envStatusTime?: string;
+      associatedEnv?: components["schemas"]["EnvTag"];
+      params?: components["schemas"]["EnvParams"];
+    };
+    EnvParams: {
+      defaultPartitions?: string;
+      maxPartitions?: string;
+      partitionsList?: (string)[];
+      defaultRepFactor?: string;
+      maxRepFactor?: string;
+      replicationFactorList?: (string)[];
+      topicPrefix?: (string)[];
+      topicSuffix?: (string)[];
+      topicRegex?: (string)[];
+      applyRegex?: boolean;
+    };
+    EnvTag: {
+      id?: string;
+      name?: string;
+    };
     KwTenantModel: {
       tenantName: string;
       tenantDesc: string;
@@ -910,22 +948,6 @@ export type components = {
       /** Format: int32 */
       tenantId?: number;
       params?: components["schemas"]["EnvParams"];
-    };
-    EnvParams: {
-      defaultPartitions?: string;
-      maxPartitions?: string;
-      partitionsList?: (string)[];
-      defaultRepFactor?: string;
-      maxRepFactor?: string;
-      replicationFactorList?: (string)[];
-      topicPrefix?: (string)[];
-      topicSuffix?: (string)[];
-      topicRegex?: (string)[];
-      applyRegex?: boolean;
-    };
-    EnvTag: {
-      id?: string;
-      name?: string;
     };
     KwClustersModel: {
       /** Format: int32 */
@@ -2826,6 +2848,49 @@ export type operations = {
     parameters: {
       query: {
         changePwd: string;
+      };
+    };
+    responses: {
+      /** @description OK */
+      200: {
+        content: {
+          "application/json": components["schemas"]["ApiResponse"];
+        };
+      };
+    };
+  };
+  addEnvToCache: {
+    parameters: {
+      header: {
+        Authorization: string;
+      };
+      path: {
+        tenantId: number;
+        id: number;
+      };
+    };
+    requestBody: {
+      content: {
+        "application/json": components["schemas"]["Env"];
+      };
+    };
+    responses: {
+      /** @description OK */
+      200: {
+        content: {
+          "application/json": components["schemas"]["ApiResponse"];
+        };
+      };
+    };
+  };
+  removeEnvFromCache: {
+    parameters: {
+      header: {
+        Authorization: string;
+      };
+      path: {
+        tenantId: number;
+        id: number;
       };
     };
     responses: {
