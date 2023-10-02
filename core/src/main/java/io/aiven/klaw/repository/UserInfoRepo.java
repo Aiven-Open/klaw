1a6fc307d856b3c35b2d2e7e74fd2301f747dd3a
Sergey Nuyanzin <sergey.nuyanzin@aiven.io>
2023-10-02 17:40:57 +0200

    Add limitation by username to reduce amount of data transferring from db (#1792)
    ---------
    
    Signed-off-by: Sergey Nuyanzin <snuyanzin@gmail.com>

--- core/src/main/java/io/aiven/klaw/repository/UserInfoRepo.java
+++ core/src/main/java/io/aiven/klaw/repository/UserInfoRepo.java
@@ -12,6 +12,8 @@ public interface UserInfoRepo extends CrudRepository<UserInfo, String> {
 
   List<UserInfo> findAllByTenantId(int tenantId);
 
+  Optional<UserInfo> findFirstByTenantIdAndUsername(int tenantId, String username);
+
   List<UserInfo> findAllByTeamIdAndTenantId(Integer teamId, int tenantId);
 
   boolean existsByTeamIdAndTenantId(Integer teamId, int tenantId);
