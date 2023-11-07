b9fc442b61e00d9844a18e773b4a3a2a42d34215
Mirjam Aulbach <mirjam.aulbach@aiven.io>
2023-11-07 15:58:42 +0100

    Rename query for team api and use same in Teams component.
    Signed-off-by: Mirjam Aulbach <mirjam.aulbach@aiven.io>

--- coral/src/app/features/configuration/teams/Teams.tsx
+++ coral/src/app/features/configuration/teams/Teams.tsx
@@ -1,4 +1,12 @@
+import { useQuery } from "@tanstack/react-query";
+import { getTeams } from "src/domain/team";
+
 function Teams() {
+  const { data: teams } = useQuery(["get-teams"], {
+    queryFn: () => getTeams(),
+  });
+
+  console.log(teams);
   return <div>Hello</div>;
 }
 
