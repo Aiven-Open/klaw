7e0fea9cc6707be9e0fc343cdf3fc8712e7746fa
Mirjam Aulbach <mirjam.aulbach@aiven.io>
2023-11-07 15:57:22 +0100

    Add Team type
    Signed-off-by: Mirjam Aulbach <mirjam.aulbach@aiven.io>

--- coral/src/domain/team/index.ts
+++ coral/src/domain/team/index.ts
@@ -1,9 +1,10 @@
 import {
   ALL_TEAMS_VALUE,
   TeamName,
+  Team,
   TEAM_NOT_INITIALIZED,
 } from "src/domain/team/team-types";
 import { getTeams, getTeamsOfUser } from "src/domain/team/team-api";
 
-export type { TeamName };
+export type { TeamName, Team };
 export { getTeams, ALL_TEAMS_VALUE, TEAM_NOT_INITIALIZED, getTeamsOfUser };
