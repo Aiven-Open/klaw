094c3a60ec80579115e86d4bbd5e6f4793850d51
Mirjam Aulbach <mirjam.aulbach@aiven.io>
2023-11-07 12:33:18 +0100

    Add route behind FF and page for teams
    Signed-off-by: Mirjam Aulbach <mirjam.aulbach@aiven.io>

---
+++ coral/src/app/pages/configuration/teams/index.tsx
@@ -0,0 +1,12 @@
+import { PageHeader } from "@aivenio/aquarium";
+
+const TeamsPage = () => {
+  return (
+    <>
+      <PageHeader title={"Teams"} />
+      <div>hello!</div>
+    </>
+  );
+};
+
+export { TeamsPage };
