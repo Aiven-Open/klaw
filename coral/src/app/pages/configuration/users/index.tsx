5722aca3fa832bf9201f6a294b8c47727a5f3d15
Mirjam Aulbach <mirjam.aulbach@aiven.io>
2023-11-08 14:52:16 +0100

    Add users page, link to it  and route behind ff.
    Signed-off-by: Mirjam Aulbach <mirjam.aulbach@aiven.io>

---
+++ coral/src/app/pages/configuration/users/index.tsx
@@ -0,0 +1,12 @@
+import { PageHeader } from "@aivenio/aquarium";
+
+const UsersPage = () => {
+  return (
+    <>
+      <PageHeader title={"Users"} />
+      <div>Hello</div>
+    </>
+  );
+};
+
+export { UsersPage };
