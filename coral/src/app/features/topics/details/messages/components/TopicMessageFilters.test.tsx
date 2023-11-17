fe8897d356a2c7370c64519f1c825a5c475bb59b
Mathieu Anderson <mathieu.anderson@aiven.io>
2023-11-17 15:18:21 +0100

    feat(coral): Add custom offset filters
    Signed-off-by: Mathieu Anderson <mathieu.anderson@aiven.io>

--- coral/src/app/features/topics/details/messages/components/TopicMessageFilters.test.tsx
+++ coral/src/app/features/topics/details/messages/components/TopicMessageFilters.test.tsx
@@ -1,17 +1,21 @@
 import { cleanup, render, screen, within } from "@testing-library/react";
-import { TopicMessageOffsetFilter } from "src/app/features/topics/details/messages/components/TopicMessageOffsetFilter";
+import { TopicMessageFilters } from "src/app/features/topics/details/messages/components/TopicMessageFilters";
 import { userEvent } from "@testing-library/user-event";
 
-describe("TopicMessageOffsetFilter", () => {
+describe("TopicMessageFilters", () => {
   afterEach(() => {
     cleanup();
   });
   it("displays 5, 25 and 50 as possible offset values as options", () => {
     render(
-      <TopicMessageOffsetFilter
-        value="25"
-        onChange={jest.fn()}
+      <TopicMessageFilters
+        values={{ defaultOffset: "25", customOffset: null, partitionId: null }}
+        onDefaultOffsetChange={jest.fn()}
+        onPartitionIdChange={jest.fn()}
+        onCustomOffsetChange={jest.fn()}
         disabled={false}
+        mode={"Default"}
+        filterErrors={{ partitionIdFilters: null, customOffsetFilters: null }}
       />
     );
     expect(screen.getByRole("radio", { name: "5" })).toBeVisible();
@@ -22,10 +26,14 @@ describe("TopicMessageOffsetFilter", () => {
   it("is possible to select an option", async () => {
     const onChange = jest.fn();
     render(
-      <TopicMessageOffsetFilter
-        value="25"
-        onChange={onChange}
+      <TopicMessageFilters
+        values={{ defaultOffset: "25", customOffset: null, partitionId: null }}
+        onDefaultOffsetChange={onChange}
+        onPartitionIdChange={onChange}
+        onCustomOffsetChange={onChange}
         disabled={false}
+        mode={"Default"}
+        filterErrors={{ partitionIdFilters: null, customOffsetFilters: null }}
       />
     );
     await userEvent.click(screen.getByRole("radio", { name: "50" }));
@@ -35,10 +43,14 @@ describe("TopicMessageOffsetFilter", () => {
 
   it("is not possible to select an option if the input is disabled", () => {
     render(
-      <TopicMessageOffsetFilter
-        value="25"
-        onChange={jest.fn()}
+      <TopicMessageFilters
+        values={{ defaultOffset: "25", customOffset: null, partitionId: null }}
+        onDefaultOffsetChange={jest.fn()}
+        onPartitionIdChange={jest.fn()}
+        onCustomOffsetChange={jest.fn()}
         disabled={true}
+        mode={"Default"}
+        filterErrors={{ partitionIdFilters: null, customOffsetFilters: null }}
       />
     );
     within(
--- coral/src/app/features/topics/details/messages/useMessagesFilters.test.tsx
+++ coral/src/app/features/topics/details/messages/useMessagesFilters.test.tsx
@@ -1,8 +1,8 @@
 import { cleanup, renderHook } from "@testing-library/react";
 import { BrowserRouter, MemoryRouter } from "react-router-dom";
-import { useOffsetFilter } from "src/app/features/topics/details/messages/useOffsetFilter";
+import { useMessagesFilters } from "src/app/features/topics/details/messages/useMessagesFilters";
 
-describe("useOffsetFilter.tsx", () => {
+describe("useMessagesFilters.tsx", () => {
   afterEach(() => {
     window.history.pushState({}, "", "/");
     cleanup();
@@ -10,44 +10,46 @@ describe("useOffsetFilter.tsx", () => {
   it("returns 5 as the default offset value", () => {
     const {
       result: { current },
-    } = renderHook(() => useOffsetFilter(), {
+    } = renderHook(() => useMessagesFilters(), {
       wrapper: ({ children }) => <MemoryRouter>{children}</MemoryRouter>,
     });
-    const offset = current[0];
+    const offset = current.defaultOffsetFilters.defaultOffset;
     expect(offset).toBe("5");
   });
   it("gets the offset from search params", () => {
     const {
       result: { current },
-    } = renderHook(() => useOffsetFilter(), {
+    } = renderHook(() => useMessagesFilters(), {
       wrapper: ({ children }) => (
-        <MemoryRouter initialEntries={["/?offset=25"]}>{children}</MemoryRouter>
+        <MemoryRouter initialEntries={["/?defaultOffset=25"]}>
+          {children}
+        </MemoryRouter>
       ),
     });
-    const offset = current[0];
+    const offset = current.defaultOffsetFilters.defaultOffset;
     expect(offset).toEqual("25");
   });
   it("gets the default offset if provided value is not one of the fixed values", () => {
     const {
       result: { current },
-    } = renderHook(() => useOffsetFilter(), {
+    } = renderHook(() => useMessagesFilters(), {
       wrapper: ({ children }) => (
-        <MemoryRouter initialEntries={["/?offset=100"]}>
+        <MemoryRouter initialEntries={["/?defaultOffset=100"]}>
           {children}
         </MemoryRouter>
       ),
     });
-    const offset = current[0];
+    const offset = current.defaultOffsetFilters.defaultOffset;
     expect(offset).toEqual("5");
   });
   it("sets the offset to search params", async () => {
     const {
       result: { current },
-    } = renderHook(() => useOffsetFilter(), {
+    } = renderHook(() => useMessagesFilters(), {
       wrapper: ({ children }) => <BrowserRouter>{children}</BrowserRouter>,
     });
-    const setOffset = current[1];
-    setOffset("25");
-    expect(window.location.search).toBe("?offset=25");
+    const setDefaultOffset = current.defaultOffsetFilters.setDefaultOffset;
+    setDefaultOffset("25");
+    expect(window.location.search).toBe("?defaultOffset=25");
   });
 });
