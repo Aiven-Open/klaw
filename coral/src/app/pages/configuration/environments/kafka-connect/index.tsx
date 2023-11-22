b504f3197753eab3c28467aaa061dd6d25f93ef9
Mirjam Aulbach <mirjam.aulbach@aiven.io>
2023-11-22 13:30:18 +0100

    Add info banner with link to old interface in env page directly.
    Signed-off-by: Mirjam Aulbach <mirjam.aulbach@aiven.io>

--- coral/src/app/pages/configuration/environments/kafka-connect/index.tsx
+++ coral/src/app/pages/configuration/environments/kafka-connect/index.tsx
@@ -1,11 +1,9 @@
 import { PageHeader } from "@aivenio/aquarium";
-import PreviewBanner from "src/app/components/PreviewBanner";
 import KafkaConnectEnvironments from "src/app/features/configuration/environments/KafkaConnect/KafkaConnectEnvironments";
 
 const KafkaConnectEnvironmentsPage = () => {
   return (
     <>
-      <PreviewBanner linkTarget={"/envs"} />
       <PageHeader title={"Kafka Connect Environments"} />
       <KafkaConnectEnvironments />
     </>
