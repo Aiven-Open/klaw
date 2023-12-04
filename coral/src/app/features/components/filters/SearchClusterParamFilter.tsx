2126e22c480072020a971a077b015e1cf9484441
Mirjam Aulbach <mirjam.aulbach@aiven.io>
2023-12-04 10:42:14 +0100

    Add search filter for Cluster params.
    Signed-off-by: Mirjam Aulbach <mirjam.aulbach@aiven.io>

---
+++ coral/src/app/features/components/filters/SearchClusterParamFilter.tsx
@@ -0,0 +1,16 @@
+import { SearchFilter } from "src/app/features/components/filters/SearchFilter";
+
+function SearchClusterParamFilter() {
+  return (
+    <SearchFilter
+      label={"Search Cluster params"}
+      placeholder={"kafkaconnect"}
+      description={
+        "Partial match for: Cluster name, bootstrap server and protocol."
+      }
+      ariaDescription={`Searching starts automatically with a little delay while typing. Press "Escape" to delete all your input.`}
+    />
+  );
+}
+
+export { SearchClusterParamFilter };
