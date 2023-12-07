45e95d2ed3aedf9c56ae5c1ee87c55cec88ce0ab
Mathieu Anderson <mathieu.anderson@aiven.io>
2023-12-07 13:47:44 +0100

    fix(coral): Fix bad rebase
    Signed-off-by: Mathieu Anderson <mathieu.anderson@aiven.io>

--- coral/src/app/layout/main-navigation/MainNavigation.tsx
+++ coral/src/app/layout/main-navigation/MainNavigation.tsx
@@ -13,6 +13,8 @@ import { usePendingRequests } from "src/app/hooks/usePendingRequests";
 import MainNavigationLink from "src/app/layout/main-navigation/MainNavigationLink";
 import MainNavigationSubmenuList from "src/app/layout/main-navigation/MainNavigationSubmenuList";
 import { Routes } from "src/app/router_utils";
+import useFeatureFlag from "src/services/feature-flags/hook/useFeatureFlag";
+import { FeatureFlag } from "src/services/feature-flags/types";
 
 function MainNavigation() {
   const { pathname } = useLocation();
