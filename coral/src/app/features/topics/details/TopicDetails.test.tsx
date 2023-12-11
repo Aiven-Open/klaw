acbb5f813f2dac78bb677cdb1b2c02bbb901091f
Murali Basani <muralidhar.basani@aiven.io>
2023-12-11 13:34:07 +0100

    Rename variables - fix openapispec (#2103)
    * Rename variables - fix openapispec
    
    Signed-off-by: muralibasani <muralidahr.basani@aiven.io>
    
    * fixcoral): Fix test fixtures
    
    Signed-off-by: Mathieu Anderson <mathieu.anderson@aiven.io>
    
    ---------
    
    Signed-off-by: muralibasani <muralidahr.basani@aiven.io>
    Signed-off-by: Mathieu Anderson <mathieu.anderson@aiven.io>
    Co-authored-by: muralibasani <muralidahr.basani@aiven.io>
    Co-authored-by: Mathieu Anderson <mathieu.anderson@aiven.io>

--- coral/src/app/features/topics/details/TopicDetails.test.tsx
+++ coral/src/app/features/topics/details/TopicDetails.test.tsx
@@ -73,6 +73,7 @@ const testTopicOverview: TopicOverview = {
     hasOpenRequest: false,
     hasSchema: false,
     description: "my description",
+    topicOwner: false,
   },
   aclInfoList: [
     {
