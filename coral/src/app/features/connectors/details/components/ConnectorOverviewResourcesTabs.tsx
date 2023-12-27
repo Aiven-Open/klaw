385578fd73d5fad52e95d7dc787f4d9c35f14288
Mathieu Anderson <mathieu.anderson@aiven.io>
2023-12-27 10:19:56 +0100

    refactor(coral): Topic/Connector overview:  Fix small flash when switching tabs (#2118)
    fix(coral): Fix small flash when switching tabs
    
    Signed-off-by: Mathieu Anderson <mathieu.anderson@aiven.io>
    Co-authored-by: Mirjam Aulbach <mirjam.aulbach@aiven.io>

--- coral/src/app/features/connectors/details/components/ConnectorOverviewResourcesTabs.tsx
+++ coral/src/app/features/connectors/details/components/ConnectorOverviewResourcesTabs.tsx
@@ -45,31 +45,9 @@ function ConnectorOverviewResourcesTabs({
     }
   }
 
-  const tabsMap: Array<{
-    connectorOverviewTabEnum: ConnectorOverviewTabEnum;
-    title: string;
-  }> = [
-    {
-      connectorOverviewTabEnum: ConnectorOverviewTabEnum.OVERVIEW,
-      title: "Overview",
-    },
-    {
-      connectorOverviewTabEnum: ConnectorOverviewTabEnum.DOCUMENTATION,
-      title: "Readme",
-    },
-    {
-      connectorOverviewTabEnum: ConnectorOverviewTabEnum.HISTORY,
-      title: "History",
-    },
-    {
-      connectorOverviewTabEnum: ConnectorOverviewTabEnum.SETTINGS,
-      title: "Settings",
-    },
-  ];
-
-  function renderTabContent() {
-    if (isError) {
-      return (
+  const tabContent = (
+    <div>
+      {isError && (
         <Box marginBottom={"l1"} marginTop={"l2"}>
           <Alert type="error">
             There was an error trying to load the connector details:{" "}
@@ -78,59 +56,79 @@ function ConnectorOverviewResourcesTabs({
             Please try again later.
           </Alert>
         </Box>
-      );
-    }
+      )}
 
-    if (isLoading || connectorIsRefetching) {
-      return (
+      {(isLoading || connectorIsRefetching) && (
         <Box paddingTop={"l2"} display={"flex"} justifyContent={"center"}>
           <div className={"visually-hidden"}>Loading connector details</div>
           <Icon icon={loading} fontSize={"30px"} />
         </Box>
-      );
-    }
+      )}
 
-    if (!connectorOverview?.connectorExists) {
-      return (
+      {!isLoading && !connectorOverview?.connectorExists && (
         <Box marginBottom={"l1"} marginTop={"l2"}>
           <Alert type="warning">
             Connector {connectorName} does not exist.
           </Alert>
         </Box>
-      );
-    }
+      )}
 
-    return (
-      <div data-testid={"tabpanel-content"}>
-        <Outlet
-          context={{
-            environmentId:
-              environmentId || connectorOverview?.connectorInfo.environmentId,
-            connectorOverview,
-            connectorIsRefetching,
-          }}
-        />
-      </div>
-    );
-  }
+      {!isError &&
+        !isLoading &&
+        !connectorIsRefetching &&
+        connectorOverview?.connectorExists && (
+          <div data-testid={"tabpanel-content"}>
+            <Outlet
+              context={{
+                environmentId:
+                  environmentId ||
+                  connectorOverview?.connectorInfo.environmentId,
+                connectorOverview,
+                connectorIsRefetching,
+              }}
+            />
+          </div>
+        )}
+    </div>
+  );
 
   return (
     <Tabs
       value={currentTab}
       onChange={(resourceTypeId) => navigateToTab(navigate, resourceTypeId)}
     >
-      {tabsMap.map((tab) => {
-        return (
-          <Tabs.Tab
-            title={tab.title}
-            value={tab.connectorOverviewTabEnum}
-            aria-label={tab.title}
-            key={tab.title}
-          >
-            {currentTab === tab.connectorOverviewTabEnum && renderTabContent()}
-          </Tabs.Tab>
-        );
-      })}
+      <Tabs.Tab
+        title={"Overview"}
+        value={ConnectorOverviewTabEnum.OVERVIEW}
+        aria-label={"Overview"}
+        key={"Overview"}
+      >
+        {tabContent}
+      </Tabs.Tab>
+      <Tabs.Tab
+        title={"Readme"}
+        value={ConnectorOverviewTabEnum.DOCUMENTATION}
+        aria-label={"Readme"}
+        key={"Readme"}
+      >
+        {tabContent}
+      </Tabs.Tab>
+      <Tabs.Tab
+        title={"History"}
+        value={ConnectorOverviewTabEnum.HISTORY}
+        aria-label={"History"}
+        key={"History"}
+      >
+        {tabContent}
+      </Tabs.Tab>
+      <Tabs.Tab
+        title={"Settings"}
+        value={ConnectorOverviewTabEnum.SETTINGS}
+        aria-label={"Settings"}
+        key={"Settings"}
+      >
+        {tabContent}
+      </Tabs.Tab>
     </Tabs>
   );
 }
