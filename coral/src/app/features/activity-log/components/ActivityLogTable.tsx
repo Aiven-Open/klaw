e7cf06b5200353c8505be9a27d8c71a425f738d7
Mathieu Anderson <mathieu.anderson@aiven.io>
2024-01-02 17:31:48 +0100

    feat(coral): Add activity log table
    Signed-off-by: Mathieu Anderson <mathieu.anderson@aiven.io>

---
+++ coral/src/app/features/activity-log/components/ActivityLogTable.tsx
@@ -0,0 +1,84 @@
+import { DataTable, DataTableColumn, EmptyState } from "@aivenio/aquarium";
+import { ActivityLog } from "src/domain/requests/requests-types";
+
+// type ActivityLogsTableRow = {
+//   id: ActivityLog["req_no"];
+//   activityName: ActivityLog["activityName"];
+//   environmentName: ActivityLog["envName"];
+//   user: ActivityLog["user"];
+//   team: ActivityLog["team"];
+//   details: ActivityLog["details"];
+//   activityTimeString: ActivityLog["activityTimeString"];
+// };
+type ActivityLogsTableRow = {
+  id: number;
+  activityName: string;
+  environmentName: string;
+  user: string;
+  team: string;
+  details: string;
+  activityTimeString: string;
+};
+
+type Props = {
+  ariaLabel: string;
+  activityLogs: ActivityLog[];
+};
+
+function ActivityLogTable({ ariaLabel, activityLogs }: Props) {
+  const columns: Array<DataTableColumn<ActivityLogsTableRow>> = [
+    { type: "text", field: "activityName", headerName: "Activity" },
+    {
+      type: "status",
+      headerName: "Environment",
+      status: ({ environmentName }) => ({
+        status: "neutral",
+        text: environmentName,
+      }),
+    },
+    { type: "text", field: "user", headerName: "User" },
+    { type: "text", field: "team", headerName: "Team" },
+    { type: "text", field: "details", headerName: "Details" },
+    {
+      type: "text",
+      field: "activityTimeString",
+      headerName: "Date",
+      formatter: (value) => {
+        return `${value}${"\u00A0"}UTC`;
+      },
+    },
+  ];
+
+  const rows: ActivityLogsTableRow[] = activityLogs.map(
+    (activity: ActivityLog) => {
+      return {
+        id: activity.req_no || 0,
+        activityName: activity.activityName || "",
+        environmentName: activity.envName || "",
+        user: activity.user || "",
+        team: activity.team || "",
+        details: activity.details || "",
+        activityTimeString: activity.activityTimeString || "",
+      };
+    }
+  );
+
+  if (!rows.length) {
+    return (
+      <EmptyState title="No activity log">
+        No activity log matched your criteria.
+      </EmptyState>
+    );
+  }
+
+  return (
+    <DataTable
+      ariaLabel={ariaLabel}
+      columns={columns}
+      rows={rows}
+      noWrap={false}
+    />
+  );
+}
+
+export { ActivityLogTable, type Props as ActivityLogTableProps };
--- coral/src/app/pages/activity-log/index.tsx
+++ coral/src/app/pages/activity-log/index.tsx
@@ -1,12 +1,13 @@
 import { PageHeader } from "@aivenio/aquarium";
 import PreviewBanner from "src/app/components/PreviewBanner";
+import ActivityLog from "src/app/features/activity-log/ActivityLog";
 
 const ActivityLogPage = () => {
   return (
     <>
       <PreviewBanner linkTarget={"/activityLog"} />
       <PageHeader title={"Activity log"} />
-      <div>Loglog</div>
+      <ActivityLog />
     </>
   );
 };
