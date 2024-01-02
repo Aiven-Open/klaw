82a46b7b1a7c989c5276a1a5e825bcb7bf3733e1
Mathieu Anderson <mathieu.anderson@aiven.io>
2024-01-02 17:30:27 +0100

    feat(coral): Add activity log APi endpoint
    Signed-off-by: Mathieu Anderson <mathieu.anderson@aiven.io>

--- coral/src/domain/requests/requests-transformers.ts
+++ coral/src/domain/requests/requests-transformers.ts
@@ -1,5 +1,5 @@
 import { RequestsWaitingForApprovalWithTotal } from "src/domain/requests/requests-types";
-import { KlawApiModel } from "types/utils";
+import { KlawApiModel, KlawApiResponse } from "types/utils";
 
 const getRequestsWaitingForApprovalTransformer = (
   data: KlawApiModel<"RequestsCountOverview">
@@ -52,4 +52,22 @@ const getRequestsWaitingForApprovalTransformer = (
   return requestsWaitingForApproval;
 };
 
-export { getRequestsWaitingForApprovalTransformer };
+function activityLogTransformer(
+  apiResponse: KlawApiResponse<"showActivityLog">
+) {
+  if (apiResponse.length === 0) {
+    return {
+      totalPages: 0,
+      currentPage: 0,
+      entries: [],
+    };
+  }
+
+  return {
+    totalPages: Number(apiResponse[0].totalNoPages),
+    currentPage: Number(apiResponse[0].currentPage),
+    entries: apiResponse,
+  };
+}
+
+export { activityLogTransformer, getRequestsWaitingForApprovalTransformer };
