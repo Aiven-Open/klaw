7c8eafd53393e21dabaf40be5954da88f5205a44
Mathieu Anderson <mathieu.anderson@aiven.io>
2024-01-09 14:43:35 +0100

    fix(coral): Handle viewport resizing on Dashboard
    Signed-off-by: Mathieu Anderson <mathieu.anderson@aiven.io>

--- coral/src/app/features/dashboard/Dashboard.tsx
+++ coral/src/app/features/dashboard/Dashboard.tsx
@@ -2,6 +2,7 @@ import { Alert, Box, Card, Grid, Icon, Template } from "@aivenio/aquarium";
 import { AreaChart, Axis, BarChart, Tooltip } from "@aivenio/aquarium/charts";
 import loading from "@aivenio/aquarium/icons/loading";
 import { useQuery } from "@tanstack/react-query";
+import { useEffect, useState } from "react";
 import StatsDisplay from "src/app/components/StatsDisplay";
 import { usePendingRequests } from "src/app/hooks/usePendingRequests";
 import {
@@ -34,6 +35,34 @@ const Dashboard = () => {
   });
 
   const { TOPIC, ACL, SCHEMA, CONNECTOR } = usePendingRequests();
+
+  // The following contraption is to allow charts to be responsive despite being rendered in a Card
+  // Without it, the size of the charts calculated by the ResponsiveContainer they are crapped in under the hood
+  // will only be calculated on the first render, which makes the entire Dashboard layout unresponsive on viewport resize
+  // This will remove the Chart components and show the loading state while resizing, and then render them again with the proper size
+  // Source: https://github.com/recharts/recharts/issues/1423#issuecomment-538670179
+  const [forceLoading, setForceLoading] = useState(false);
+  useEffect(() => {
+    let resizeThrottleTimeout: NodeJS.Timeout;
+    let skip = false;
+    const resizeCb = () => {
+      if (skip) {
+        return;
+      }
+
+      setForceLoading(true);
+      skip = true;
+      resizeThrottleTimeout = setTimeout(() => {
+        setForceLoading(false);
+        skip = false;
+      }, 100);
+    };
+    window.addEventListener("resize", resizeCb);
+
+    return () => {
+      clearTimeout(resizeThrottleTimeout);
+    };
+  }, [setForceLoading]);
 
   return (
     <Template columns={1} gap={"l2"}>
@@ -92,6 +121,7 @@ const Dashboard = () => {
           />
         </Grid>
       </Card>
+
       <Card title="My team's requests per day" fullWidth>
         {isErrorChartData && (
           <Alert type={"error"}>
@@ -99,11 +129,12 @@ const Dashboard = () => {
             {parseErrorMsg(errorChartData)}
           </Alert>
         )}
-        {isLoadingChartData ? (
+        {isLoadingChartData || forceLoading ? (
           <Box.Flex
             justifyContent={"center"}
             alignContent={"center"}
             style={{ minHeight: "250px" }}
+            flexWrap={"wrap"}
           >
             <Icon icon={loading} fontSize={"30px"} />
           </Box.Flex>
@@ -116,6 +147,7 @@ const Dashboard = () => {
           </AreaChart>
         )}
       </Card>
+
       <Card title="My team's topics per environment" fullWidth>
         {isErrorChartData && (
           <Alert type={"error"}>
@@ -123,11 +155,12 @@ const Dashboard = () => {
             {parseErrorMsg(errorChartData)}
           </Alert>
         )}
-        {isLoadingChartData ? (
+        {isLoadingChartData || forceLoading ? (
           <Box.Flex
             justifyContent={"center"}
             alignContent={"center"}
             style={{ minHeight: "250px" }}
+            flexWrap={"wrap"}
           >
             <Icon icon={loading} fontSize={"30px"} />
           </Box.Flex>
