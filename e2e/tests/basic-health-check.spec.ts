d4f7fb25a73a89487c559c47bdf9982277170fca
Mirjam Aulbach <mirjam.aulbach@aiven.io>
2024-01-25 11:18:22 +0100

    Change password back to default.
    Signed-off-by: Mirjam Aulbach <mirjam.aulbach@aiven.io>

--- e2e/tests/basic-health-check.spec.ts
+++ e2e/tests/basic-health-check.spec.ts
@@ -8,7 +8,7 @@ const superAdminUserName = "superadmin";
 // this test will fail, because it would need to use the local password.
 // We need to make sure that Klaw runs on clean DBs for E2E tests.
 // For this basic health check, this is sufficient.
-const superAdminPassword = "kwsuperadmin123$$";
+const superAdminPassword = "welcometoklaw";
 test("has title", async ({ page }) => {
   await page.goto("/");
 
