604165a0253e0a95d0e7f6a62f4f7a2341484d54
Mathieu Anderson <mathieu.anderson@aiven.io>
2024-02-12 15:41:35 +0100

    feat(coral): Add primary action to Clusters header
    Signed-off-by: Mathieu Anderson <mathieu.anderson@aiven.io>

--- coral/src/app/pages/configuration/clusters/index.tsx
+++ coral/src/app/pages/configuration/clusters/index.tsx
@@ -1,12 +1,32 @@
 import { PageHeader } from "@aivenio/aquarium";
+import add from "@aivenio/aquarium/icons/add";
+import { useNavigate } from "react-router-dom";
 import PreviewBanner from "src/app/components/PreviewBanner";
+import { useAuthContext } from "src/app/context-provider/AuthProvider";
 import Clusters from "src/app/features/configuration/clusters/Clusters";
+import { Routes } from "src/app/router_utils";
 
 const ClustersPage = () => {
+  const navigate = useNavigate();
+  const {
+    permissions: { addDeleteEditClusters },
+  } = useAuthContext();
+
   return (
     <>
       <PreviewBanner linkTarget={"/clusters"} />
-      <PageHeader title={"Clusters"} />
+      <PageHeader
+        title={"Clusters"}
+        primaryAction={
+          addDeleteEditClusters
+            ? {
+                text: "Add new cluster",
+                onClick: () => navigate(Routes.ADD_CLUSTER),
+                icon: add,
+              }
+            : undefined
+        }
+      />
       <Clusters />
     </>
   );
