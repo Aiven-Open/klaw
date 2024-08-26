10ef3ebd9926757ffd6c5e3cf4847b582506863e
khatibtamal <khatibtamal@gmail.com>
2024-08-26 03:26:22 -0400

    Feature: Add backend for topic events view based on offset range (#2551)
    * Feature: Add backend for topic events view based on offset range
    
    Signed-off-by: khatib tamal <khatibtamal@gmail.com>
    
    * revert accidental change
    
    Signed-off-by: khatib tamal <khatibtamal@gmail.com>
    
    * removed slf4j from TopicContentsControllerIT
    
    Signed-off-by: khatib tamal <khatibtamal@gmail.com>
    
    * code review changes
    
    Signed-off-by: khatib tamal <khatibtamal@gmail.com>
    
    * ran mvn spotless
    
    Signed-off-by: khatib tamal <khatibtamal@gmail.com>
    
    * code review changes and ClusterApiControllerIT improvement
    
    Signed-off-by: Khatib Tamal <khatibtamal@gmail.com>
    
    ---------
    
    Signed-off-by: khatib tamal <khatibtamal@gmail.com>
    Signed-off-by: Khatib Tamal <khatibtamal@gmail.com>

---
+++ cluster-api/src/main/java/io/aiven/klaw/clusterapi/models/enums/TopicContentType.java
@@ -0,0 +1,15 @@
+package io.aiven.klaw.clusterapi.models.enums;
+
+import lombok.Getter;
+
+@Getter
+public enum TopicContentType {
+  CUSTOM("custom"),
+  RANGE("range");
+
+  private final String value;
+
+  TopicContentType(String value) {
+    this.value = value;
+  }
+}
