06ac682f1a65f392052de1da20438450eb4d0a35
Muralidhar Basani <muralidhar.basani@aiven.io>
2025-05-27 12:26:12 +0200

    Fix coral redirect

--- core/src/test/java/io/aiven/klaw/auth/KwAuthenticationSuccessHandlerTest.java
+++ core/src/test/java/io/aiven/klaw/auth/KwAuthenticationSuccessHandlerTest.java
@@ -1,8 +1,13 @@
 package io.aiven.klaw.auth;
 
 import static org.assertj.core.api.Assertions.assertThat;
+import static org.mockito.ArgumentMatchers.anyString;
+import static org.mockito.Mockito.doThrow;
+import static org.mockito.Mockito.mock;
 import static org.mockito.Mockito.when;
 
+import io.aiven.klaw.helpers.db.rdbms.HandleDbRequestsJdbc;
+import io.aiven.klaw.service.UtilControllerService;
 import jakarta.servlet.http.HttpServletRequest;
 import jakarta.servlet.http.HttpSession;
 import org.junit.jupiter.api.AfterEach;
@@ -12,8 +17,10 @@ import org.junit.jupiter.api.Test;
 import org.junit.jupiter.api.TestMethodOrder;
 import org.junit.jupiter.api.extension.ExtendWith;
 import org.mockito.Mock;
+import org.springframework.security.core.Authentication;
 import org.springframework.security.web.savedrequest.DefaultSavedRequest;
 import org.springframework.test.context.junit.jupiter.SpringExtension;
+import org.springframework.test.util.ReflectionTestUtils;
 
 @ExtendWith(SpringExtension.class)
 @TestMethodOrder(MethodOrderer.OrderAnnotation.class)
@@ -76,5 +83,31 @@ public class KwAuthenticationSuccessHandlerTest {
     String redirectedPage =
         kwAuthenticationSuccessHandler.getRedirectPage(httpServletRequest, null);
     assertThat(redirectedPage).isEqualTo("/browseTopics?topicName=testtopic");
+  }
+
+  @Test
+  void getRedirectPage_ReturnIndexWhenUserInfoThrowsException() {
+    // Arrange
+    when(httpServletRequest.getSession()).thenReturn(httpSession);
+    when(httpSession.getAttribute(springSavedReqAttribute)).thenReturn(defaultSavedRequest);
+
+    Authentication mockAuthentication = mock(Authentication.class);
+    HandleDbRequestsJdbc handleDbRequests = mock(HandleDbRequestsJdbc.class);
+
+    // Simulate exception when getUsersInfo is called
+    when(mockAuthentication.getPrincipal()).thenReturn("userPrincipal");
+    doThrow(new RuntimeException("DB error")).when(handleDbRequests).getUsersInfo(anyString());
+    ReflectionTestUtils.setField(kwAuthenticationSuccessHandler, "coralEnabled", true);
+    UtilControllerService.isCoralBuilt = true;
+
+    // Inject mocked handleDbRequests
+    kwAuthenticationSuccessHandler.handleDbRequests = handleDbRequests;
+
+    // Act
+    String redirectedPage =
+        kwAuthenticationSuccessHandler.getRedirectPage(httpServletRequest, mockAuthentication);
+
+    // Assert
+    assertThat(redirectedPage).isEqualTo("index");
   }
 }
